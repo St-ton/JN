0009190ae36e496ca8c6022a38f204b1e544b510
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2016-05-04 15:45:39 +0200

    Added Stub Hello World Widget UnlockRequestNotifier

---
+++ version/100/adminmenu/widget/class.WidgetUnlockRequestNotifier_jtl_widgets.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright (c) JTL-Software-GmbH
+ * @license http://jtl-url.de/jtlshoplicense
+ */
+
+require_once PFAD_ROOT . PFAD_ADMIN . PFAD_INCLUDES . PFAD_WIDGETS . 'class.WidgetBase.php';
+
+/**
+ * Class WidgetUnlockRequestNotifier_jtl_widgets
+ */
+class WidgetUnlockRequestNotifier_jtl_widgets extends WidgetBase
+{
+    /**
+     *
+     */
+    public function init()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return "Hello World!";
+    }
+}

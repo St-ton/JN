44aca1a34b4a45755c67744e98584b279327bfbe
David Hannappel <david.hannappel@jtl-software.com>
2016-05-19 12:55:41 +0200

    fix jtlshop/shop4#83

--- robots.php
+++ robots.php
@@ -9,7 +9,7 @@ require_once dirname(__FILE__) . '/includes/globalinclude.php';
 $robotsContent = file_get_contents(PFAD_ROOT . 'robots.txt');
 
 if (file_exists(PFAD_ROOT . '/export/sitemap_index.xml')) {
-    $robotsContent .= PHP_EOL . Shop::getURL() . '/export/sitemap_index.xml';
+    $robotsContent .= PHP_EOL . 'Sitemap: ' . Shop::getURL() . '/export/sitemap_index.xml';
 }
 
 ob_end_clean();

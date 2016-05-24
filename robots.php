70b6fe087acad0b4521167364d11640f0b2e9080
David Hannappel <david.hannappel@jtl-software.com>
2016-05-24 12:08:58 +0200

    only append Sitemap if no existing Sitemap entry found. re jtlshop/shop4#83

--- robots.php
+++ robots.php
@@ -8,7 +8,7 @@ require_once dirname(__FILE__) . '/includes/globalinclude.php';
 
 $robotsContent = file_get_contents(PFAD_ROOT . 'robots.txt');
 
-if (file_exists(PFAD_ROOT . '/export/sitemap_index.xml')) {
+if (file_exists(PFAD_ROOT . '/export/sitemap_index.xml') && strpos($robotsContent, 'Sitemap: ') === false) {
     $robotsContent .= PHP_EOL . 'Sitemap: ' . Shop::getURL() . '/sitemap_index.xml';
 } 
 

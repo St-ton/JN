ed27db39b4e1edcd27514b26690b31836ac00673
andy <andreas.juetten@jtl-software.com>
2016-06-08 08:58:38 +0200

    Image compatibility mode

---
+++ update/403/20160607140140_create_image_history_table.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * create image history table
+ *
+ * @author andy
+ * @created Tue, 07 Jun 2016 14:01:40 +0200
+ */
+
+/**
+ * Migration
+ *
+ * Available methods:
+ * execute            - returns affected rows
+ * fetchOne           - single fetched object
+ * fetchAll           - array of fetched objects
+ * fetchArray         - array of fetched assoc arrays
+ * dropColumn         - drops a column if exists
+ * addLocalization    - add localization
+ * removeLocalization - remove localization
+ * setConfig          - add / update config property
+ * removeConfig       - remove config property
+ */
+class Migration_20160607140140 extends Migration implements IMigration
+{
+    protected $author = 'andy';
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}

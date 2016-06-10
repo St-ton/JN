38b5df0a4491d2ed84cd81575c0014a14a4792ac
andy <andreas.juetten@jtl-software.com>
2016-06-10 16:55:11 +0200

    Update admin menu

---
+++ update/403/20160610163710_update_admin_menu.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * update admin menu
+ *
+ * @author andy
+ * @created Fri, 10 Jun 2016 16:37:10 +0200
+ */
+
+/**
+ * Migration
+ *
+ * Available methods:
+ * execute            - returns affected rows
+ * fetchOne           - single fetched object
+ * fetchAll           - array of fetched objects
+ * fetchArray         - array of fetched assoc arrays
+ * dropColumn         - drops a column if exists
+ * addLocalization    - add localization
+ * removeLocalization - remove localization
+ * setConfig          - add / update config property
+ * removeConfig       - remove config property
+ */
+class Migration_20160610163710 extends Migration implements IMigration
+{
+    protected $author = 'andy';
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}

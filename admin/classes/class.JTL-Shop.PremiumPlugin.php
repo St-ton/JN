eabf042a9667a32f477b697f0a0ff6792df5e1c7
Felix Moche <felix.moche@jtl-software.com>
2016-06-27 13:14:46 +0200

    added premium plugin - re#177

---
+++ admin/classes/class.JTL-Shop.PremiumPlugin.php
@@ -0,0 +1,406 @@
+<?php
+/**
+ * @copyright (c) JTL-Software-GmbH
+ * @license http://jtl-url.de/jtlshoplicense
+ */
+
+/**
+ * Class PremiumPlugin
+ */
+class PremiumPlugin
+{
+    const CERTIFICATION_LOGO = 'https://images.jtl-software.de/servicepartner/cert/jtl_certified_128.png';
+
+    /**
+     * @var array
+     */
+    private $advantages = array();
+
+    /**
+     * @var array
+     */
+    private $howTos = array();
+
+    /**
+     * @var string
+     */
+    private $longDescription = '';
+
+    /**
+     * @var string
+     */
+    private $shortDescription = '';
+
+    /**
+     * @var null
+     */
+    private $author = null;
+
+    /**
+     * @var array
+     */
+    private $badges = array();
+
+    /**
+     * @var string
+     */
+    private $title = '';
+
+    /**
+     * @var array
+     */
+    private $buttons = array();
+
+    /**
+     * @var bool
+     */
+    private $isInstalled = false;
+
+    /**
+     * @var bool
+     */
+    private $isActivated = false;
+
+    /**
+     * @var bool
+     */
+    private $exists = false;
+
+    /**
+     * @var null|string
+     */
+    private $pluginID = null;
+
+    /**
+     * @var int
+     */
+    private $kPlugin = 0;
+
+    /**
+     * @var null|stdClass
+     */
+    private $servicePartner = null;
+
+    /**
+     * @var array
+     */
+    private $screeShots = array();
+
+    /**
+     * PremiumPlugin constructor.
+     * @param string $pluginID
+     */
+    public function __construct($pluginID)
+    {
+        $plugin            = Plugin::getPluginById($pluginID);
+        $this->pluginID    = $pluginID;
+        $this->exists      = (file_exists(PFAD_ROOT . PFAD_PLUGIN . $pluginID . '/info.xml'));
+        $this->isInstalled = (isset($plugin->kPlugin) && $plugin->kPlugin > 0);
+        $this->isActivated = ($this->isInstalled && (int)$plugin->nStatus === 2);
+        $this->kPlugin     = ($this->isInstalled) ? (int)$plugin->kPlugin : 0;
+    }
+
+    /**
+     * @return int
+     */
+    public function getKPlugin()
+    {
+        return $this->kPlugin;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPluginID()
+    {
+        return $this->pluginID;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getExists()
+    {
+        return $this->exists;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsActivated()
+    {
+        return $this->isActivated;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsInstalled()
+    {
+        return $this->isInstalled;
+    }
+
+    /**
+     * @param stdClass $sp
+     * @return $this
+     */
+    public function setservicePartner($sp)
+    {
+        $this->servicePartner = $sp;
+        
+        return $this;
+    }
+
+    /**
+     * @return null|stdClass
+     */
+    public function getservicePartner()
+    {
+        return $this->servicePartner;
+    }
+
+    /**
+     * @param array $screnShots
+     * @return $this
+     */
+    public function setScreenshots(array $screnShots)
+    {
+        $this->screeShots = $screnShots;
+
+        return $this;
+    }
+
+    /**
+     * @param stdClass $screenShot
+     * @return $this
+     */
+    public function addScreenShot(stdClass $screenShot)
+    {
+        $this->screeShots[] = $screenShot;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getScreenShots()
+    {
+        return $this->screeShots;
+    }
+
+    /**
+     * @return null
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param $author
+     * @return $this
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @param string $title
+     * @param string $description
+     * @return $this
+     */
+    public function setLongDescription($title, $description)
+    {
+        $this->longDescription        = new stdClass();
+        $this->longDescription->title = $title;
+        $this->longDescription->html  = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLongDescription()
+    {
+        return $this->longDescription;
+    }
+
+    /**
+     * @param string $title
+     * @param string $description
+     * @return $this
+     */
+    public function setShortDescription($title, $description)
+    {
+        $this->shortDescription        = new stdClass();
+        $this->shortDescription->title = $title;
+        $this->shortDescription->html  = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortDescription()
+    {
+        return $this->shortDescription;
+    }
+
+    /**
+     * @param string $advantage
+     * @return $this
+     */
+    public function addAdvantage($advantage)
+    {
+        $this->advantages[] = $advantage;
+        return $this;
+    }
+
+    /**
+     * @param array $advantages
+     * @return $this
+     */
+    public function setAdvantages(array $advantages)
+    {
+        $this->advantages = $advantages;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAdvantages()
+    {
+        return $this->advantages;
+    }
+
+    /**
+     * @param string $howTo
+     * @return $this
+     */
+    public function addHowTo($howTo)
+    {
+        $this->howTos[] = $howTo;
+        return $this;
+    }
+
+    /**
+     * @param array $howTos
+     * @return $this
+     */
+    public function setHowTos(array $howTos)
+    {
+        $this->howTos = $howTos;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHowTos()
+    {
+        return $this->howTos;
+    }
+
+    /**
+     * @param string $url
+     * @param bool   $relative
+     * @return $this
+     */
+    public function addBadge($url, $relative = true)
+    {
+        $this->badges[] = ($relative) ? (Shop::getURL() . '/' . PFAD_ADMIN . PFAD_GFX . 'PremiumPlugins/' . $url) : $url;
+        
+        return $this;
+    }
+
+    /**
+     * @param array $badges
+     * @return $this
+     */
+    public function setBadges(array $badges)
+    {
+        $this->badges = $badges;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBadges()
+    {
+        return $this->badges;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCertifcationLogo()
+    {
+        return self::CERTIFICATION_LOGO;
+    }
+
+    /**
+     * @return array
+     */
+    public function getButtons()
+    {
+        return $this->buttons;
+    }
+
+    /**
+     * @param string      $caption
+     * @param string      $link
+     * @param string      $class
+     * @param null|string $fa
+     * @param bool        $external
+     * @return $this
+     */
+    public function addButton($caption, $link, $class = 'btn btn-default', $fa = null, $external = false)
+    {
+        $btn             = new stdClass();
+        $btn->link       = $link;
+        $btn->caption    = $caption;
+        $btn->class      = $class;
+        $btn->fa         = $fa;
+        $btn->external   = $external;
+        if ($external === true) {
+            $btn->fa .= ' fa-external-link';
+        }
+        $this->buttons[] = $btn;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCertifcates()
+    {
+        return (isset($this->servicePartner->oZertifizierungen_arr) && count($this->servicePartner->oZertifizierungen_arr) > 0);
+    }
+}
---
+++ admin/premiumplugin.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @copyright (c) JTL-Software-GmbH
+ * @license http://jtl-url.de/jtlshoplicense
+ */
+require_once dirname(__FILE__) . '/includes/admininclude.php';
+
+$oAccount->permission('PLUGIN_ADMIN_VIEW', true, true);
+
+$cHinweis      = '';
+$cFehler       = '';
+$step          = 'uebersicht';
+
+setzeSprache();
+
+$pluginID = (isset($_GET['plugin_id'])) ? $_GET['plugin_id'] : 's360_amazon_lpa_shop4';
+$pp       = null;
+if (!empty($pluginID)) {
+    $pp = new PremiumPlugin($pluginID);
+    $pp->setLongDescription('Schnell, einfach und sicher.',
+        '"Login und Bezahlen mit Amazon" ist die schnelle, einfache und sichere Art, Shop-Besucher zu Kunden zu machen. 
+        Ermöglichen Sie Millionen von Amazon-Kunden, sich in Ihrem Shop über "Login und Bezahlen mit Amazon" in ihr Amazon-Kundenkonto einzuloggen und mit den dort hinterlegten Zahlungs- und Versandinformationen in Ihrem Shop zu bezahlen. 
+        Jeder Kunde, der ein Amazon-Kundenkonto besitzt, kann "Login und Bezahlen mit Amazon" als Zahlungsart in Ihrem Shop auswählen.');
+    $pp->setShortDescription('Zertifiziertes Plugin für JTL-Shop 4',
+        'Für JTL-Shop 4 steht Ihnen "Login und Bezahlen mit Amazon" als zertifiziertes Plugin direkt im Backend zur Verfügung.');
+    $pp->setTitle('Amazon Payments Login & Pay (JTL Shop 4)');
+
+    $pp->setAuthor('Solution 360 GmbH');
+    
+    $pp->addButton('Jetzt registrieren', 'https://payments.amazon.de/', 'btn btn-primary', 'sign-in')
+       ->addButton('Dokumentation', 'https://shop.solution360.de/downloads/dokus/Plugin_Doku_AmazonLogin&Pay-Shop4.pdf', 'btn btn-default', null, true);
+    
+    $pp->addAdvantage('Neukundengewinnung und verbessertes Einkaufserlebnis - Chance auf höhere Konversion und mehr Umsatz Online-Shop durch vereinfachten Bezahlprozess. Käufer werden zu Ihren Kunden und Sie können Ihre Produkte direkt an sie vermarkten.')
+       ->addAdvantage('Desktop-, Tablet- und Smartphone-optimierte Buttons und Widgets - Erzielen Sie Verkäufe, die Ihnen ohne Mobiloptimierung entgehen würden.')
+       ->addAdvantage('Zahlungsvorgang als Widget in Ihrem Shop - keine Weiterleitung auf eine externe Website')
+       ->addAdvantage('Reine Zahlungsabwicklung - keine Weitergabe von Artikel- oder Warenkorbdaten an Amazon')
+       ->addAdvantage('Schutz vor Zahlungsausfall und Betrugsversuchen')
+       ->addAdvantage('Kostensenkung durch transaktionsbasiertes Preismodell ohne Grundgebühren, Vorauszahlungen o.Ä.');
+    
+    $pp->addHowTo('Registrieren Sie sich bei Amazon Payments unter <a title="Amazon Payments" href="https://payments.amazon.de/" target="_blank"><i class="fa fa-external-link"></i> https://payments.amazon.de/</a>')
+       ->addHowTo('Aktivieren Sie das Amazon Payments Plugin in Ihrem JTL-Shop 4')
+       ->addHowTo('Konfigurieren Sie das Amazon Payments Plugin mit Hilfe der Dokumentation von Solution 360. Diese finden Sie unter diesem <a title="Dokumentation" href="https://shop.solution360.de/downloads/dokus/Plugin_Doku_AmazonLogin&Pay-Shop4.pdf" target="_blank"><i class="fa fa-external-link"></i> Link</a>.')
+       ->addHowTo('Fertig!');
+
+    $ss          = new stdClass();
+    $ss->preview = 'https://bilder.jtl-software.de/erweiterungen/1165396.png';
+    $ss->full    = 'https://bilder.jtl-software.de/erweiterungen/1165396.png';
+    $pp->addScreenShot($ss);
+
+    $sp                        = new stdClass();
+    $sp->kServiceParnter       = 519;
+    $sp->marketPlaceURL        = 'https://www.jtl-software.de/Servicepartner-Detailansicht?id=519';
+    $sp->oZertifizierungen_arr = array(
+        'https://bilder.jtl-software.de/zertifikat/jtl_premium_sp_280.png',
+        'https://bilder.jtl-software.de/zertifikat/jtl_cert_badge_1_280.png',
+        'https://bilder.jtl-software.de/zertifikat/jtl_cert_badge_6_280.png',
+        'https://bilder.jtl-software.de/zertifikat/jtl_cert_badge_7_280.png',
+        'https://bilder.jtl-software.de/zertifikat/jtl_cert_badge_8_280.png',
+    );
+    $sp->cLogoPfad             = 'https://bilder.jtl-software.de/splogos/kServicepartner_519.png';
+    $sp->cFirma                = 'Solution 360 GmbH';
+    $sp->cPLZ                  = '10179';
+    $sp->cOrt                  = 'Berlin';
+    $sp->cStrasse              = 'Engeldamm 20';
+    $sp->cWWW                  = 'http://www.solution360.de';
+    $sp->cMail                 = 'mail@solution360.de';
+    $sp->cAdresszusatz         = '';
+    $sp->cLandName             = 'Deutschland';
+
+    $pp->setservicePartner($sp);
+
+    $pp->addBadge('AmazonPayments_PartnerLogos_Black_Premier_Partner.png', true);
+}
+
+$smarty->assign('pp', $pp)
+       ->display('premiumplugin.tpl');

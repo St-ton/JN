7c19bae0af7fe243a3f3cecdc9e65b5508fdb0a0
Mirko Schmidt <mirko.schmidt@jtl-software.com>
2016-08-08 13:09:11 +0200

    Delete serbia and montenegro from tland and change cEnglisch Serbien into Serbia
    fix #182

---
+++ update/405/20160808121428_delete_serbia_and_montenegro_from_tland.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * delete_serbia_and_montenegro_from_tland
+ *
+ * @author Mirko Schmidt
+ * @created Mon, 08 Aug 2016 12:14:28 +0200
+ */
+
+/**
+ * Migration
+ *
+ * Available methods:
+ * execute            - returns affected rows
+ * fetchOne           - single fetched object
+ * fetchAll           - array of fetched objects
+ * fetchArray         - array of fetched assoc arrays
+ * dropColumn         - drops a column if exists
+ * addLocalization    - add localization
+ * removeLocalization - remove localization
+ * setConfig          - add / update config property
+ * removeConfig       - remove config property
+ */
+class Migration_20160808121428 extends Migration implements IMigration
+{
+    protected $author = 'Mirko Schmidt';
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}

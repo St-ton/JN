139bf20495b6f8eac85808eadff6c26242fc2ce9
Felix Moche <felix.moche@jtl-software.com>
2016-09-01 14:27:46 +0200

    removed unnecessary uri check, set filenName to NULL on 404 pages - re #393

--- index.php
+++ index.php
@@ -109,20 +109,12 @@ Shop::getEntryPoint();
 
 if (Shop::$is404 === true) {
     $cParameter_arr['is404'] = true;
+    Shop::$fileName = null;
 }
 if (Shop::$fileName !== null) {
     require PFAD_ROOT . Shop::$fileName;
 }
 if ($cParameter_arr['is404'] === true) {
-    $uri = $_SERVER['REQUEST_URI'];
-    if (isset($_SERVER['HTTP_X_REWRITE_URL'])) {
-        $uri = $_SERVER['HTTP_X_REWRITE_URL'];
-    }
-    $parsed = parse_url($uri);
-    if (isset($parsed['path']) && (in_array($parsed['path'], array('index.php', 'navi.php')))) {
-        $kLink       = $linkHelper->getSpecialPageLinkKey(LINKTYP_STARTSEITE);
-        Shop::$kLink = $kLink;
-    }
     if (!isset($seo)) {
         $seo = null;
     }

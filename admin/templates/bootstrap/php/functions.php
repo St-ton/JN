b1c628d3f70c3c1a8cdb9659c78b516e87830058
Felix Moche <felix.moche@jtl-software.com>
2016-09-20 11:17:39 +0200

    smarty3 changes, phpdoc

--- admin/templates/bootstrap/php/functions.php
+++ admin/templates/bootstrap/php/functions.php
@@ -3,15 +3,15 @@
  * @copyright (c) JTL-Software-GmbH
  * @license http://jtl-url.de/jtlshoplicense
  */
-$smarty->register_function('getCurrencyConversionSmarty', 'getCurrencyConversionSmarty');
-$smarty->register_function('getCurrencyConversionTooltipButton', 'getCurrencyConversionTooltipButton');
-$smarty->register_function('getCurrentPage', 'getCurrentPage');
-$smarty->register_function('SmartyConvertDate', 'SmartyConvertDate');
-$smarty->register_function('getHelpDesc', 'getHelpDesc');
-$smarty->register_function('getExtensionCategory', 'getExtensionCategory');
-$smarty->register_function('formatVersion', 'formatVersion');
-$smarty->register_function('gravatarImage', 'gravatarImage');
-$smarty->register_modifier('permission', 'permission');
+$smarty->registerPlugin('function', 'getCurrencyConversionSmarty', 'getCurrencyConversionSmarty')
+       ->registerPlugin('function', 'getCurrencyConversionTooltipButton', 'getCurrencyConversionTooltipButton')
+       ->registerPlugin('function', 'getCurrentPage', 'getCurrentPage')
+       ->registerPlugin('function', 'SmartyConvertDate', 'SmartyConvertDate')
+       ->registerPlugin('function', 'getHelpDesc', 'getHelpDesc')
+       ->registerPlugin('function', 'getExtensionCategory', 'getExtensionCategory')
+       ->registerPlugin('function', 'formatVersion', 'formatVersion')
+       ->registerPlugin('function', 'gravatarImage', 'gravatarImage')
+       ->registerPlugin('modifier', 'permission', 'permission');
 
 /**
  * @param array $params
@@ -211,11 +211,16 @@ function formatVersion($params, &$smarty)
 /**
  * Get either a Gravatar URL or complete image tag for a specified email address.
  *
- * @param string $email The email address
- * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
- * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
- * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+ * @param array $params
+ *
+ * array['email'] - The email address
+ * array['s']     - Size in pixels, defaults to 80px [ 1 - 2048 ]
+ * array['d']     - Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+ * array['r']     - Maximum rating (inclusive) [ g | pg | r | x ]
+ *
+ * @param JTLSmarty $smarty
  * @source https://gravatar.com/site/implement/images/php/
+ * @return string
  */
 function gravatarImage($params, &$smarty)
 {

6e19bd470f7469244307b00f5b8b129b3fd38ea5
Felix Moche <felix.moche@jtl-software.com>
2016-10-12 11:35:17 +0200

    added sql

--- classes/class.JTL-Shop.FilterSearchSpecial.php
+++ classes/class.JTL-Shop.FilterSearchSpecial.php
@@ -108,7 +108,60 @@ class FilterSearchSpecial extends AbstractFilter implements IFilter
      */
     public function getSQLCondition()
     {
-        return '';
+//        $kKey = (isset($NaviFilter->Suchspecial->kKey)) ? $NaviFilter->Suchspecial->kKey : null;
+//        if (isset($NaviFilter->SuchspecialFilter->kKey) && $NaviFilter->SuchspecialFilter->kKey > 0) {
+//            $kKey = $NaviFilter->SuchspecialFilter->kKey;
+//        }
+        $conf = Shop::getSettings(array(CONF_BOXEN, CONF_GLOBAL));
+        switch ($this->kKey) {
+            case SEARCHSPECIALS_BESTSELLER:
+                $nAnzahl = (isset($conf['global']['global_bestseller_minanzahl'])
+                    && intval($conf['global']['global_bestseller_minanzahl'] > 0))
+                    ? (int)$conf['global']['global_bestseller_minanzahl']
+                    : 100;
+                return "ROUND(tbestseller.fAnzahl) >= " . $nAnzahl;
+
+            case SEARCHSPECIALS_SPECIALOFFERS:
+                $tasp = 'tartikelsonderpreis';
+                $tsp  = 'tsonderpreise';
+                if (!Shop::getNaviFilter()->PreisspannenFilter->isInitialized()) {
+                    $tasp = 'tasp';
+                    $tsp  = 'tsp';
+                }
+                return $tasp . " .kArtikel = tartikel.kArtikel
+                                    AND " . $tasp . ".cAktiv = 'Y' AND " . $tasp . ".dStart <= now()
+                                    AND (" . $tasp . ".dEnde >= curdate() OR " . $tasp . ".dEnde = '0000-00-00')
+                                    AND " . $tsp . " .kKundengruppe = " . (int)$_SESSION['Kundengruppe']->kKundengruppe;
+//@todo:
+//                $oFilter->tasp = $tasp;
+//                $oFilter->tsp  = $tsp;
+
+                break;
+
+            case SEARCHSPECIALS_NEWPRODUCTS:
+                $alter_tage      = ($conf['boxen']['box_neuimsortiment_alter_tage'] > 0)
+                    ? (int)$conf['boxen']['box_neuimsortiment_alter_tage']
+                    : 30;
+                return "tartikel.cNeu = 'Y' AND DATE_SUB(now(),INTERVAL $alter_tage DAY) < tartikel.dErstellt AND tartikel.cNeu = 'Y'";
+
+            case SEARCHSPECIALS_TOPOFFERS:
+                return "tartikel.cTopArtikel = 'Y'";
+
+            case SEARCHSPECIALS_UPCOMINGPRODUCTS:
+                return "now() < tartikel.dErscheinungsdatum";
+
+            case SEARCHSPECIALS_TOPREVIEWS:
+                if (!Shop::getNaviFilter()->BewertungFilter->isInitialized()) {
+                    $nMindestSterne  = (intval($conf['boxen']['boxen_topbewertet_minsterne'] > 0))
+                        ? (int)$conf['boxen']['boxen_topbewertet_minsterne']
+                        : 4;
+                    return " AND round(taex.fDurchschnittsBewertung) >= " . $nMindestSterne;
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 
     /**
@@ -116,6 +169,33 @@ class FilterSearchSpecial extends AbstractFilter implements IFilter
      */
     public function getSQLJoin()
     {
-        return '';
+        //        $kKey = (isset($NaviFilter->Suchspecial->kKey)) ? $NaviFilter->Suchspecial->kKey : null;
+//        if (isset($NaviFilter->SuchspecialFilter->kKey) && $NaviFilter->SuchspecialFilter->kKey > 0) {
+//            $kKey = $NaviFilter->SuchspecialFilter->kKey;
+//        }
+        switch ($this->kKey) {
+            case SEARCHSPECIALS_BESTSELLER:
+                return "JOIN tbestseller ON tbestseller.kArtikel = tartikel.kArtikel";
+
+            case SEARCHSPECIALS_SPECIALOFFERS:
+                if (!Shop::getNaviFilter()->PreisspannenFilter->isInitialized()) {
+                    return "JOIN tartikelsonderpreis AS tasp ON tasp.kArtikel = tartikel.kArtikel\nJOIN tsonderpreise AS tsp ON tsp.kArtikelSonderpreis = tasp.kArtikelSonderpreis";
+                }
+                return '';
+
+            case SEARCHSPECIALS_NEWPRODUCTS:
+            case SEARCHSPECIALS_TOPOFFERS:
+            case SEARCHSPECIALS_UPCOMINGPRODUCTS:
+                return '';
+
+            case SEARCHSPECIALS_TOPREVIEWS:
+                if (!Shop::getNaviFilter()->BewertungFilter->isInitialized()) {
+                    return "JOIN tartikelext AS taex ON taex.kArtikel = tartikel.kArtikel";
+                }
+                return '';
+
+            default:
+                return '';
+        }
     }
 }

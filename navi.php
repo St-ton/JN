5c75ea5c8b6d5bfb3113b39eae667569e9df91ce
Felix Moche <felix.moche@jtl-software.com>
2016-12-19 12:18:51 +0100

    compat with old URLs

--- navi.php
+++ navi.php
@@ -3,4 +3,15 @@
  * @copyright (c) JTL-Software-GmbH
  * @license http://jtl-url.de/jtlshoplicense
  */
-require_once dirname(__FILE__) . 'filter.php';
+require_once dirname(__FILE__) . '/includes/globalinclude.php';
+Shop::run();
+$cParameter_arr = Shop::getParameters();
+if ($cParameter_arr['kLink'] > 0) {
+    require_once PFAD_ROOT . PFAD_INCLUDES . 'smartyInclude.php';
+    require dirname(__FILE__) . '/seite.php';
+} elseif ($cParameter_arr['kArtikel'] > 0) {
+    require_once PFAD_ROOT . PFAD_INCLUDES . 'smartyInclude.php';
+    require_once dirname(__FILE__) . '/artikel.php';
+} else {
+    require_once dirname(__FILE__) . '/filter.php';
+}

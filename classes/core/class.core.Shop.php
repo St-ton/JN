589fac849abf8953eb4a7ed4dc334c7ca689f0c1
Felix Moche <felix.moche@jtl-software.com>
2017-03-22 12:03:30 +0100

    manually revert mr#205 - re #314

--- classes/core/class.core.Shop.php
+++ classes/core/class.core.Shop.php
@@ -1957,8 +1957,13 @@ final class Shop
         };
         if (isset($_COOKIE['eSIdAdm'])) {
             if (session_name() !== 'eSIdAdm') {
+                $oldID = session_id();
+                session_write_close();
+                session_id($_COOKIE['eSIdAdm']);
                 $result = $isLogged();
-                Session::getInstance(true, true);
+                session_write_close();
+                session_id($oldID);
+                new Session();
             } else {
                 $result = $isLogged();
             }

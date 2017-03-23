e91df935ca3bc96159798205f8c95c8f95156f17
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2017-03-23 15:36:49 +0100

    Hookup dashboard AJAX functions in io.php. Re #1047

--- admin/io.php
+++ admin/io.php
@@ -12,14 +12,19 @@ if (!$oAccount->getIsAuthenticated()) {
     exit();
 }
 
-$jsonApi = JSONAPI::getInstance();
-$io      = IO::getInstance();
+$jsonApi      = JSONAPI::getInstance();
+$io           = IO::getInstance();
+$dashboardInc = PFAD_ROOT . PFAD_ADMIN . PFAD_INCLUDES . 'dashboard_inc.php';
 
 $io->register('getPages', [$jsonApi, 'getPages'])
    ->register('getCategories', [$jsonApi, 'getCategories'])
    ->register('getProducts', [$jsonApi, 'getProducts'])
    ->register('getManufacturers', [$jsonApi, 'getManufacturers'])
-   ->register('getCustomers', [$jsonApi, 'getCustomers']);
+   ->register('getCustomers', [$jsonApi, 'getCustomers'])
+   ->register('closeWidget', 'closeWidget', $dashboardInc, 'DASHBOARD_VIEW')
+   ->register('addWidget', 'addWidget', $dashboardInc, 'DASHBOARD_VIEW')
+   ->register('expandWidget', 'expandWidget', $dashboardInc, 'DASHBOARD_VIEW')
+   ->register('setWidgetPosition', 'setWidgetPosition', $dashboardInc, 'DASHBOARD_VIEW');
 
 $data = $io->handleRequest($_REQUEST['io']);
 $io->respondAndExit($data);

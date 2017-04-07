2239b2811d416816792debe37af0369cb57f1248
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2017-04-07 15:00:42 +0200

    Add support for file downloads to IO class. Re #1047

--- classes/class.JTL-Shop.IO.php
+++ classes/class.JTL-Shop.IO.php
@@ -78,7 +78,16 @@ class IO
             return new IOError("Missing request property");
         }
 
-        return $this->execute($request['name'], $request['params']);
+        ob_start();
+        set_time_limit(0);
+
+        $result = $this->execute($request['name'], $request['params']);
+
+        if (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+
+        return $result;
     }
 
     /**
@@ -86,16 +95,18 @@ class IO
      */
     public function respondAndExit($data)
     {
+        // respond with an error?
+        if (is_object($data) && get_class($data) === 'IOError') {
+            header(makeHTTPHeader($data->code), true, $data->code);
+        } elseif (is_object($data) && get_class($data) === 'IOFile') {
+            $this->pushFile($data->filename, $data->mimetype);
+        }
+
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: no-cache, must-revalidate');
         header('Pragma: no-cache');
         header('Content-type: application/json');
-
-        // respond with an error?
-        if (is_object($data) && get_class($data) === 'IOError') {
-            header(makeHTTPHeader($data->code), true, $data->code);
-        }
 
         // encode data if not already encoded
         if (is_string($data)) {
@@ -157,5 +168,60 @@ class IO
         }
 
         return call_user_func_array($function, $params);
+    }
+
+    /**
+     * @param string $filename
+     * @param string $mimetype
+     */
+    protected function pushFile($filename, $mimetype)
+    {
+        $userAgent = '';
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        }
+
+        $browserAgent = '';
+        if (preg_match('/Opera\/([0-9].[0-9]{1,2})/', $userAgent, $m)) {
+            $browserAgent = 'opera';
+        } elseif (preg_match('/MSIE ([0-9].[0-9]{1,2})/', $userAgent, $m)) {
+            $browserAgent = 'ie';
+        } elseif (preg_match('/OmniWeb\/([0-9].[0-9]{1,2})/', $userAgent, $m)) {
+            $browserAgent = 'omniweb';
+        } elseif (preg_match('/Netscape([0-9]{1})/', $userAgent, $m)) {
+            $browserAgent = 'netscape';
+        } elseif (preg_match('/Mozilla\/([0-9].[0-9]{1,2})/', $userAgent, $m)) {
+            $browserAgent = 'mozilla';
+        } elseif (preg_match('/Konqueror\/([0-9].[0-9]{1,2})/', $userAgent, $m)) {
+            $browserAgent = 'konqueror';
+        }
+
+        if (($mimetype === 'application/octet-stream') || ($mimetype === 'application/octetstream')) {
+            $mimetype = ($browserAgent === 'ie' || $browserAgent === 'opera')
+                ? 'application/octetstream'
+                : 'application/octet-stream';
+        }
+
+        @ob_end_clean();
+        @ini_set('zlib.output_compression', 'Off');
+
+        header('Pragma: public');
+        header('Content-Transfer-Encoding: none');
+
+        if ($browserAgent === 'ie') {
+            header('Content-Type: ' . $mimetype);
+            header('Content-Disposition: inline; filename="' . basename($filename) . '"');
+        } else {
+            header('Content-Type: ' . $mimetype . '; name="' . basename($filename) . '"');
+            header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+        }
+
+        $size = @filesize($filename);
+        if ($size) {
+            header("Content-length: $size");
+        }
+
+        readfile($filename);
+        exit;
     }
 }

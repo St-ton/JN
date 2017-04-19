5be7f5a045b1e612b4eaa0d2db670c6324afeb4b
Felix Moche <felix.moche@jtl-software.com>
2017-04-19 11:36:01 +0200

    do not use outer variable name for prepared statement - fix #1192

--- includes/bestellabschluss_inc.php
+++ includes/bestellabschluss_inc.php
@@ -1264,8 +1264,7 @@ function finalisiereBestellung($cBestellNr = '', $bSendeMail = true)
             trim($bestellung->oRechnungsadresse->cOrt),
             trim($bestellung->oRechnungsadresse->cLand)
         );
-        $obj->cVerwendet = 'Y';
-        Shop::DB()->update('tkuponneukunde', 'cDatenHash', $hash, $obj);
+        Shop::DB()->update('tkuponneukunde', 'cDatenHash', $hash, (object)['cVerwendet' => 'Y']);
     }
 
     $_upd              = new stdClass();

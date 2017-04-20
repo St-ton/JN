7f8c4f80e47f563922d7937fc7586b9a07e2a427
Felix Moche <felix.moche@jtl-software.com>
2017-04-20 11:44:35 +0200

    removed unnecessary validateToken calls

--- admin/exportformate.php
+++ admin/exportformate.php
@@ -14,7 +14,6 @@ $step                = 'uebersicht';
 $oSmartyError        = new stdClass();
 $oSmartyError->nCode = 0;
 $link                = null;
-
 if (isset($_GET['neuerExport']) && (int)$_GET['neuerExport'] === 1 && validateToken()) {
     $step = 'neuer Export';
 }
@@ -90,10 +89,10 @@ if (isset($_POST['neu_export']) && (int)$_POST['neu_export'] === 1 && validateTo
 }
 $cAction       = null;
 $kExportformat = null;
-if (isset($_POST['action']) && strlen($_POST['action']) > 0 && (int)$_POST['kExportformat'] > 0 && validateToken()) {
+if (isset($_POST['action']) && strlen($_POST['action']) > 0 && (int)$_POST['kExportformat'] > 0) {
     $cAction       = $_POST['action'];
     $kExportformat = (int)$_POST['kExportformat'];
-} elseif (isset($_GET['action']) && strlen($_GET['action']) > 0 && (int)$_GET['kExportformat'] > 0 && validateToken()) {
+} elseif (isset($_GET['action']) && strlen($_GET['action']) > 0 && (int)$_GET['kExportformat'] > 0) {
     $cAction       = $_GET['action'];
     $kExportformat = (int)$_GET['kExportformat'];
 }
@@ -164,6 +163,8 @@ if ($cAction !== null && $kExportformat !== null && validateToken()) {
             } else {
                 $fehler = 'Das Exportformat <b>' . $exportformat->cName . '</b> konnte nicht erstellt werden.';
             }
+            break;
+        default:
             break;
     }
 }

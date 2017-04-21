c5c5abbd265822383f6cf19a885a0632d0f1c42e
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2017-04-21 11:00:43 +0200

    Remove unused code in plz_ort_import.php. Re #1047

--- admin/suche.php
+++ admin/suche.php
@@ -2,8 +2,6 @@
 /**
  * @copyright (c) JTL-Software-GmbH
  * @license http://jtl-url.de/jtlshoplicense
- */
-/**
  * @deprecated since 4.06 - AJAX function adminSearch() in admin/suche_inc.php is used instead
  */
 ob_start();

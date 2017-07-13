b68f02266bdade67f16549da9d851e9a944e6ece
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2017-07-13 11:10:22 +0200

    Set IO element property through eval chunk. Resolves SHOP-1438

--- admin/templates/bootstrap/js/global.js
+++ admin/templates/bootstrap/js/global.js
@@ -742,10 +742,12 @@ function ioCall(name, args, success, error, context)
                 var csslist = data.css || [];
 
                 csslist.forEach(function (assign) {
-                    var $target = $('#' + assign.target);
-                    if($target.length > 0) {
-                        $target[0][assign.attr] = assign.data;
-                    }
+                    var value = assign.data.replace(/'/g, "\\'").replace(/\n/g, "\\n");
+                    var js =
+                        "if ($('#" + assign.target + "').length > 0) {" +
+                        "   $('#" + assign.target + "')[0]." + assign.attr + " = '" + value + "';" +
+                        "}";
+                    jslist.push(js);
                 });
 
                 jslist.forEach(function (js) {

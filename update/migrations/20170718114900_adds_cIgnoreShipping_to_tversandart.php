1e2aa4d04c1681e07978436caaa6ed73792600d6
Marco Stickel <marco.stickel@jtl-software.com>
2017-07-20 15:23:05 +0200

    adds update for pickup shipping methods. SHOP-336

--- update/migrations/20170718114900_adds_cIgnoreShipping_to_tversandart.php
+++ update/migrations/20170718114900_adds_cIgnoreShipping_to_tversandart.php
@@ -29,6 +29,21 @@ class Migration_20170718114900 extends Migration implements IMigration
     {
         $this->execute("ALTER TABLE tversandart
             ADD COLUMN cIgnoreShippingProposal CHAR(1) NOT NULL DEFAULT 'N' AFTER cSendConfirmationMail;");
+
+        $bar = $this->execute("UPDATE tversandartzahlungsart AS vz
+                                        JOIN tzahlungsart AS z ON 
+                                            vz.kZahlungsart = z.kZahlungsart
+                                        JOIN tversandart AS v ON 
+                                            vz.kVersandart = v.kVersandart SET v.cIgnoreShippingProposal='Y'
+                                    WHERE v.cName LIKE'%Abholung%' 
+                                        OR z.cTSCode = 'CASH_ON_PICKUP' 
+                                            AND	(SELECT 
+                                                COUNT(nvz.kZahlungsart) 
+                                                FROM tversandartzahlungsart AS nvz 
+                                                WHERE nvz.kVersandart = v.kVersandart) = 1;");
+
+
+
     }
 
     public function down()

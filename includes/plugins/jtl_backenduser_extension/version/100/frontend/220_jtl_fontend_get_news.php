fe3283c1efc4d653a75adb1c4a102e1cc0df7725
Clemens Rudolph <clemens.rudolph@jtl-software.com>
2017-08-30 12:04:18 +0200

    add author to start-page-news. SHOP-1369 #close

---
+++ includes/plugins/jtl_backenduser_extension/version/100/frontend/220_jtl_fontend_get_news.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * HOOK_GET_NEWS
+ *
+ * Dieses Plugin erweitert Backend Nutzeraccounts um weitere Felder
+ * Ausgabe der Felder im News Frontend
+ *
+ * @package   jtl_backenduser_extension
+ * @copyright JTL-Software-GmbH
+ *
+ * @global array $args_arr
+ * @global Plugin $oPlugin
+ */
+
+// insert the author-object in each news-array-element
+//
+foreach ($args_arr['oNews_arr'] as $i => $oNews) {
+    $oNews->oAuthor = ContentAuthor::getInstance()->getAuthor('NEWS', $oNews->kNews);
+}
+
+

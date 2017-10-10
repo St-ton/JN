9866e61f449e46b997c8fac76c779db34d4fb98c
Felix Moche <felix.moche@jtl-software.com>
2017-10-10 12:47:17 +0200

    cs

--- admin/emailvorlagen.php
+++ admin/emailvorlagen.php
@@ -742,15 +742,15 @@ if (isset($_POST['Aendern'], $_POST['kEmailvorlage']) && (int)$_POST['Aendern'] 
     Shop::DB()->delete($cTableSetting, 'kEmailvorlage', $kEmailvorlage);
     // Email Ausgangsadresse
     if (isset($_POST['cEmailOut']) && strlen($_POST['cEmailOut']) > 0) {
-        saveEmailSetting($cTableSetting, $kEmailvorlage, "cEmailOut", $_POST['cEmailOut']);
+        saveEmailSetting($cTableSetting, $kEmailvorlage, 'cEmailOut', $_POST['cEmailOut']);
     }
     // Email Absendername
     if (isset($_POST['cEmailSenderName']) && strlen($_POST['cEmailSenderName']) > 0) {
-        saveEmailSetting($cTableSetting, $kEmailvorlage, "cEmailSenderName", $_POST['cEmailSenderName']);
+        saveEmailSetting($cTableSetting, $kEmailvorlage, 'cEmailSenderName', $_POST['cEmailSenderName']);
     }
     // Email Kopie
     if (isset($_POST['cEmailCopyTo']) && strlen($_POST['cEmailCopyTo']) > 0) {
-        saveEmailSetting($cTableSetting, $kEmailvorlage, "cEmailCopyTo", $_POST['cEmailCopyTo']);
+        saveEmailSetting($cTableSetting, $kEmailvorlage, 'cEmailCopyTo', $_POST['cEmailCopyTo']);
     }
 
     if ($nFehler == 1) {

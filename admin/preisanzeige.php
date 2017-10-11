9fa34bc317a64b352481abfdb74b0895eaccde7c
Felix Moche <felix.moche@jtl-software.com>
2017-10-11 17:05:21 +0200

    holePreisanzeigeEinstellungen

--- admin/preisanzeige.php
+++ admin/preisanzeige.php
@@ -7,18 +7,114 @@
  */
 require_once __DIR__ . '/includes/admininclude.php';
 
+/**
+ * Preisanzeige Einstellungen holen
+ *
+ * @return array|mixed
+ * @former holePreisanzeigeEinstellungen
+ */
+function getPriceDisplayConfig()
+{
+    $oPreisanzeigeConfTMP_arr = [];
+    $oPreisanzeigeConf_arr    = Shop::DB()->selectAll(
+        'teinstellungen',
+        'kEinstellungenSektion',
+        CONF_PREISANZEIGE,
+        '*',
+        'cName'
+    );
+    $cMapping_arr             = [
+        'preisanzeige_preisgrafik_artikeldetails_anzeigen'    => 'Artikeldetails',
+        'preisanzeige_preisgrafik_artikeluebersicht_anzeigen' => 'Artikeluebersicht',
+        'preisanzeige_preisgrafik_boxen_anzeigen'             => 'Boxen',
+        'preisanzeige_preisgrafik_startseite_anzeigen'        => 'Startseite',
+
+        'preisanzeige_groesse_artikeldetails'                 => 'Artikeldetails',
+        'preisanzeige_groesse_artikeluebersicht'              => 'Artikeluebersicht',
+        'preisanzeige_groesse_boxen'                          => 'Boxen',
+        'preisanzeige_groesse_startseite'                     => 'Startseite',
+
+        'preisanzeige_farbe_artikeldetails'                   => 'Artikeldetails',
+        'preisanzeige_farbe_artikeluebersicht'                => 'Artikeluebersicht',
+        'preisanzeige_farbe_boxen'                            => 'Boxen',
+        'preisanzeige_farbe_startseite'                       => 'Startseite',
+
+        'preisanzeige_schriftart_artikeldetails'              => 'Artikeldetails',
+        'preisanzeige_schriftart_artikeluebersicht'           => 'Artikeluebersicht',
+        'preisanzeige_schriftart_boxen'                       => 'Boxen',
+        'preisanzeige_schriftart_startseite'                  => 'Startseite'
+    ];
+    // Mapping
+    if (is_array($oPreisanzeigeConf_arr) && count($oPreisanzeigeConf_arr) > 0) {
+        foreach ($oPreisanzeigeConf_arr as $z => $oPreisanzeigeConf) {
+            foreach ($cMapping_arr as $i => $cMapping) {
+                if ($oPreisanzeigeConf->cName == $i) {
+                    $oPreisanzeigeConfTMP_arr[$cMapping][] = $oPreisanzeigeConf;
+                }
+            }
+        }
+    } else {
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_preisgrafik_artikeldetails_anzeigen', 'N', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_preisgrafik_artikeluebersicht_anzeigen', 'N', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_preisgrafik_boxen_anzeigen', 'N', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_preisgrafik_startseite_anzeigen', 'N', NULL)", 4);
+
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_groesse_artikeldetails', '18', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_groesse_artikeluebersicht', '18', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_groesse_boxen', '18', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_groesse_startseite', '18', NULL)", 4);
+
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_farbe_artikeldetails', '#000000', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_farbe_artikeluebersicht', '#000000', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_farbe_boxen', '#000000', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_farbe_startseite', '#000000', NULL)", 4);
+
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_schriftart_artikeldetails', 'GeosansLight.ttf', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_schriftart_artikeluebersicht', 'GeosansLight.ttf', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_schriftart_boxen', 'GeosansLight.ttf', NULL)", 4);
+        Shop::DB()->query("INSERT INTO teinstellungen (kEinstellungenSektion, cName, cWert, cModulId)
+                            VALUES(" . CONF_PREISANZEIGE . ", 'preisanzeige_schriftart_startseite', 'GeosansLight.ttf', NULL)", 4);
+
+        $oPreisanzeigeConf_arr = Shop::DB()->selectAll('teinstellungen', 'kEinstellungenSektion', CONF_PREISANZEIGE, '*', 'cName ASC');
+        foreach ($oPreisanzeigeConf_arr as $z => $oPreisanzeigeConf) {
+            foreach ($cMapping_arr as $i => $cMapping) {
+                if ($oPreisanzeigeConf->cName == $i) {
+                    $oPreisanzeigeConfTMP_arr[$cMapping][] = $oPreisanzeigeConf;
+                }
+            }
+        }
+    }
+    $oPreisanzeigeConf_arr = $oPreisanzeigeConfTMP_arr;
+
+    return $oPreisanzeigeConf_arr;
+}
+
 $oAccount->permission('DISPLAY_PRICECHART_VIEW', true, true);
 /** @global JTLSmarty $smarty */
 $cHinweis              = '';
 $cFehler               = '';
-$oPreisanzeigeConf_arr = [];
-$oPreisanzeigeConf_arr = holePreisanzeigeEinstellungen();
+$oPreisanzeigeConf_arr = getPriceDisplayConfig();
 // Update Preisanzeige
-if (isset($_POST['update']) &&
-    (int)$_POST['update'] === 1 &&
-    is_array($oPreisanzeigeConf_arr) &&
-    validateToken() &&
-    count($oPreisanzeigeConf_arr) > 0
+if (isset($_POST['update'])
+    && (int)$_POST['update'] === 1
+    && is_array($oPreisanzeigeConf_arr)
+    && validateToken()
+    && count($oPreisanzeigeConf_arr) > 0
 ) {
     foreach ($oPreisanzeigeConf_arr as $oPreisanzeigeConf) {
         $upd = new stdClass();
@@ -42,7 +138,7 @@ if (isset($_POST['update']) &&
     }
 
     unset($GLOBALS['Einstellungen']['preisverlauf']);
-    $oPreisanzeigeConf_arr = holePreisanzeigeEinstellungen();
+    $oPreisanzeigeConf_arr = getPriceDisplayConfig();
     $cHinweis .= 'Ihre Einstellungen wurde erfolgreich gespeichert.';
 
     Shop::Cache()->flushTags([CACHING_GROUP_ARTICLE, CACHING_GROUP_CATEGORY, CACHING_GROUP_OPTION]);

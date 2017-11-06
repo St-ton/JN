a5ebeb51584ceec7777f0fcc389a96c4b66fde15
Clemens Rudolph <clemens.rudolph@jtl-software.com>
2017-11-06 10:26:08 +0100

    old tstaat removed, new one created. SHOP-1611

---
+++ update/406/20171103125032_new_tstaat.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * new_tstaat
+ *
+ * @author Clemens Rudolph
+ * @created Fri, 03 Nov 2017 12:50:32 +0100
+ */
+
+/**
+ * Migration
+ *
+ * Available methods:
+ * execute            - returns affected rows
+ * fetchOne           - single fetched object
+ * fetchAll           - array of fetched objects
+ * fetchArray         - array of fetched assoc arrays
+ * dropColumn         - drops a column if exists
+ * addLocalization    - add localization
+ * removeLocalization - remove localization
+ * setConfig          - add / update config property
+ * removeConfig       - remove config property
+ */
+class Migration_20171103125032 extends Migration implements IMigration
+{
+    protected $author = 'Clemens Rudolph';
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}

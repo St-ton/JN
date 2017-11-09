d32cd87fbcd7c10bf06b7433c3aa585932379f67
Marco Stickel <marco.stickel@jtl-software.com>
2017-11-09 11:16:56 +0100

    adds settings for headlines

--- admin/includes/portlets/class.PortletHeading.php
+++ admin/includes/portlets/class.PortletHeading.php
@@ -17,8 +17,48 @@ class PortletHeading extends PortletBase
     {
         $level = $this->properties['level'];
         $text  = $this->properties['text'];
+        $class = $this->properties['class'];
 
-        return "<h$level>$text</h$level>";
+        // animation
+        $animationStyle     = $this->properties['animation-style'];
+        $animationDuration  = $this->properties['animation-duration'];
+        $animationDelay     = $this->properties['animation-delay'];
+        $animationOffset    = $this->properties['animation-offset'];
+        $animationIteration = $this->properties['animation-iteration'];
+        // style
+        $this->properties['style']['background-color']    = $this->properties['background-color'];
+        $this->properties['style']['margin-top']          = $this->properties['margin-top'];
+        $this->properties['style']['margin-right']        = $this->properties['margin-right'];
+        $this->properties['style']['margin-bottom']       = $this->properties['margin-bottom'];
+        $this->properties['style']['margin-left']         = $this->properties['margin-left'];
+        $this->properties['style']['padding-top']         = $this->properties['padding-top'];
+        $this->properties['style']['padding-right']       = $this->properties['padding-right'];
+        $this->properties['style']['padding-bottom']      = $this->properties['padding-bottom'];
+        $this->properties['style']['padding-left']        = $this->properties['padding-left'];
+        $this->properties['style']['border-top-width']    = $this->properties['border-top-width'];
+        $this->properties['style']['border-right-width']  = $this->properties['border-right-width'];
+        $this->properties['style']['border-bottom-width'] = $this->properties['border-bottom-width'];
+        $this->properties['style']['border-left-width']   = $this->properties['border-left-width'];
+        $this->properties['style']['border-style']        = $this->properties['border-style'];
+        $this->properties['style']['border-color']        = $this->properties['border-color'];
+
+        if (!empty($animationStyle)){
+            $class .= ' wow '.$animationStyle;
+            if (!empty($animationDuration) && trim($animationDuration) != ''){
+                $this->properties['attr']['data-wow-duration'] = $animationDuration;
+            }
+            if (!empty($animationDelay) && trim($animationDelay) != ''){
+                $this->properties['attr']['data-wow-delay'] = $animationDelay;
+            }
+            if (!empty($animationOffset) && trim($animationOffset) != ''){
+                $this->properties['attr']['data-wow-offset'] = $animationOffset;
+            }
+            if (!empty($animationIteration) && trim($animationIteration) != ''){
+                $this->properties['attr']['data-wow-iteration'] = $animationIteration;
+            }
+        }
+
+        return "<h$level class=\"$class\"" . $this->attr_str() . $this->style_str() . ">$text</h$level>";
     }
 
     /**
@@ -36,7 +76,30 @@ class PortletHeading extends PortletBase
     {
         return [
             'level' => 1,
-            'text'  => 'Heading Title'
+            'text'  => 'Heading Title',
+            'class' => '',
+             // animation
+            'animation-style'     => '',
+            'animation-duration'  => '',
+            'animation-delay'     => '',
+            'animation-offset'    => '',
+            'animation-iteration' => '',
+            // style
+            'margin-top'          => '',
+            'margin-right'        => '',
+            'margin-bottom'       => '',
+            'margin-left'         => '',
+            'background-color'    => '',
+            'padding-top'         => '',
+            'padding-right'       => '',
+            'padding-bottom'      => '',
+            'padding-left'        => '',
+            'border-top-width'    => '',
+            'border-right-width'  => '',
+            'border-bottom-width' => '',
+            'border-left-width'   => '',
+            'border-style'        => '',
+            'border-color'        => ''
         ];
     }
 

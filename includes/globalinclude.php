54985963c85f6ec4952f287bf53bdcc5fce8f031
Felix Moche <felix.moche@jtl-software.com>
2017-11-10 11:52:45 +0100

    earlier profiler start, removed unnecessary comments

--- includes/globalinclude.php
+++ includes/globalinclude.php
@@ -18,11 +18,13 @@ if (defined('PFAD_ROOT')) {
 
 require_once PFAD_ROOT . PFAD_INCLUDES . 'error_handler.php';
 require_once PFAD_ROOT . PFAD_INCLUDES . 'autoload.php';
-// existiert Konfiguration?
+
 defined('DB_HOST') || die('Kein MySql-Datenbank Host angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
 defined('DB_NAME') || die('Kein MySql Datenbanknamen angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
 defined('DB_USER') || die('Kein MySql-Datenbank Benutzer angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
 defined('DB_PASS') || die('Kein MySql-Datenbank Passwort angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
+
+Profiler::start();
 
 $shop = Shop::getInstance();
 
@@ -35,8 +37,6 @@ if (!function_exists('Shop')) {
         return Shop::getInstance();
     }
 }
-
-// PHP memory_limit work around
 if (!Shop()->PHPSettingsHelper()->hasMinLimit(64)) {
     ini_set('memory_limit', '64M');
 }
@@ -44,8 +44,6 @@ if (!Shop()->PHPSettingsHelper()->hasMinLimit(64)) {
 require_once PFAD_ROOT . PFAD_INCLUDES . 'tools.Global.php';
 require_once PFAD_ROOT . PFAD_BLOWFISH . 'xtea.class.php';
 
-Profiler::start();
-// datenbankverbindung aufbauen
 try {
     $GLOBALS['DB'] = new NiceDB(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 } catch (Exception $exc) {
@@ -60,14 +58,11 @@ if (PHP_SAPI !== 'cli'
     && $conf['global']['kaufabwicklung_ssl_nutzen'] === 'P'
     && (!isset($_SERVER['HTTPS']) || (strtolower($_SERVER['HTTPS']) !== 'on' && (int)$_SERVER['HTTPS'] !== 1))
 ) {
-    $https = false;
-    if ((isset($_SERVER['HTTP_X_FORWARDED_HOST']) && $_SERVER['HTTP_X_FORWARDED_HOST'] === 'ssl.webpack.de')
+    $https = ((isset($_SERVER['HTTP_X_FORWARDED_HOST']) && $_SERVER['HTTP_X_FORWARDED_HOST'] === 'ssl.webpack.de')
         || (isset($_SERVER['SCRIPT_URI']) && preg_match('/^ssl-id/', $_SERVER['SCRIPT_URI']))
         || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
         || (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && preg_match('/^ssl/', $_SERVER['HTTP_X_FORWARDED_HOST']))
-    ) {
-        $https = true;
-    }
+    );
     if (!$https) {
         $lang = '';
         if (!standardspracheAktiv(true)) {
@@ -83,35 +78,26 @@ if (PHP_SAPI !== 'cli'
 if (!JTL_INCLUDE_ONLY_DB) {
     require_once PFAD_ROOT . PFAD_INCLUDES . 'sprachfunktionen.php';
     require_once PFAD_ROOT . PFAD_INCLUDES . 'parameterhandler.php';
-    // globale Werkzeuge
     require_once PFAD_ROOT . PFAD_XAJAX . 'xajax_core/xajax.inc.php';
     require_once PFAD_ROOT . PFAD_INCLUDES_EXT . 'auswahlassistent_ext_inc.php';
     require_once PFAD_ROOT . PFAD_INCLUDES . 'artikelsuchspecial_inc.php';
-    // Liste aller Hooks, die momentan im Shop gebraucht werden könnten
-    // An jedem Hook hängt ein Array mit Plugin die diesen Hook benutzen
     $oPluginHookListe_arr = Plugin::getHookList();
     $nSystemlogFlag       = Jtllog::getSytemlogFlag();
-    // Mobil-Template
-    $template = Template::getInstance();
+    $template             = Template::getInstance();
     $template->check(true);
-    // Globale Einstellungen
-    $GlobaleEinstellungen = Shop::getSettings([
+    $GlobaleEinstellungen         = Shop::getSettings([
         CONF_GLOBAL,
         CONF_RSS,
         CONF_METAANGABEN,
         CONF_KUNDENWERBENKUNDEN,
         CONF_BILDER
     ]);
-    // Globale Metaangaben
     $oGlobaleMetaAngabenAssoc_arr = Metadata::getGlobalMetaData();
     executeHook(HOOK_GLOBALINCLUDE_INC);
-    // Boxen
-    $oBoxen = Boxen::getInstance();
-    // Session
-    $session = (defined('JTLCRON') && JTLCRON === true)
+    $oBoxen              = Boxen::getInstance();
+    $session             = (defined('JTLCRON') && JTLCRON === true)
         ? Session::getInstance(true, true, 'JTLCRON')
         : Session::getInstance();
-    //Wartungsmodus aktiviert?
     $bAdminWartungsmodus = false;
     if ($GlobaleEinstellungen['global']['wartungsmodus_aktiviert'] === 'Y'
         && basename($_SERVER['SCRIPT_FILENAME']) !== 'wartung.php'

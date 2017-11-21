53b8d51b4113b2cc41e5d1ad141a71845dfe9567
Mirko Schmidt <mirko.schmidt@jtl-software.com>
2017-11-21 09:33:12 +0100

    Prevent combination of new customer and other coupons
    SHOP-1857 #resolve

--- includes/bestellvorgang_inc.php
+++ includes/bestellvorgang_inc.php
@@ -3015,7 +3015,8 @@ function kuponMoeglich()
     $Kategorie_qry  = '';
     $Kunden_qry     = '';
     /** @var array('Warenkorb' => Warenkorb) $_SESSION */
-    if (isset($_SESSION['Zahlungsart']->cModulId) && strpos($_SESSION['Zahlungsart']->cModulId, 'za_billpay') === 0) {
+    if (isset($_SESSION['Zahlungsart']->cModulId) && strpos($_SESSION['Zahlungsart']->cModulId, 'za_billpay') === 0
+        || isset($_SESSION["NeukundenKuponAngenommen"]) && $_SESSION["NeukundenKuponAngenommen"]) {
         return 0;
     }
     if (is_array($_SESSION['Warenkorb']->PositionenArr) && count($_SESSION['Warenkorb']->PositionenArr) > 0) {

5a72eed7fa0f4b7dad765db819f07a2d127f7645
Felix Moche <felix.moche@jtl-software.com>
2017-12-21 11:06:04 +0100

    setData->setBaseData

--- classes/interface.JTL-Shop.IFilter.php
+++ classes/interface.JTL-Shop.IFilter.php
@@ -163,7 +163,7 @@ interface IFilter
      * @param ProductFilter|null $productFilter
      * @return $this
      */
-    public function setData($productFilter);
+    public function setBaseData($productFilter);
 
     /**
      * the language ID currently active in the shop

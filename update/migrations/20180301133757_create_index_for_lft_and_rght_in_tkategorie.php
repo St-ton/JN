097edc587c7dc957967fbe5ca41281cdaf0c9991
Falk Prüfer <falk.pruefer@jtl-software.com>
2018-03-01 15:19:39 +0100

    Create index for lft and rght in tkategorie. SHOP-2195

---
+++ update/migrations/20180301133757_create_index_for_lft_and_rght_in_tkategorie.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Create index for lft and rght in tkategorie
+ *
+ * @author Falk Prüfer
+ * @created Thu, 01 Mar 2018 13:37:57 +0100
+ */
+
+/**
+ * Migration
+ *
+ * Available methods:
+ * execute            - returns affected rows
+ * fetchOne           - single fetched object
+ * fetchAll           - array of fetched objects
+ * fetchArray         - array of fetched assoc arrays
+ * dropColumn         - drops a column if exists
+ * addLocalization    - add localization
+ * removeLocalization - remove localization
+ * setConfig          - add / update config property
+ * removeConfig       - remove config property
+ */
+class Migration_20180301133757 extends Migration implements IMigration
+{
+    protected $author      = 'fp';
+    protected $description = /** @lang text */
+        'Create index for lft and rght in tkategorie';
+
+    /**
+     * @return bool|void
+     * @throws Exception
+     */
+    public function up()
+    {
+        // Check if an index for lft or rght always exists
+        $idxExists = $this->fetchAll(
+            "SHOW INDEX FROM tkategorie WHERE Column_name IN ('lft', 'rght')"
+        );
+
+        if (count($idxExists) > 0) {
+            // If so - delete it if name or column count matches...
+            $idxDelete = [];
+            foreach ($idxExists as $idx) {
+                if (array_key_exists($idx->Key_name, $idxDelete)) {
+                    $idxDelete[$idx->Key_name]++;
+                } else {
+                    $idxDelete[$idx->Key_name] = 1;
+                }
+            }
+            foreach ($idxDelete as $idxName => $idxColumnCount) {
+                if ($idxName === 'idx_tkategorie_lft_rght' || $idxColumnCount <= 2) {
+                    $this->execute(
+                        "ALTER TABLE `tkategorie` 
+                            DROP INDEX `$idxName`"
+                    );
+                }
+            }
+        }
+
+        $this->execute(
+            "ALTER TABLE `tkategorie` 
+                ADD INDEX `idx_tkategorie_lft_rght` (`lft` ASC, `rght` ASC);"
+        );
+    }
+
+    public function down()
+    {
+        $this->execute(
+            "ALTER TABLE `tkategorie` 
+                DROP INDEX `idx_tkategorie_lft_rght`"
+        );
+    }
+}

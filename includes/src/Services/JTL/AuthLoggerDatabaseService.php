609a11c87c5191045979d89be173b55ab863a7a2
Felix Moche <felix.moche@jtl-software.com>
2018-03-05 15:25:06 +0100

    removed handler from logger file

--- includes/src/Services/JTL/AuthLoggerDatabaseService.php
+++ includes/src/Services/JTL/AuthLoggerDatabaseService.php
@@ -6,11 +6,10 @@
 
 namespace Services\JTL;
 
+use JTL\ProcessingHandler\NiceDBHandler;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
-use Monolog\Handler\AbstractProcessingHandler;
 
 /**
  * Class AuthLoggerDatabaseService
@@ -30,44 +29,5 @@ class AuthLoggerDatabaseService extends AuthLoggerService
             ->setFormatter(new LineFormatter(null, null, false, true));
         $this->logger->pushHandler($handler)
                      ->pushProcessor(new PsrLogMessageProcessor());
-    }
-}
-
-/**
- * Class NiceDBHandler
- * @package Services\JTL
- */
-class NiceDBHandler extends AbstractProcessingHandler
-{
-    /**
-     * @var \NiceDB
-     */
-    private $db;
-
-    /**
-     * NiceDBHandler constructor.
-     * @param \NiceDB $db
-     * @param int     $level
-     * @param bool    $bubble
-     */
-    public function __construct(\NiceDB $db, $level = Logger::DEBUG, $bubble = true)
-    {
-        $this->db = $db;
-        parent::__construct($level, $bubble);
-    }
-
-    /**
-     * @param array $record
-     */
-    protected function write(array $record)
-    {
-        $this->db->insert(
-            'tjtllog',
-            (object)[
-                'cKey'      => $record['channel'],
-                'nLevel'    => $record['level'],
-                'cLog'      => $record['formatted'],
-                'dErstellt' => $record['datetime']->format('Y-m-d H:i:s'),
-            ]);
     }
 }

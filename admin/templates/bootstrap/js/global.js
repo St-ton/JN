8c1b70a4396f542ed604d4d35f1cde80b1ccdb0b
Marco Stickel <marco.stickel@jtl-software.com>
2018-04-13 15:02:47 +0200

    uses indeterminate checkboxes at box configuration page. SHOP-978 #resolve

--- admin/templates/bootstrap/js/global.js
+++ admin/templates/bootstrap/js/global.js
@@ -377,6 +377,15 @@ function switchCouponTooltipVisibility() {
     });
 }
 
+function tristateInit() {
+    $("input[type=checkbox].tristate").click(tristate(this));
+}
+
+function tristate(cb) {
+    if (cb.readOnly) cb.checked=cb.readOnly=false;
+    else if (!cb.checked) cb.readOnly=cb.indeterminate=true;
+}
+
 /**
  * document ready
  */
@@ -493,6 +502,12 @@ $(document).ready(function () {
     // Massenerstellung von Kupons de-/aktivieren
     $("#couponCreation").change(function () {
         massCreationCoupons();
+    });
+
+
+    $("input[type=checkbox].tristate").prop("indeterminate", true).prop("readonly", true);
+    $("input[type=checkbox].tristate").change(function(e){
+        tristate(e.target);
     });
 });
 

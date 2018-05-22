6e55877059e402c8ddc417a6671394315962faae
Felix Moche <felix.moche@jtl-software.com>
2018-05-22 11:39:39 +0200

    lang setter

--- includes/src/AdminSession.php
+++ includes/src/AdminSession.php
@@ -96,9 +96,11 @@ class AdminSession
             $_SESSION['jtl_token'] = generateCSRFToken();
         }
         if (!isset($_SESSION['kSprache'])) {
-            $lang                 = Shop::Container()->getDB()->select('tsprache', 'cShopStandard', 'Y');
-            $_SESSION['kSprache'] = isset($lang->kSprache) ? (int)$lang->kSprache : 1;
+            $lang                    = Shop::Container()->getDB()->select('tsprache', 'cShopStandard', 'Y');
+            $_SESSION['kSprache']    = isset($lang->kSprache) ? (int)$lang->kSprache : 1;
+            $_SESSION['cISOSprache'] = $lang->cISO ?? 'ger';
         }
+        Shop::setLanguage($_SESSION['kSprache'], $_SESSION['cISOSprache']);
         if (isset($_SESSION['Kundengruppe']) && get_class($_SESSION['Kundengruppe']) === 'stdClass') {
             $_SESSION['Kundengruppe'] = new Kundengruppe($_SESSION['Kundengruppe']->kKundengruppe);
         }

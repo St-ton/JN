274a0ed26e87cfe6350e09a5c77f4a661b69c3e4
Falk Prüfer <falk.pruefer@jtl-software.com>
2018-05-31 14:29:25 +0200

    Add console logs for debugging.

--- includes/plugins/jtl_google_recaptcha/version/100/frontend/js/jtl_google_recaptcha.js
+++ includes/plugins/jtl_google_recaptcha/version/100/frontend/js/jtl_google_recaptcha.js
@@ -20,6 +20,7 @@
         },
 
         renderCaptcha: function(parameters) {
+            console.log('reCaptcha.renderCaptcha');
             var $g_reCaptcha = $('.g-recaptcha');
 
             if ($g_reCaptcha.length > 0) {
@@ -39,6 +40,7 @@
                     $g_reCaptcha.each(function (index, item) {
                         parameters = $.extend({}, $(item).data(), parameters);
                         try {
+                            console.log('reCaptcha.render');
                             that.captcha.widget = grecaptcha.render(item, parameters);
                         } catch (e) {
                         }
@@ -52,6 +54,7 @@
 
         renderCaptchaPopup: function(parameters) {
             //the modal just copies all the html.. so we got duplicate IDs which confuses recaptcha
+            console.log('reCaptcha.renderCaptchaPopup');
             var $recaptcha = $('.tmp-modal-content .g-recaptcha');
             if ($recaptcha.length === 1) {
                 if (typeof grecaptcha === 'undefined' && !this.captcha.loaded) {
@@ -61,16 +64,18 @@
                 } else if (typeof grecaptcha !== 'undefined') {
                     var siteKey       = $recaptcha.data('sitekey'),
                         $newRecaptcha = this.captcha.size === 'invisible' ? $('<div data-size="invisible" data-badge="inline" />') : $('<div />');
-
+                    console.log('reCaptcha.renderCaptchaPopup - grecaptcha defined');
                     if (typeof siteKey !== 'undefined') {
                         //create empty recapcha div, give it a unique id and delete the old one
                         $newRecaptcha.attr('id', 'popup-recaptcha').addClass('g-recaptcha form-group');
                         $recaptcha.replaceWith($newRecaptcha);
+                        console.log('reCaptcha.render popup-recaptcha');
                         this.captcha.widget = grecaptcha.render('popup-recaptcha', {
                             'sitekey': siteKey,
                             'callback': 'g_recaptcha_filled'
                         });
 
+                        console.log('reCaptcha.newRecaptcha submit-handler');
                         $newRecaptcha.closest('form').on('submit', function (ev) {
                             $.reCaptcha.submitForm(ev);
                         });
@@ -83,7 +88,9 @@
         },
 
         submitForm: function(ev) {
+            console.log('reCaptcha.submitForm');
             if (typeof grecaptcha !== 'undefined' && this.captcha.size === 'invisible' && !$('.g-recaptcha-response').val()) {
+                console.log('reCaptcha.invisible detected');
                 ev.preventDefault();
                 this.captcha.submitted = ev.target;
                 grecaptcha.execute(this.captcha.widget);
@@ -106,18 +113,22 @@
 })(jQuery);
 
 function g_recaptcha_callback() {
+    console.log('reCaptcha.callback_normal');
     $.reCaptcha.renderCaptcha();
 }
 
 function g_recaptcha_popup_callback() {
+    console.log('reCaptcha.callback_popup');
     $.reCaptcha.renderCaptchaPopup();
 }
 
 function g_recaptcha_filled(token) {
+    console.log('reCaptcha.filled with token: ' + token);
     $('.g-recaptcha').closest('.form-group').find('div.form-error-msg').remove();
     $('.g-recaptcha-response').val(token);
     if (typeof $.reCaptcha.captcha.submitted !== 'undefined') {
-        $.reCaptcha.captcha.submitted.submit();
+        console.log('reCaptcha.submit');
+        //$.reCaptcha.captcha.submitted.submit();
     }
 }
 

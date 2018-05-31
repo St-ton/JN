8d4b135a90feac42fc3fc53091df50d5abae0b30
Felix Moche <felix.moche@jtl-software.com>
2018-05-31 16:20:32 +0200

    try to create missing link language and seo entries

--- update/migrations/20180522105000_new_linkgroup_associations.php
+++ update/migrations/20180522105000_new_linkgroup_associations.php
@@ -27,6 +27,64 @@ class Migration_20180522105000 extends Migration implements IMigration
     public function up()
     {
         $this->execute("ALTER TABLE `tlinkgruppe` CHANGE COLUMN `kLinkgruppe` `kLinkgruppe` INT NOT NULL AUTO_INCREMENT;");
+        $missingLanguageEntries = Shop::Container()->getDB()->query(
+            "SELECT tlink.*, tseo.* 
+                FROM tlink
+                    LEFT JOIN tseo
+                        ON tseo.cKey = 'kLink'
+                        AND tseo.kKey = tlink.kLink
+                WHERE kLink NOT IN (SELECT kLink FROM tlinksprache)",
+            \DB\ReturnType::ARRAY_OF_OBJECTS
+        );
+        $missingLanguageEntries = \Functional\group($missingLanguageEntries, function ($e) {
+            return $e->kLink;
+        });
+        $sprachen               = gibAlleSprachen();
+        foreach ($missingLanguageEntries as $linkID => $links) {
+            $linkData           = \Functional\first($links);
+            $linkSprache        = new stdClass();
+            $linkSprache->kLink = $linkID;
+            foreach ($sprachen as $sprache) {
+                $match = \Functional\first($links, function ($e) use ($sprache) {
+                    return (int)$e->kSprache === $sprache->kSprache;
+                });
+                if ($match === null) {
+                    // no seo entry exists for this language ID
+                    $linkSprache->cName       = $linkData->cName;
+                    $linkSprache->cSeo        = $linkData->cName;
+                } else {
+                    $linkSprache->cSeo        = $match->cSeo;
+                    $linkSprache->cName       = $match->cName;
+                }
+                $linkSprache->cISOSprache = $sprache->cISO;
+                $linkSprache->cTitle      = '';
+                $linkSprache->cContent    = '';
+                $linkSprache->cMetaTitle  = '';
+                $linkSprache->cSeo        = getSeo($linkSprache->cSeo);
+                Shop::Container()->getDB()->insert('tlinksprache', $linkSprache);
+            }
+        }
+        $missingSeo = Shop::Container()->getDB()->query(
+            "SELECT tlink.*, tlinksprache.*, tsprache.kSprache 
+                FROM tlink
+                JOIN tlinksprache
+                    ON tlinksprache.kLink = tlink.kLink
+                JOIN tsprache
+                    ON tsprache.cISO = tlinksprache.cISOSprache
+                LEFT JOIN tseo
+                    ON tseo.cKey = 'kLink'
+                    AND tseo.kKey = tlink.kLink 
+                WHERE tseo.cSeo IS NULL",
+            \DB\ReturnType::ARRAY_OF_OBJECTS
+        );
+        foreach ($missingSeo as $item) {
+            $oSeo           = new stdClass();
+            $oSeo->cSeo     = checkSeo($item->cSeo);
+            $oSeo->kKey     = $item->kLink;
+            $oSeo->cKey     = 'kLink';
+            $oSeo->kSprache = $item->kSprache;
+            Shop::Container()->getDB()->insert('tseo', $oSeo);
+        }
         $this->execute("CREATE TABLE `tlinkgroupassociations` (
               `id` INT NOT NULL AUTO_INCREMENT,
               `linkID` INT NOT NULL,
@@ -56,7 +114,11 @@ class Migration_20180522105000 extends Migration implements IMigration
             unset($duplicate->kLink);
             Shop::Container()->getDB()->insert('tlink', $duplicate);
         }
-        $this->execute("INSERT INTO tlinkgroupassociations (`linkID`, `linkGroupID`) (SELECT kLink, kLinkgruppe FROM tlink)");
+        $this->execute("INSERT INTO tlinkgroupassociations (`linkID`, `linkGroupID`) 
+            (SELECT tlink.kLink, tlink.kLinkgruppe 
+                FROM tlink 
+                JOIN tlinkgruppe
+                    ON tlinkgruppe.kLinkgruppe = tlink.kLinkgruppe)");
         $this->execute("ALTER TABLE `tlink` 
             DROP COLUMN `kLinkgruppe`,
             DROP PRIMARY KEY,

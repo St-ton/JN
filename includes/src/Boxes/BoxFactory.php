60dc3350a33dfd6d7b0978fcdf53951543ec5831
Felix Moche <felix.moche@jtl-software.com>
2018-06-04 12:12:19 +0200

    factory, service

---
+++ includes/src/Boxes/BoxFactory.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @copyright (c) JTL-Software-GmbH
+ * @license       http://jtl-url.de/jtlshoplicense
+ */
+
+namespace Boxes;
+
+/**
+ * Class BoxFactory
+ *
+ * @package Boxes
+ */
+class BoxFactory
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * BoxFactory constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @param int $baseType
+     * @return boxInterface
+     */
+    public function getBoxByBaseType(int $baseType): BoxInterface
+    {
+        switch ($baseType) {
+            case BOX_BESTSELLER:
+                return new BoxBestsellingProducts($this->config);
+            case BOX_CONTAINER:
+                return new BoxContainer($this->config);
+            case BOX_IN_KUERZE_VERFUEGBAR:
+                return new BoxUpcomingProducts($this->config);
+            case BOX_ZULETZT_ANGESEHEN:
+                return new BoxRecentlyViewedProducts($this->config);
+            case BOX_NEUE_IM_SORTIMENT:
+                return new BoxNewProducts($this->config);
+            case BOX_TOP_ANGEBOT:
+                return new BoxTopOffers($this->config);
+            case BOX_SONDERANGEBOT:
+                return new BoxSpecialOffers($this->config);
+            case BOX_LOGIN:
+                return new BoxLogin($this->config);
+            case BOX_GLOBALE_MERKMALE:
+                return new BoxGlobalAttributes($this->config);
+            case BOX_KATEGORIEN:
+                return new BoxProductCategories($this->config);
+            case BOX_NEWS_KATEGORIEN:
+                return new BoxNewsCategories($this->config);
+            case BOX_NEWS_AKTUELLER_MONAT:
+                return new BoxNewsCurrentMonth($this->config);
+            case BOX_TAGWOLKE:
+                return new BoxTagCloud($this->config);
+            case BOX_WUNSCHLISTE:
+                return new BoxWishlist($this->config);
+            case BOX_WARENKORB:
+                return new BoxCart($this->config);
+            case BOX_SCHNELLKAUF:
+                return new BoxDirectPurchase($this->config);
+            case BOX_VERGLEICHSLISTE:
+                return new BoxCompareList($this->config);
+            case BOX_EIGENE_BOX_MIT_RAHMEN:
+            case BOX_EIGENE_BOX_OHNE_RAHMEN:
+                return new BoxPlain($this->config);
+            case BOX_LINKGRUPPE:
+                return new BoxLinkGroup($this->config);
+            case BOX_UMFRAGE:
+                return new BoxPoll($this->config);
+            case BOX_PREISRADAR:
+                return new BoxPriceRadar($this->config);
+            case BOX_HERSTELLER:
+                return new BoxManufacturer($this->config);
+            case BOX_FILTER_MERKMALE:
+                return new BoxFilterAttribute($this->config);
+            case BOX_FILTER_KATEGORIE:
+                return new BoxFilterCategory($this->config);
+            case BOX_FILTER_HERSTELLER:
+                return new BoxFilterManufacturer($this->config);
+            case BOX_FILTER_TAG:
+                return new BoxFilterTag($this->config);
+            case BOX_FILTER_PREISSPANNE:
+                return new BoxFilterPricerange($this->config);
+            case BOX_FILTER_BEWERTUNG:
+                return new BoxFilterRating($this->config);
+            case BOX_FILTER_SUCHE:
+                return new BoxFilterSearch($this->config);
+            case BOX_FILTER_SUCHSPECIAL:
+                return new BoxFilterItem($this->config);
+            case BOX_TRUSTEDSHOPS_GUETESIEGEL:
+                return new BoxTrustedShopsSeal($this->config);
+            case BOX_TRUSTEDSHOPS_KUNDENBEWERTUNGEN:
+                return new BoxTrustedShopsReviews($this->config);
+            default:
+                return new BoxDefault($this->config);
+        }
+    }
+}
---
+++ includes/src/Services/JTL/BoxService.php
@@ -0,0 +1,404 @@
+<?php
+/**
+ * @copyright (c) JTL-Software-GmbH
+ * @license       http://jtl-url.de/jtlshoplicense
+ */
+
+namespace Services\JTL;
+
+use Boxes\BoxFactory;
+use Boxes\BoxInterface;
+use Boxes\BoxType;
+use Boxes\Renderer\DefaultRenderer;
+use DB\DbInterface;
+use DB\ReturnType;
+use Filter\ProductFilter;
+use Filter\ProductFilterSearchResults;
+use Filter\Visibility;
+
+/**
+ * Class BoxService
+ */
+class BoxService
+{
+    /**
+     * @var array
+     */
+    public $boxes = [];
+
+    /**
+     * @var array
+     */
+    public $boxConfig = [];
+
+    /**
+     * @var string
+     */
+    public $lagerFilter = '';
+
+    /**
+     * @var string
+     */
+    public $cVaterSQL = ' AND tartikel.kVaterArtikel = 0';
+
+    /**
+     * unrendered box template file name + data
+     *
+     * @var array
+     */
+    public $rawData = [];
+
+    /**
+     * @var array
+     */
+    public $visibility;
+
+    /**
+     * @var BoxService
+     */
+    private static $_instance;
+
+    /**
+     * @var \Boxes\BoxFactory
+     */
+    private $boxFactory;
+
+    /**
+     * @var DbInterface
+     */
+    private $db;
+
+    /**
+     * @param array       $config
+     * @param BoxFactory  $factory
+     * @param DbInterface $db
+     * @return BoxService
+     */
+    public static function getInstance(array $config, BoxFactory $factory, DbInterface $db): self
+    {
+        return self::$_instance ?? new self($config, $factory, $db);
+    }
+
+    /**
+     * BoxService constructor.
+     *
+     * @param array       $config
+     * @param BoxFactory  $factory
+     * @param DbInterface $db
+     */
+    public function __construct(array $config, BoxFactory $factory, DbInterface $db)
+    {
+        $this->boxConfig  = $config;
+        $this->boxFactory = $factory;
+        $this->db         = $db;
+        self::$_instance  = $this;
+    }
+
+    /**
+     * @param int $kArtikel
+     * @param int $nMaxAnzahl
+     */
+    public function addRecentlyViewed(int $kArtikel, $nMaxAnzahl = null)
+    {
+        if ($kArtikel <= 0) {
+            return;
+        }
+        if ($nMaxAnzahl === null) {
+            $nMaxAnzahl = (int)$this->boxConfig['boxen']['box_zuletztangesehen_anzahl'];
+        }
+        if (!isset($_SESSION['ZuletztBesuchteArtikel']) || !is_array($_SESSION['ZuletztBesuchteArtikel'])) {
+            $_SESSION['ZuletztBesuchteArtikel'] = [];
+        }
+        $oArtikel           = new \stdClass();
+        $oArtikel->kArtikel = $kArtikel;
+        if (isset($_SESSION['ZuletztBesuchteArtikel']) && count($_SESSION['ZuletztBesuchteArtikel']) > 0) {
+            $alreadyPresent = false;
+            foreach ($_SESSION['ZuletztBesuchteArtikel'] as $_article) {
+                if (isset($_article->kArtikel) && $_article->kArtikel === $oArtikel->kArtikel) {
+                    $alreadyPresent = true;
+                    break;
+                }
+            }
+            if ($alreadyPresent === false) {
+                if (count($_SESSION['ZuletztBesuchteArtikel']) < $nMaxAnzahl) {
+                    $_SESSION['ZuletztBesuchteArtikel'][] = $oArtikel;
+                } else {
+                    $oTMP_arr = array_reverse($_SESSION['ZuletztBesuchteArtikel']);
+                    array_pop($oTMP_arr);
+                    $oTMP_arr                           = array_reverse($oTMP_arr);
+                    $oTMP_arr[]                         = $oArtikel;
+                    $_SESSION['ZuletztBesuchteArtikel'] = $oTMP_arr;
+                }
+            }
+        } else {
+            $_SESSION['ZuletztBesuchteArtikel'][] = $oArtikel;
+        }
+        executeHook(HOOK_ARTIKEL_INC_ZULETZTANGESEHEN);
+    }
+
+    /**
+     * @param int  $nSeite
+     * @param bool $bGlobal
+     * @return array|bool
+     */
+    public function holeBoxAnzeige(int $nSeite, bool $bGlobal = true)
+    {
+        if ($this->visibility !== null) {
+            return $this->visibility;
+        }
+        $oBoxAnzeige = [];
+        $oBox_arr    = $this->db->selectAll('tboxenanzeige', 'nSeite', $nSeite);
+        if (is_array($oBox_arr) && count($oBox_arr)) {
+            foreach ($oBox_arr as $oBox) {
+                $oBoxAnzeige[$oBox->ePosition] = (boolean)$oBox->bAnzeigen;
+            }
+            $this->visibility = $oBoxAnzeige;
+
+            return $oBoxAnzeige;
+        }
+
+        return $nSeite !== 0 && $bGlobal
+            ? $this->holeBoxAnzeige(0)
+            : false;
+    }
+
+    /**
+     * @param int          $kBox
+     * @param int          $kSeite
+     * @param string|array $cFilter
+     * @return int
+     */
+    public function filterBoxVisibility(int $kBox, int $kSeite, $cFilter = ''): int
+    {
+        if (is_array($cFilter)) {
+            $cFilter = array_unique($cFilter);
+            $cFilter = implode(',', $cFilter);
+        }
+        $_upd          = new \stdClass();
+        $_upd->cFilter = $cFilter;
+
+        return $this->db->update('tboxensichtbar', ['kBox', 'kSeite'], [$kBox, $kSeite], $_upd);
+    }
+
+    /**
+     * @param ProductFilter              $pf
+     * @param ProductFilterSearchResults $sr
+     * @return bool
+     */
+    public function gibBoxenFilterNach(ProductFilter $pf, ProductFilterSearchResults $sr): bool
+    {
+        $cf  = $pf->getCategoryFilter();
+        $mf  = $pf->getManufacturerFilter();
+        $prf = $pf->getPriceRangeFilter();
+        $rf  = $pf->getRatingFilter();
+        $tf  = $pf->tagFilterCompat;
+        $afc = $pf->getAttributeFilterCollection();
+        $ssf = $pf->getSearchSpecialFilter();
+        $sf  = $pf->searchFilterCompat;
+
+        $invis      = Visibility::SHOW_NEVER();
+        $visContent = Visibility::SHOW_CONTENT();
+
+        return ((!$cf->getVisibility()->equals($invis) && !$cf->getVisibility()->equals($visContent))
+            || (!$mf->getVisibility()->equals($invis) && !$mf->getVisibility()->equals($visContent))
+            || (!$prf->getVisibility()->equals($invis) && !$prf->getVisibility()->equals($visContent))
+            || (!$rf->getVisibility()->equals($invis) && !$rf->getVisibility()->equals($visContent))
+            || (!$tf->getVisibility()->equals($invis) && !$tf->getVisibility()->equals($visContent))
+            || (!$afc->getVisibility()->equals($invis) && !$afc->getVisibility()->equals($visContent))
+            || (!$ssf->getVisibility()->equals($invis) && !$ssf->getVisibility()->equals($visContent))
+            || (!$sf->getVisibility()->equals($invis) && !$sf->getVisibility()->equals($visContent))
+        );
+    }
+
+    /**
+     * get raw data from visible boxes
+     * to allow custom renderes
+     *
+     * @return array
+     */
+    public function getRawData(): array
+    {
+        return $this->rawData;
+    }
+
+    /**
+     * compatibility layer for gibBoxen() which returns unrendered content
+     *
+     * @return array
+     */
+    public function compatGet(): array
+    {
+        $boxes = [];
+        foreach ($this->rawData as $_type => $_boxes) {
+            $boxes[$_type] = [];
+            foreach ($_boxes as $_box) {
+                $boxes[$_type][] = $_box['obj'];
+            }
+        }
+
+        return $boxes;
+    }
+
+    /**
+     * @param array $positionedBoxes
+     * @return array
+     * @throws \Exception
+     * @throws \SmartyException
+     */
+    public function render(array $positionedBoxes): array
+    {
+        $smarty   = \Shop::Smarty();
+        $pageType = \Shop::getPageType();
+        $pageID   = 0;
+        if ($pageType === PAGE_ARTIKELLISTE) {
+            $pageID = (int)\Shop::$kKategorie;
+        } elseif ($pageType === PAGE_ARTIKEL) {
+            $pageID = (int)\Shop::$kArtikel;
+        } elseif ($pageType === PAGE_EIGENE) {
+            $pageID = (int)\Shop::$kLink;
+        } elseif ($pageType === PAGE_HERSTELLER) {
+            $pageID = (int)\Shop::$kHersteller;
+        }
+        $originalArticle   = $smarty->getTemplateVars('Artikel');
+        $productFilter     = \Shop::getProductFilter();
+        $filterAfter       = !empty($this->boxConfig)
+            ? $this->gibBoxenFilterNach($productFilter, $productFilter->getSearchResults(false))
+            : 0;
+        $this->lagerFilter = $productFilter->getFilterSQL()->getStockFilterSQL();
+        $htmlArray         = [
+            'top'    => null,
+            'right'  => null,
+            'bottom' => null,
+            'left'   => null
+        ];
+        $smarty->assign('BoxenEinstellungen', $this->boxConfig)
+               ->assign('bBoxenFilterNach', $filterAfter)
+               ->assign('NettoPreise', \Session::CustomerGroup()->getIsMerchant());
+
+        $boxRenderer = new DefaultRenderer($smarty);
+        foreach ($positionedBoxes as $_position => $_boxes) {
+            if (!is_array($_boxes)) {
+                $_boxes = [];
+            }
+            $htmlArray[$_position]     = '';
+            $this->rawData[$_position] = [];
+            foreach ($_boxes as $_box) {
+                /** @var BoxInterface $_box */
+                $renderClass = $_box->getRenderer();
+                if ($renderClass !== get_class($boxRenderer)) {
+                    $boxRenderer = new $renderClass($smarty);
+                }
+                $boxRenderer->setBox($_box);
+                $htmlArray[$_position]       .= trim($boxRenderer->render($pageType, $pageID));
+                $this->rawData[$_position][] = [
+                    'obj' => $_box,
+                    'tpl' => $_box->getTemplateFile()
+                ];
+            }
+        }
+        // avoid modification of article object on render loop
+        $smarty->assign('Artikel', $originalArticle);
+
+        return $htmlArray;
+    }
+
+    /**
+     * @param int  $nSeite
+     * @param bool $bAktiv
+     * @param bool $bVisible
+     * @return array
+     */
+    public function getBoxes(int $nSeite = 0, bool $bAktiv = true, bool $bVisible = false): array
+    {
+        $cacheID           = 'bx_' . $nSeite .
+            '_' . (($bAktiv === true) ? '1' : '0') .
+            '_' . (($bVisible === true) ? '1' : '0') .
+            '_' . \Shop::getLanguageID();
+        $this->visibility  = $this->holeBoxAnzeige($nSeite);
+        $template          = \Template::getInstance();
+        $templatePositions = $template->getBoxLayoutXML();
+        $visiblePositions  = [];
+        foreach ($this->visibility as $position => $isVisible) {
+            if (isset($templatePositions[$position])) {
+                $isVisible = $isVisible && $templatePositions[$position];
+            }
+            if ($isVisible) {
+                $visiblePositions[] = $position;
+            }
+        }
+        $visiblePositions = \Functional\map($visiblePositions, function ($e) {
+            return "'" . $e . "'";
+        });
+        $cacheTags        = [CACHING_GROUP_OBJECT, CACHING_GROUP_BOX, 'boxes'];
+        $cSQLAktiv        = $bAktiv
+            ? ' AND bAktiv = 1 AND tboxen.ePosition IN (' . implode(',', $visiblePositions) . ')'
+            : '';
+        $cPluginAktiv     = $bAktiv
+            ? " AND (tplugin.nStatus IS NULL OR tplugin.nStatus = 2  OR tboxvorlage.eTyp != 'plugin')"
+            : '';
+        if (($grouped = \Shop::Cache()->get($cacheID)) === false) {
+            $boxData = $this->db->query(
+                'SELECT tboxen.kBox, tboxen.kBoxvorlage, tboxen.kCustomID, tboxen.kContainer, 
+                       tboxen.cTitel, tboxen.ePosition, tboxensichtbar.kSeite, tboxensichtbar.nSort, 
+                       tboxensichtbar.bAktiv, tboxensichtbar.cFilter, tboxvorlage.eTyp, 
+                       tboxvorlage.cName, tboxvorlage.cTemplate, tplugin.nStatus AS pluginStatus,
+                       GROUP_CONCAT(tboxensichtbar.kSeite) AS visiblePages,
+                       tsprache.kSprache, tboxsprache.cInhalt, tboxsprache.cTitel
+                    FROM tboxen
+                    LEFT JOIN tboxensichtbar
+                        ON tboxen.kBox = tboxensichtbar.kBox
+                    LEFT JOIN tplugin
+                        ON tboxen.kCustomID = tplugin.kPlugin
+                    LEFT JOIN tboxvorlage
+                        ON tboxen.kBoxvorlage = tboxvorlage.kBoxvorlage
+                    LEFT JOIN tboxsprache
+                        ON tboxsprache.kBox = tboxen.kBox
+                    LEFT JOIN tsprache
+                        ON tsprache.cISO = tboxsprache.cISO
+                    WHERE tboxen.kContainer > -1 ' . $cSQLAktiv . $cPluginAktiv .
+                ' GROUP BY tboxsprache.kBoxSprache, tboxen.kBox, tboxensichtbar.cFilter
+                    ORDER BY tboxensichtbar.nSort, tboxen.kBox ASC',
+                ReturnType::ARRAY_OF_OBJECTS
+            );
+            $grouped = \Functional\group($boxData, function ($e) {
+                return $e->kBox;
+            });
+            \Shop::Cache()->set($cacheID, $grouped, array_unique($cacheTags));
+        }
+        $children = [];
+        foreach ($grouped as $i => $boxes) {
+            if (!is_array($boxes)) {
+                continue;
+            }
+            $first = \Functional\first($boxes);
+            if ((int)$first->kContainer > 0) {
+                $boxInstance = $this->boxFactory->getBoxByBaseType($first->kBoxvorlage);
+                $boxInstance->map($boxes);
+                if (!isset($children[(int)$first->kContainer])) {
+                    $children[(int)$first->kContainer] = [];
+                }
+                $children[(int)$first->kContainer][] = $boxInstance;
+                unset($grouped[$i]);
+            }
+        }
+        $result = [];
+        foreach ($grouped as $boxes) {
+            if (!is_array($boxes)) {
+                continue;
+            }
+            $first       = \Functional\first($boxes);
+            $boxInstance = $this->boxFactory->getBoxByBaseType($first->kBoxvorlage);
+            $boxInstance->map($boxes);
+            if ($boxInstance->getType() === BoxType::CONTAINER) {
+                $boxInstance->setChildren($children);
+            }
+            $result[] = $boxInstance;
+        }
+        $byPosition  = \Functional\group($result, function (BoxInterface $e) {
+            return $e->getPosition();
+        });
+        $this->boxes = $byPosition; // shop 4.x compatability only
+
+        return $byPosition;
+    }
+}

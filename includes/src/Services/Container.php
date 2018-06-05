8172e530b7241d8e03872dd192b1f934d8642bf6
Felix Moche <felix.moche@jtl-software.com>
2018-06-05 10:38:24 +0200

    interface

--- includes/src/Services/Container.php
+++ includes/src/Services/Container.php
@@ -11,6 +11,7 @@ use DB\DbInterface;
 use DB\Services\GcServiceInterface;
 use Services\JTL\LinkService;
 use Services\JTL\CryptoServiceInterface;
+use Services\JTL\LinkServiceInterface;
 use Services\JTL\PasswordServiceInterface;
 use Psr\Log\LoggerInterface;
 
@@ -75,8 +76,8 @@ class Container extends ContainerBase implements DefaultServicesInterface
     /**
      * @inheritdoc
      */
-    public function getLinkService(): LinkService
+    public function getLinkService(): LinkServiceInterface
     {
-        return $this->get(LinkService::class);
+        return $this->get(LinkServiceInterface::class);
     }
 }

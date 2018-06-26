1d46d84c04378e3792063d1dac03a0e91b3e9ed5
Felix Moche <felix.moche@jtl-software.com>
2018-06-26 12:58:44 +0200

    typo

--- admin/io.php
+++ admin/io.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/includes/admininclude.php';
 if (!$oAccount->getIsAuthenticated()) {
     AdminIO::getInstance()->respondAndExit(new IOError('Not authenticated as admin.', 401));
 }
-if (!FormHelper::validateToken()()) {
+if (!FormHelper::validateToken()) {
     AdminIO::getInstance()->respondAndExit(new IOError('CSRF validation failed.', 403));
 }
 

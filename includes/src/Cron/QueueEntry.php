d27531026a0da8dca1dcb7ad290447fb8fbb0b02
Felix Moche <felix.moche@jtl-software.com>
2018-07-26 15:38:21 +0200

    phpdoc, cs

--- includes/src/Cron/QueueEntry.php
+++ includes/src/Cron/QueueEntry.php
@@ -6,65 +6,89 @@
 
 namespace Cron;
 
-
+/**
+ * Class QueueEntry
+ * @package Cron
+ */
 class QueueEntry
 {
-
+    /**
+     * @var int
+     */
     public $kJobQueue;
 
+    /**
+     * @var int
+     */
     public $kCron;
 
+    /**
+     * @var int
+     */
     public $kKey;
 
+    /**
+     * @var int
+     */
     public $nLimitN;
 
+    /**
+     * @var int
+     */
     public $nLimitM;
 
+    /**
+     * @var int
+     */
     public $nLastArticleID;
 
+    /**
+     * @var int
+     */
     public $nInArbeit;
 
+    /**
+     * @var string
+     */
     public $cJobArt;
 
+    /**
+     * @var string
+     */
     public $cTabelle;
 
+    /**
+     * @var string
+     */
     public $cKey;
 
+    /**
+     * @var \DateTime
+     */
     public $dStartZeit;
 
+    /**
+     * @var \DateTime
+     */
     public $dZuletztGelaufen;
 
-    public function __construct($data) {
-        $this->kJobQueue = (int)$data->kJobQueue;
+    /**
+     * QueueEntry constructor.
+     * @param $data
+     */
+    public function __construct($data)
+    {
+        $this->kJobQueue        = (int)$data->kJobQueue;
         $this->kCron            = (int)$data->kCron;
         $this->kKey             = (int)$data->kKey;
         $this->nLimitN          = (int)$data->nLimitN;
         $this->nLimitM          = (int)$data->nLimitM;
-        $this->nLastArticleID   = (int)$data->nLastArticleID;;
-
+        $this->nLastArticleID   = (int)$data->nLastArticleID;
         $this->nInArbeit        = 0;
-
         $this->cJobArt          = $data->cJobArt;
         $this->cTabelle         = $data->cTabelle;
         $this->cKey             = $data->cKey;
         $this->dStartZeit       = new \DateTime($data->dStartZeit);
         $this->dZuletztGelaufen = new \DateTime($data->dZuletztGelaufen);
     }
-
-//    public function __construct(JobInterface $job) {
-//        $this->kCron            = $job->getID();
-//        $this->kKey             = $job->getForeignKeyID();
-//        $this->nLimitN          = $job->getExecuted();
-//        $this->nLimitM          = $job->getLimit();
-//        $this->nLastArticleID   = 0;
-//
-//        $this->nInArbeit        = 0;
-//        $this->kJobQueue        = $job->getQueueID();
-//
-//        $this->cJobArt          = $job->getType();
-//        $this->cTabelle         = $job->getTable();
-//        $this->cKey             = $job->getForeignKey();
-//        $this->dStartZeit       = $job->getDateLastStarted();
-//        $this->dZuletztGelaufen = new \DateTime();
-//    }
 }

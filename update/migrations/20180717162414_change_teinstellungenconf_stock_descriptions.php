b558c889a1838f31575d840fd1d879a623317d6a
Michael Hillmann <michael.hillmann@jtl-software.com>
2018-08-06 12:16:17 +0200

    unify namings

--- update/migrations/20180717162414_change_teinstellungenconf_stock_descriptions.php
+++ update/migrations/20180717162414_change_teinstellungenconf_stock_descriptions.php
@@ -29,16 +29,22 @@ class Migration_20180717162414 extends Migration implements IMigration
     {
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='So wird der Lagerbestand eines Artikels angezeigt. (Einzelansicht)' WHERE kEinstellungenConf = 110");
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='So wird der Lagerbestand eines Artikels angezeigt. (z.B. in Suche oder Kategorieübersicht)' WHERE kEinstellungenConf = 118");
-        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Wenn der Lagerbestand kleiner 0 sein darf (Überverkauf möglich), soll Ampel grün sein?' WHERE kEinstellungenConf = 200");
-        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Ab diesem Lagerbestand steht die Lagerampel auf grün. Zwischen diesem Wert und Lagerbestandsampel rot steht die Ampel auf gelb.' WHERE kEinstellungenConf = 112");
-        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Bis zu diesem Lagerbestand steht die Lagerampel auf rot. Zwischen diesem Wert und Lagerbestandsampel grün steht die Ampel auf gelb.' WHERE kEinstellungenConf = 111");
+        $this->execute("UPDATE teinstellungenconf 
+                          SET cBeschreibung='Wenn der Lagerbestand kleiner 0 sein darf (Überverkauf möglich), soll Lagerbestandsampel grün sein?',
+                              cName='Lagerbestandsampel grün Sonderbedingung'
+                          WHERE kEinstellungenConf = 200");
+        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Ab diesem Lagerbestand steht die Lagerbestandsampel auf grün. Zwischen diesem Wert und Lagerbestandsampel rot, steht die Lagerbestandsampel auf gelb.' WHERE kEinstellungenConf = 112");
+        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Bis zu diesem Lagerbestand steht die Lagerbestandsampel auf rot. Zwischen diesem Wert und Lagerbestandsampel grün, steht die Lagerbestandsampel auf gelb.' WHERE kEinstellungenConf = 111");
     }
 
     public function down()
     {
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='So wird der Lagerbestand eines Artikels angezeigt' WHERE kEinstellungenConf = 110");
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='So wird der Lagerbestand eines Artikels angezeigt' WHERE kEinstellungenConf = 118");
-        $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Wenn der Lagerbestand kleiner 0 sein darf, soll Ampel grün sein?' WHERE kEinstellungenConf = 200");
+        $this->execute("UPDATE teinstellungenconf 
+                          SET cBeschreibung='Wenn der Lagerbestand kleiner 0 sein darf, soll Ampel grün sein?',
+                              cName='Lagerampel grün Sonderbedingung'
+                          WHERE kEinstellungenConf = 200");
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Ab diesem Lagerbestand steht die Lagerampel auf grün. Zwischen diesem Wert und Lagerampel grün steht die Ampel auf gelb.' WHERE kEinstellungenConf = 112");
         $this->execute("UPDATE teinstellungenconf SET cBeschreibung='Bis zu diesem Lagerbestand steht die Lagerampel auf rot. Zwischen diesem Wert und Lagerampel grün steht die Ampel auf gelb.' WHERE kEinstellungenConf = 111");
     }

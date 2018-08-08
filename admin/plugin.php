80eaf8cb23eb1fc6b5a79c9d307de2a8047cf8c4
Felix Moche <felix.moche@jtl-software.com>
2018-08-08 10:05:59 +0200

    cs

--- admin/plugin.php
+++ admin/plugin.php
@@ -10,206 +10,200 @@ $oAccount->permission('PLUGIN_ADMIN_VIEW', true, true);
 require_once PFAD_ROOT . PFAD_ADMIN . PFAD_INCLUDES . 'plugin_inc.php';
 require_once PFAD_ROOT . PFAD_ADMIN . PFAD_INCLUDES . 'toolsajax_inc.php';
 
-$cHinweis           = '';
-$cFehler            = '';
-$step               = 'plugin_uebersicht';
-$customPluginTabs   = [];
-$invalidateCache    = false;
-$pluginTemplateFile = 'plugin.tpl';
-$bError             = false;
-$updated            = false;
-if ($step === 'plugin_uebersicht') {
-    $kPlugin = RequestHelper::verifyGPCDataInt('kPlugin');
-    if ($kPlugin > 0) {
-        // Ein Settinglink wurde submitted
-        if (RequestHelper::verifyGPCDataInt('Setting') === 1) {
-            $updated = true;
-            if (!FormHelper::validateToken()) {
-                $bError = true;
-            } else {
-                $oPluginEinstellungConf_arr = isset($_POST['kPluginAdminMenu'])
-                    ? Shop::Container()->getDB()->queryPrepared(
-                        "SELECT *
-                            FROM tplugineinstellungenconf
-                            WHERE kPluginAdminMenu != 0
-                                AND kPlugin = :plgn
-                                AND cConf != 'N'
-                                AND kPluginAdminMenu = :kpm",
-                        ['plgn' => $kPlugin, 'kpm' => (int)$_POST['kPluginAdminMenu']],
-                        \DB\ReturnType::ARRAY_OF_OBJECTS
-                    )
-                    : [];
-                foreach ($oPluginEinstellungConf_arr as $oPluginEinstellungConf) {
-                    Shop::Container()->getDB()->delete(
-                        'tplugineinstellungen',
-                        ['kPlugin', 'cName'],
-                        [$kPlugin, $oPluginEinstellungConf->cWertName]
-                    );
-                    $oPluginEinstellung          = new stdClass();
-                    $oPluginEinstellung->kPlugin = $kPlugin;
-                    $oPluginEinstellung->cName   = $oPluginEinstellungConf->cWertName;
-                    if (isset($_POST[$oPluginEinstellungConf->cWertName])) {
-                        if (is_array($_POST[$oPluginEinstellungConf->cWertName])) {
-                            if ($oPluginEinstellungConf->cConf === 'M') {
-                                //selectbox with "multiple" attribute
-                                $oPluginEinstellung->cWert = serialize($_POST[$oPluginEinstellungConf->cWertName]);
-                            } else {
-                                //radio buttons
-                                $oPluginEinstellung->cWert = $_POST[$oPluginEinstellungConf->cWertName][0];
-                            }
+$cHinweis        = '';
+$cFehler         = '';
+$step            = 'plugin_uebersicht';
+$customTabs      = [];
+$invalidateCache = false;
+$bError          = false;
+$updated         = false;
+$kPlugin         = RequestHelper::verifyGPCDataInt('kPlugin');
+if ($step === 'plugin_uebersicht' && $kPlugin > 0) {
+    if (RequestHelper::verifyGPCDataInt('Setting') === 1) {
+        $updated = true;
+        if (!FormHelper::validateToken()) {
+            $bError = true;
+        } else {
+            $oPluginEinstellungConf_arr = isset($_POST['kPluginAdminMenu'])
+                ? Shop::Container()->getDB()->queryPrepared(
+                    "SELECT *
+                        FROM tplugineinstellungenconf
+                        WHERE kPluginAdminMenu != 0
+                            AND kPlugin = :plgn
+                            AND cConf != 'N'
+                            AND kPluginAdminMenu = :kpm",
+                    ['plgn' => $kPlugin, 'kpm' => (int)$_POST['kPluginAdminMenu']],
+                    \DB\ReturnType::ARRAY_OF_OBJECTS
+                )
+                : [];
+            foreach ($oPluginEinstellungConf_arr as $oPluginEinstellungConf) {
+                Shop::Container()->getDB()->delete(
+                    'tplugineinstellungen',
+                    ['kPlugin', 'cName'],
+                    [$kPlugin, $oPluginEinstellungConf->cWertName]
+                );
+                $oPluginEinstellung          = new stdClass();
+                $oPluginEinstellung->kPlugin = $kPlugin;
+                $oPluginEinstellung->cName   = $oPluginEinstellungConf->cWertName;
+                if (isset($_POST[$oPluginEinstellungConf->cWertName])) {
+                    if (is_array($_POST[$oPluginEinstellungConf->cWertName])) {
+                        if ($oPluginEinstellungConf->cConf === 'M') {
+                            //selectbox with "multiple" attribute
+                            $oPluginEinstellung->cWert = serialize($_POST[$oPluginEinstellungConf->cWertName]);
                         } else {
-                            //textarea/text
-                            $oPluginEinstellung->cWert = $_POST[$oPluginEinstellungConf->cWertName];
+                            //radio buttons
+                            $oPluginEinstellung->cWert = $_POST[$oPluginEinstellungConf->cWertName][0];
                         }
                     } else {
-                        //checkboxes that are not checked
-                        $oPluginEinstellung->cWert = null;
+                        //textarea/text
+                        $oPluginEinstellung->cWert = $_POST[$oPluginEinstellungConf->cWertName];
                     }
-                    $kKey = Shop::Container()->getDB()->insert('tplugineinstellungen', $oPluginEinstellung);
-
-                    if (!$kKey) {
-                        $bError = true;
-                    }
-                    $invalidateCache = true;
+                } else {
+                    //checkboxes that are not checked
+                    $oPluginEinstellung->cWert = null;
                 }
-            }
-            if ($bError) {
-                $cFehler = 'Fehler: Ihre Einstellungen konnten nicht gespeichert werden.';
-            } else {
-                $cHinweis = 'Ihre Einstellungen wurden erfolgreich gespeichert';
-            }
-        }
-        if (RequestHelper::verifyGPCDataInt('kPluginAdminMenu') > 0) {
-            $smarty->assign('defaultTabbertab', RequestHelper::verifyGPCDataInt('kPluginAdminMenu'));
-        }
-        if (strlen(RequestHelper::verifyGPDataString('cPluginTab')) > 0) {
-            $smarty->assign('defaultTabbertab', RequestHelper::verifyGPDataString('cPluginTab'));
-        }
+                $kKey = Shop::Container()->getDB()->insert('tplugineinstellungen', $oPluginEinstellung);
 
-        $oPlugin = new Plugin($kPlugin, $invalidateCache);
-        if (!$invalidateCache) { //make sure dynamic options are reloaded
-            foreach ($oPlugin->oPluginEinstellungConf_arr as $option) {
-                if (!empty($option->cSourceFile)) {
-                    $option->oPluginEinstellungenConfWerte_arr = $oPlugin->getDynamicOptions($option);
+                if (!$kKey) {
+                    $bError = true;
                 }
+                $invalidateCache = true;
             }
         }
-        $smarty->assign('oPlugin', $oPlugin);
-        if ($updated === true) {
-            executeHook(HOOK_PLUGIN_SAVE_OPTIONS, [
-                'hasError' => &$bError,
-                'msg'      => &$cHinweis,
-                'error'    => $cFehler,
-                'options'  => $oPlugin->oPluginEinstellungAssoc_arr
-            ]);
+        if ($bError) {
+            $cFehler = 'Fehler: Ihre Einstellungen konnten nicht gespeichert werden.';
+        } else {
+            $cHinweis = 'Ihre Einstellungen wurden erfolgreich gespeichert';
         }
-        $i            = 0;
-        $j            = 0;
-        $fAddAsDocTab = false;
-        $szReadmeFile = PFAD_ROOT . PFAD_PLUGIN . $oPlugin->cVerzeichnis . '/README.md';
-        if ('' !== $oPlugin->cTextReadmePath) {
-            $szReadmeContent = StringHandler::convertUTF8(file_get_contents($szReadmeFile));
-            $fMarkDown       = false;
-            if (class_exists('Parsedown')) {
-                $fMarkDown       = true;
-                $oParseDown      = new Parsedown();
-                $szReadmeContent = $oParseDown->text($szReadmeContent);
+    }
+    if (RequestHelper::verifyGPCDataInt('kPluginAdminMenu') > 0) {
+        $smarty->assign('defaultTabbertab', RequestHelper::verifyGPCDataInt('kPluginAdminMenu'));
+    }
+    if (strlen(RequestHelper::verifyGPDataString('cPluginTab')) > 0) {
+        $smarty->assign('defaultTabbertab', RequestHelper::verifyGPDataString('cPluginTab'));
+    }
+    $oPlugin = new Plugin($kPlugin, $invalidateCache);
+    if (!$invalidateCache) { //make sure dynamic options are reloaded
+        foreach ($oPlugin->oPluginEinstellungConf_arr as $option) {
+            if (!empty($option->cSourceFile)) {
+                $option->oPluginEinstellungenConfWerte_arr = $oPlugin->getDynamicOptions($option);
             }
-            $smarty->assign('fMarkDown', $fMarkDown)
-                   ->assign('szReadmeContent', $szReadmeContent);
-
-            $oUnnamedTab                     = new stdClass();
-            $oUnnamedTab->kPluginAdminMenu   = count($oPlugin->oPluginAdminMenu_arr) + 1;
-            $oUnnamedTab->kPlugin            = $oPlugin->kPlugin;
-            $oUnnamedTab->cName              = 'Dokumentation';
-            $oUnnamedTab->cDateiname         = '';
-            $oUnnamedTab->nSort              = count($oPlugin->oPluginAdminMenu_arr) + 1;
-            $oUnnamedTab->nConf              = 0;
-            $oPlugin->oPluginAdminMenu_arr[] = $oUnnamedTab;
-
-            $fAddAsDocTab = true;
         }
-        $fAddAsLicenseTab = false;
-        if ('' !== $oPlugin->cTextLicensePath) {
-            $szLicenseContent = StringHandler::convertUTF8(file_get_contents($oPlugin->cTextLicensePath));
-            $fMarkDown        = false;
-            if (class_exists('Parsedown')) {
-                $fMarkDown        = true;
-                $oParseDown       = new Parsedown();
-                $szLicenseContent = $oParseDown->text($szLicenseContent);
-            }
-            $smarty->assign('fMarkDown', $fMarkDown)
-                   ->assign('szLicenseContent', $szLicenseContent);
-
-            $oUnnamedTab                     = new stdClass();
-            $oUnnamedTab->kPluginAdminMenu   = count($oPlugin->oPluginAdminMenu_arr) + 1;
-            $oUnnamedTab->kPlugin            = $oPlugin->kPlugin;
-            $oUnnamedTab->cName              = 'Lizenzvereinbarung';
-            $oUnnamedTab->cDateiname         = '';
-            $oUnnamedTab->nSort              = count($oPlugin->oPluginAdminMenu_arr) + 1;
-            $oUnnamedTab->nConf              = 0;
-            $oPlugin->oPluginAdminMenu_arr[] = $oUnnamedTab;
-
-            $fAddAsLicenseTab = true;
+    }
+    $smarty->assign('oPlugin', $oPlugin);
+    if ($updated === true) {
+        executeHook(HOOK_PLUGIN_SAVE_OPTIONS, [
+            'hasError' => &$bError,
+            'msg'      => &$cHinweis,
+            'error'    => $cFehler,
+            'options'  => $oPlugin->oPluginEinstellungAssoc_arr
+        ]);
+    }
+    $i            = 0;
+    $j            = 0;
+    $fAddAsDocTab = false;
+    $szReadmeFile = PFAD_ROOT . PFAD_PLUGIN . $oPlugin->cVerzeichnis . '/README.md';
+    if ('' !== $oPlugin->cTextReadmePath) {
+        $szReadmeContent = StringHandler::convertUTF8(file_get_contents($szReadmeFile));
+        $fMarkDown       = false;
+        if (class_exists('Parsedown')) {
+            $fMarkDown       = true;
+            $oParseDown      = new Parsedown();
+            $szReadmeContent = $oParseDown->text($szReadmeContent);
         }
-        // build the the tabs
-        foreach ($oPlugin->oPluginAdminMenu_arr as $_adminMenu) {
-            if ((int)$_adminMenu->nConf === 0 && $_adminMenu->cDateiname !== ''
-                && file_exists($oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname)
-            ) {
-                ob_start();
-                require $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
+        $smarty->assign('fMarkDown', $fMarkDown)
+               ->assign('szReadmeContent', $szReadmeContent);
 
-                $tab                   = new stdClass();
-                $tab->file             = $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
-                $tab->idx              = $i;
-                $tab->id               = str_replace('.php', '', $_adminMenu->cDateiname);
-                $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
-                $tab->cName            = $_adminMenu->cName;
-                $tab->html             = ob_get_contents();
-                $customPluginTabs[]    = $tab;
-                ob_end_clean();
-                ++$i;
-            } elseif ((int)$_adminMenu->nConf === 1) {
-                $smarty->assign('oPluginAdminMenu', $_adminMenu);
-                $tab                   = new stdClass();
-                $tab->file             = $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
-                $tab->idx              = $i;
-                $tab->id               = 'settings-' . $j;
-                $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
-                $tab->cName            = $_adminMenu->cName;
-                $tab->html             = $smarty->fetch('tpl_inc/plugin_options.tpl');
-                $customPluginTabs[]    = $tab;
-                ++$j;
-            } elseif (true === $fAddAsDocTab) {
-                $tab                   = new stdClass();
-                $tab->file             = '';
-                $tab->idx              = $i;
-                $tab->id               = 'addon-' . $j;
-                $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
-                $tab->cName            = $_adminMenu->cName;
-                $tab->html             = $smarty->fetch('tpl_inc/plugin_documentation.tpl');
-                $customPluginTabs[]    = $tab;
-                ++$j;
-                $fAddAsDocTab = false; // prevent another appending!
-            } elseif (true === $fAddAsLicenseTab) {
-                $tab                   = new stdClass();
-                $tab->file             = '';
-                $tab->idx              = $i;
-                $tab->id               = 'addon-' . $j;
-                $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
-                $tab->cName            = $_adminMenu->cName;
-                $tab->html             = $smarty->fetch('tpl_inc/plugin_license.tpl');
-                $customPluginTabs[]    = $tab;
-                ++$j;
-                $fAddAsLicenseTab = false; // prevent another appending!
-            }
+        $oUnnamedTab                     = new stdClass();
+        $oUnnamedTab->kPluginAdminMenu   = count($oPlugin->oPluginAdminMenu_arr) + 1;
+        $oUnnamedTab->kPlugin            = $oPlugin->kPlugin;
+        $oUnnamedTab->cName              = 'Dokumentation';
+        $oUnnamedTab->cDateiname         = '';
+        $oUnnamedTab->nSort              = count($oPlugin->oPluginAdminMenu_arr) + 1;
+        $oUnnamedTab->nConf              = 0;
+        $oPlugin->oPluginAdminMenu_arr[] = $oUnnamedTab;
+
+        $fAddAsDocTab = true;
+    }
+    $fAddAsLicenseTab = false;
+    if ('' !== $oPlugin->cTextLicensePath) {
+        $szLicenseContent = StringHandler::convertUTF8(file_get_contents($oPlugin->cTextLicensePath));
+        $fMarkDown        = false;
+        if (class_exists('Parsedown')) {
+            $fMarkDown        = true;
+            $oParseDown       = new Parsedown();
+            $szLicenseContent = $oParseDown->text($szLicenseContent);
+        }
+        $smarty->assign('fMarkDown', $fMarkDown)
+               ->assign('szLicenseContent', $szLicenseContent);
+
+        $oUnnamedTab                     = new stdClass();
+        $oUnnamedTab->kPluginAdminMenu   = count($oPlugin->oPluginAdminMenu_arr) + 1;
+        $oUnnamedTab->kPlugin            = $oPlugin->kPlugin;
+        $oUnnamedTab->cName              = 'Lizenzvereinbarung';
+        $oUnnamedTab->cDateiname         = '';
+        $oUnnamedTab->nSort              = count($oPlugin->oPluginAdminMenu_arr) + 1;
+        $oUnnamedTab->nConf              = 0;
+        $oPlugin->oPluginAdminMenu_arr[] = $oUnnamedTab;
+
+        $fAddAsLicenseTab = true;
+    }
+    foreach ($oPlugin->oPluginAdminMenu_arr as $_adminMenu) {
+        if ((int)$_adminMenu->nConf === 0 && $_adminMenu->cDateiname !== ''
+            && file_exists($oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname)
+        ) {
+            ob_start();
+            require $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
+
+            $tab                   = new stdClass();
+            $tab->file             = $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
+            $tab->idx              = $i;
+            $tab->id               = str_replace('.php', '', $_adminMenu->cDateiname);
+            $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
+            $tab->cName            = $_adminMenu->cName;
+            $tab->html             = ob_get_contents();
+            $customTabs[]          = $tab;
+            ob_end_clean();
+            ++$i;
+        } elseif ((int)$_adminMenu->nConf === 1) {
+            $smarty->assign('oPluginAdminMenu', $_adminMenu);
+            $tab                   = new stdClass();
+            $tab->file             = $oPlugin->cAdminmenuPfad . $_adminMenu->cDateiname;
+            $tab->idx              = $i;
+            $tab->id               = 'settings-' . $j;
+            $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
+            $tab->cName            = $_adminMenu->cName;
+            $tab->html             = $smarty->fetch('tpl_inc/plugin_options.tpl');
+            $customTabs[]          = $tab;
+            ++$j;
+        } elseif (true === $fAddAsDocTab) {
+            $tab                   = new stdClass();
+            $tab->file             = '';
+            $tab->idx              = $i;
+            $tab->id               = 'addon-' . $j;
+            $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
+            $tab->cName            = $_adminMenu->cName;
+            $tab->html             = $smarty->fetch('tpl_inc/plugin_documentation.tpl');
+            $customTabs[]          = $tab;
+            ++$j;
+            $fAddAsDocTab = false; // prevent another appending!
+        } elseif (true === $fAddAsLicenseTab) {
+            $tab                   = new stdClass();
+            $tab->file             = '';
+            $tab->idx              = $i;
+            $tab->id               = 'addon-' . $j;
+            $tab->kPluginAdminMenu = $_adminMenu->kPluginAdminMenu;
+            $tab->cName            = $_adminMenu->cName;
+            $tab->html             = $smarty->fetch('tpl_inc/plugin_license.tpl');
+            $customTabs[]          = $tab;
+            ++$j;
+            $fAddAsLicenseTab = false; // prevent another appending!
         }
     }
 }
 
-$smarty->assign('customPluginTabs', $customPluginTabs)
+$smarty->assign('customPluginTabs', $customTabs)
        ->assign('hinweis', $cHinweis)
        ->assign('fehler', $cFehler)
        ->assign('step', $step)
-       ->display($pluginTemplateFile);
+       ->display('plugin.tpl');

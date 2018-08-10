11c95f92e0e68a6eb319f767b0e9edb6d532797d
Felix Moche <felix.moche@jtl-software.com>
2018-08-10 12:23:01 +0200

    fix sql

--- includes/src/Boxes/BoxNewsCategories.php
+++ includes/src/Boxes/BoxNewsCategories.php
@@ -23,21 +23,23 @@ final class BoxNewsCategories extends AbstractBox
         parent::__construct($config);
         parent::addMapping('oNewsKategorie_arr', 'Items');
         $cSQL      = (int)$config['news']['news_anzahl_box'] > 0
-            ? " LIMIT " . (int)$config['news']['news_anzahl_box']
+            ? ' LIMIT ' . (int)$config['news']['news_anzahl_box']
             : '';
         $langID    = \Shop::getLanguageID();
         $cacheID   = 'bnk_' . $langID . '_' . \Session::CustomerGroup()->getID() . '_' . \md5($cSQL);
         $cached    = true;
         $cacheTags = [\CACHING_GROUP_BOX, \CACHING_GROUP_NEWS];
-        if (($newsCategories = \Shop::Container()->getCache()->get($cacheID)) === false) {
+        if (true||($newsCategories = \Shop::Container()->getCache()->get($cacheID)) === false) {
             $cached         = false;
             $newsCategories = \Shop::Container()->getDB()->queryPrepared(
-                "SELECT tnewskategorie.kNewsKategorie, tnewskategorie.kSprache, tnewskategorie.cName,
-                    tnewskategorie.cBeschreibung, tnewskategorie.cMetaTitle, tnewskategorie.cMetaDescription,
+                "SELECT tnewskategorie.kNewsKategorie, t.languageID AS kSprache, t.name AS cName,
+                    t.description AS cBeschreibung, t.metaTitle AS cMetaTitle, t.metaDescription AS cMetaDescription,
                     tnewskategorie.nSort, tnewskategorie.nAktiv, tnewskategorie.dLetzteAktualisierung,
                     tnewskategorie.cPreviewImage, tseo.cSeo,
                     COUNT(DISTINCT(tnewskategorienews.kNews)) AS nAnzahlNews
                     FROM tnewskategorie
+                    JOIN tnewskategoriesprache t 
+                        ON tnewskategorie.kNewsKategorie = t.kNewsKategorie
                     LEFT JOIN tnewskategorienews 
                         ON tnewskategorienews.kNewsKategorie = tnewskategorie.kNewsKategorie
                     LEFT JOIN tnews 
@@ -46,7 +48,7 @@ final class BoxNewsCategories extends AbstractBox
                         ON tseo.cKey = 'kNewsKategorie'
                         AND tseo.kKey = tnewskategorie.kNewsKategorie
                         AND tseo.kSprache = :lid
-                    WHERE tnewskategorie.kSprache = :lid
+                    WHERE t.languageID = :lid
                         AND tnewskategorie.nAktiv = 1
                         AND tnews.nAktiv = 1
                         AND tnews.dGueltigVon <= now()

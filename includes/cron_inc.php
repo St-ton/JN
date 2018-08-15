53247dabff867f4d38fd126666f59ef91910015b
Felix Moche <felix.moche@jtl-software.com>
2018-08-15 12:56:02 +0200

    pseudo-async handling

--- includes/cron_inc.php
+++ includes/cron_inc.php
@@ -3,11 +3,24 @@
  * @copyright (c) JTL-Software-GmbH
  * @license http://jtl-url.de/jtlshoplicense
  */
+
 defined('JTLCRON') || define('JTLCRON', true);
 if (!defined('PFAD_LOGFILES')) {
     require __DIR__ . '/globalinclude.php';
 }
-
+if (PHP_SAPI !== 'cli') {
+    while (ob_get_level()) {
+        ob_end_clean();
+    }
+    ignore_user_abort(true);
+    header('Connection: close');
+    ob_start();
+    echo 'Starting cron';
+    $size = ob_get_length();
+    header('Content-Length: ' . $size);
+    ob_end_flush();
+    flush();
+}
 define('JOBQUEUE_LOCKFILE', PFAD_LOGFILES . 'jobqueue.lock');
 
 if (file_exists(JOBQUEUE_LOCKFILE) === false) {
@@ -16,9 +29,9 @@ if (file_exists(JOBQUEUE_LOCKFILE) === false) {
 
 $lockfile = fopen(JOBQUEUE_LOCKFILE, 'rb');
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 if (PHP_SAPI === 'cli') {
     $handler = new StreamHandler('php://stdout', Logger::DEBUG);
@@ -29,10 +42,9 @@ if (PHP_SAPI === 'cli') {
 }
 
 if (flock($lockfile, LOCK_EX | LOCK_NB) === false) {
-    $logger->log(JTLLOG_LEVEL_NOTICE, 'Cron currently locked');
+    $logger->debug('Cron currently locked');
     exit;
 }
-
 $db = Shop::Container()->getDB();
 
 $factory = new \Cron\JobFactory($db, $logger);

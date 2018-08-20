6e97c9daa27dc7ea3581b20de4aad77a188fd26a
Felix Moche <felix.moche@jtl-software.com>
2018-08-20 15:28:13 +0200

    removed down

--- update/migrations/20180820125000_remove_old_payment_methods.php
+++ update/migrations/20180820125000_remove_old_payment_methods.php
@@ -66,18 +66,5 @@ class Migration_20180820125000 extends Migration implements IMigration
 
     public function down()
     {
-        $this->execute("
-        INSERT INTO `tzahlungsart`
-        (`cName`, `cModulId`, `cKundengruppen`, `cZusatzschrittTemplate`, `cPluginTemplate`, `cBild`, `nSort`, `nMailSenden`, `nActive`, `cAnbieter`, `cTSCode`, `nWaehrendBestellung`, `nCURL`, `nSOAP`, `nSOCKETS`, `nNutzbar`)
-        VALUES 
-        ('PayPal','za_paypal_jtl','','',NULL,'',0,1,1,'PayPal','PAYPAL',0,0,0,1,1),
-        ('WorldPay','za_worldpay_jtl',NULL,'',NULL,'',0,1,1,'RBS','OTHER',0,0,0,0,1),
-        ('iPayment','za_ipayment_jtl',NULL,'',NULL,'',0,1,1,'1&1','OTHER',0,0,0,0,1),
-        ('Safetypay','za_safetypay','','',NULL,'',0,1,1,'Safetypay','OTHER',0,0,1,0,1),
-        ('PaymentPartner','za_paymentpartner_jtl','','',NULL,'',0,0,1,'PaymentPartner','OTHER',0,0,0,1,1),
-        ('PostFinance','za_postfinance_jtl','','',NULL,'',0,0,1,'PostFinance','OTHER',0,0,0,0,1),
-        ('Saferpay','za_saferpay_jtl','','',NULL,'',0,0,0,'SIX Card Solutions','OTHER',0,0,0,1,0),
-        ('Wirecard','za_wirecard_jtl','','',NULL,'',0,0,1,'Wirecard','OTHER',0,0,0,0,1);
-        ");
     }
 }

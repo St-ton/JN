ae01a1c97b3007c57f41e0a196dd243be030fb29
Felix Moche <felix.moche@jtl-software.com>
2018-08-28 18:26:51 +0200

    fixed agb/wrb

--- includes/src/Services/JTL/LinkService.php
+++ includes/src/Services/JTL/LinkService.php
@@ -552,7 +552,7 @@ final class LinkService implements LinkServiceInterface
         $oAGBWRB = $this->db->select('ttext', 'nStandard', 1);
         if (!empty($oAGBWRB->kText)) {
             $oAGBWRB->cURLAGB  = $oLinkAGB !== null ? $oLinkAGB->getURL() : '';
-            $oAGBWRB->cURLWRB  = $oAGBWRB !== null ? $oLinkAGB->getURL() : '';
+            $oAGBWRB->cURLWRB  = $oLinkWRB !== null ? $oLinkWRB->getURL() : '';
             $oAGBWRB->kLinkAGB = $oLinkAGB !== null ? $oLinkAGB->getID() : 0;
             $oAGBWRB->kLinkWRB = $oLinkWRB !== null ? $oLinkWRB->getID() : 0;
 

6ce476cdbe3f779a843d6466d011b6c81beb571b
David <david.hannappel@jtl-software.com>
2018-09-28 09:05:31 +0200

    Update initial_schema.sql Remove FULLTEXT KEY for tjtllog.cLog

--- install/initial_schema.sql
+++ install/initial_schema.sql
@@ -1,4 +1,3 @@
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -1756,8 +1755,7 @@ CREATE TABLE `tjtllog` (
   `kKey` int(10) unsigned DEFAULT NULL,
   `dErstellt` datetime NOT NULL,
   PRIMARY KEY (`kLog`),
-  KEY `cKey` (`cKey`,`kKey`),
-  FULLTEXT KEY `cLog` (`cLog`)
+  KEY `cKey` (`cKey`,`kKey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Globaler JTL Log';
 /*!40101 SET character_set_client = @saved_cs_client */;
 

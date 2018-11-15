154e5bfc1c99b6e421dcba4f2ea8f04156fccb3f
Felix Moche <felix.moche@jtl-software.com>
2018-11-15 15:30:46 +0100

    path

--- admin/getMarkdownAsHTML.php
+++ admin/getMarkdownAsHTML.php
@@ -18,7 +18,8 @@ $oAccount->redirectOnFailure();
 if (isset($_POST['path']) && FormHelper::validateToken()) {
     $path = realpath($_POST['path']);
     if ($path !== false
-        && (strpos($path . '/', PFAD_ROOT . 'plugins') === 0 || strpos($path . '/', PFAD_ROOT . PFAD_PLUGIN) === 0)
+        && (strpos($path . DIRECTORY_SEPARATOR, PFAD_ROOT . PFAD_EXTENSIONS) === 0
+            || strpos($path . DIRECTORY_SEPARATOR, PFAD_ROOT . PFAD_PLUGIN) === 0)
     ) {
         $info = pathinfo($path);
         if (strtolower($info['extension']) === 'md') {

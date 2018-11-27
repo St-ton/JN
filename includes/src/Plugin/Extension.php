b7fb196471a7199850870b1dc990653164f2944e
Felix Moche <felix.moche@jtl-software.com>
2018-11-27 13:05:15 +0100

    version node

--- includes/src/Plugin/Extension.php
+++ includes/src/Plugin/Extension.php
@@ -26,6 +26,6 @@ class Extension extends AbstractExtension
         $parser  = new XMLParser();
         $xml     = $parser->parse($path . '/' . \PLUGIN_INFO_FILE);
 
-        return $xml['jtlshopplugin']['Version'] ?? '0';
+        return $xml['jtlshopplugin'][0]['Version'] ?? '0';
     }
 }

ab0e7a9fd03271e43870e3c054f3c47eb0fe469a
mirkoschmidt <mschmidi92@gmx.de>
2018-11-27 18:34:26 +0100

    Check file path for patching exists and change zip exclude, file name for patching

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -23,12 +23,12 @@ if [[ ! -z $(find . -maxdepth 1 -type d -regex '^./patch-dir-.*') ]]; then
         LOWER_VERSION_STR="${LOWER_VERSION//[\.]/-}";
         HIGHER_VERSION=${BASH_REMATCH[2]};
         HIGHER_VERSION_STR="${HIGHER_VERSION//[\.]/-}";
-        PATCH_FILENAME="${PROJECT_NAME}-${LOWER_VERSION_STR}-to-${HIGHER_VERSION_STR}.zip";
+        PATCH_FILENAME="shop-${LOWER_VERSION_STR}-to-${HIGHER_VERSION_STR}.zip";
         PATCH_ARCHIVE_PATH="${3}/${PATCH_FILENAME}";
         CUR_PWD=$(pwd);
         echo "  Patch '${LOWER_VERSION}' to '${HIGHER_VERSION}'";
         cd ${path};
-        zip -r -q ${PATCH_ARCHIVE_PATH} . -x  \*.git* \*.idea* \*docs/* \*patch-dir-* \*tests/* \*tools/* \*.asset_cs \*.php_cs \*.travis.yml \*phpunit.xml;
+        zip -r -q ${PATCH_ARCHIVE_PATH} . -x  \*.git* \*.idea* \*build/* \*docs/* \*patch-dir-* \*tests/* \*.asset_cs \*.php_cs \*.travis.yml \*phpunit.xml;
         echo "    ${PATCH_FILENAME}";
         cd ${CUR_PWD};
     done< <(find . -maxdepth 1 -type d -regex '^./patch-dir-.*');

4d4cb4ed5f25efe6a1c4fe2b242e3e99d4434bc5
mirkoschmidt <mschmidi92@gmx.de>
2018-11-27 14:16:45 +0100

    Change build process

--- build/scripts/less.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$rootPath = realpath(dirname(__FILE__) . '/../..');
-require_once $rootPath . '/includes/globalinclude.php';
-
-$oPlugin = Plugin::getPluginById('evo_editor');
-
-require_once $oPlugin->cAdminmenuPfad . '/cli.php';

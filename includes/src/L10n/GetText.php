8ae355eedb2c880cc74139182b430fb8570456af
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2018-12-10 09:43:05 +0100

    Check kSprache is valid

--- includes/src/L10n/GetText.php
+++ includes/src/L10n/GetText.php
@@ -34,7 +34,7 @@ class GetText
         $this->translator = new \Gettext\Translator();
         $this->translator->register();
 
-        if (isset($_SESSION['AdminAccount']->kSprache)) {
+        if (isset($_SESSION['AdminAccount']->kSprache) && (int)$_SESSION['AdminAccount']->kSprache > 0) {
             $this->setLangIso(\Shop::Lang()->getIsoFromLangID($_SESSION['AdminAccount']->kSprache)->cISO)
                  ->loadAdminLocale('base');
         }

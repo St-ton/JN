3ca99008ac7002b95f9bca45bd17204aec45c3ac
Felix Moche <felix.moche@jtl-software.com>
2018-12-12 15:33:30 +0100

    ns

--- includes/src/Services/JTL/CryptoService.php
+++ includes/src/Services/JTL/CryptoService.php
@@ -17,7 +17,7 @@ class CryptoService implements CryptoServiceInterface
      */
     public function randomBytes($bytesAmount): string
     {
-        return random_bytes($bytesAmount);
+        return \random_bytes($bytesAmount);
     }
 
     /**
@@ -33,7 +33,7 @@ class CryptoService implements CryptoServiceInterface
      */
     public function randomInt($min, $max): int
     {
-        return random_int($min, $max);
+        return \random_int($min, $max);
     }
 
     /**
@@ -45,24 +45,24 @@ class CryptoService implements CryptoServiceInterface
     }
 
     /**
-     * @param string $cText
+     * @param string $text
      * @return string
      */
-    public function encryptXTEA(string $cText): string
+    public function encryptXTEA(string $text): string
     {
-        return \strlen($cText) > 0
-            ? (new \XTEA(BLOWFISH_KEY))->encrypt($cText)
-            : $cText;
+        return \strlen($text) > 0
+            ? (new \XTEA(\BLOWFISH_KEY))->encrypt($text)
+            : $text;
     }
 
     /**
-     * @param string $cText
+     * @param string $text
      * @return string
      */
-    public function decryptXTEA(string $cText): string
+    public function decryptXTEA(string $text): string
     {
-        return \strlen($cText) > 0
-            ? (new \XTEA(BLOWFISH_KEY))->decrypt($cText)
-            : $cText;
+        return \strlen($text) > 0
+            ? (new \XTEA(\BLOWFISH_KEY))->decrypt($text)
+            : $text;
     }
 }

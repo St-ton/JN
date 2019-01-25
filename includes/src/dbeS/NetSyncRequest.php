1178f59417a78b9909df9c0b33a6f88843e71859
Felix Moche <felix.moche@jtl-software.com>
2019-01-25 17:32:16 +0100

    cs

--- includes/src/dbeS/NetSyncRequest.php
+++ includes/src/dbeS/NetSyncRequest.php
@@ -13,11 +13,18 @@ namespace dbeS;
 class NetSyncRequest
 {
     public const UNKNOWN = 0;
+
     public const UPLOADFILES = 1;
+
     public const UPLOADFILEDATA = 2;
+
     public const DOWNLOADFOLDERS = 3;
+
     public const DOWNLOADFILESINFOLDER = 4;
+
     public const CRONJOBTRIGGER = 5;
+
     public const CRONJOBSTATUS = 6;
+
     public const CRONJOBHISTORY = 7;
 }
--- includes/src/dbeS/NetSyncResponse.php
+++ includes/src/dbeS/NetSyncResponse.php
@@ -13,11 +13,18 @@ namespace dbeS;
 class NetSyncResponse
 {
     public const UNKNOWN = -1;
+
     public const OK = 0;
+
     public const ERRORLOGIN = 1;
+
     public const ERRORDESERIALIZE = 2;
+
     public const RECEIVINGDATA = 3;
+
     public const FOLDERNOTEXISTS = 4;
+
     public const ERRORINTERNAL = 5;
+
     public const ERRORNOLICENSE = 6;
 }

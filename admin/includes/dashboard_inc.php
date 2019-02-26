2a42520019b621ac33278b6d38a24fa310b806e9
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2019-02-26 12:37:55 +0100

    Also load locales of plugin widgets. Fallback to old desc and title. SHOP-3056

--- admin/includes/dashboard_inc.php
+++ admin/includes/dashboard_inc.php
@@ -20,59 +20,91 @@ use JTL\Network\JTLApi;
  */
 function getWidgets(bool $bActive = true)
 {
-    $cache   = Shop::Container()->getCache();
-    $db      = Shop::Container()->getDB();
-    $gettext = Shop::Container()->getGetText();
+    $cache        = Shop::Container()->getCache();
+    $db           = Shop::Container()->getDB();
+    $gettext      = Shop::Container()->getGetText();
+    $loaderLegacy = Helper::getLoader(false, $db, $cache);
+    $loaderExt    = Helper::getLoader(true, $db, $cache);
+    $plugins      = [];
+
     $widgets = $db->queryPrepared(
         'SELECT tadminwidgets.*, tplugin.cPluginID, tplugin.bExtension
             FROM tadminwidgets
             LEFT JOIN tplugin 
                 ON tplugin.kPlugin = tadminwidgets.kPlugin
             WHERE bActive = :active
-                AND tplugin.nStatus IS NULL OR tplugin.nStatus = :activated
+                AND (tplugin.nStatus IS NULL OR tplugin.nStatus = :activated)
             ORDER BY eContainer ASC, nPos ASC',
         ['active' => (int)$bActive, 'activated' => State::ACTIVATED],
         ReturnType::ARRAY_OF_OBJECTS
     );
+
     foreach ($widgets as $widget) {
-        $gettext->loadAdminLocale('widgets/' . $widget->cClass);
-        $widget->cTitle       = __($widget->cClass . '_title');
-        $widget->cDescription = __($widget->cClass . '_desc');
+        $widget->kWidget    = (int)$widget->kWidget;
+        $widget->kPlugin    = (int)$widget->kPlugin;
+        $widget->nPos       = (int)$widget->nPos;
+        $widget->bExpanded  = (int)$widget->bExpanded;
+        $widget->bActive    = (int)$widget->bActive;
+        $widget->bExtension = (int)$widget->bExtension;
+
+        if ($widget->cPluginID !== null) {
+            if (array_key_exists($widget->cPluginID, $plugins)) {
+                $widget->plugin = $plugins[$widget->cPluginID];
+            } else {
+                if ($widget->bExtension === 1) {
+                    $widget->plugin = $loaderExt->init((int)$widget->kPlugin);
+                } else {
+                    $widget->plugin = $loaderLegacy->init((int)$widget->kPlugin);
+                }
+
+                $plugins[$widget->cPluginID] = $widget->plugin;
+            }
+
+            $gettext->loadPluginLocale('widgets/' . $widget->cClass, $widget->plugin);
+        } else {
+            $gettext->loadAdminLocale('widgets/' . $widget->cClass);
+            $widget->plugin = null;
+        }
+
+        $msgid  = $widget->cClass . '_title';
+        $msgstr = __($msgid);
+
+        if ($msgid !== $msgstr) {
+            $widget->cTitle = $msgstr;
+        }
+
+        $msgid  = $widget->cClass . '_desc';
+        $msgstr = __($msgid);
+
+        if ($msgid !== $msgstr) {
+            $widget->cDescription = $msgstr;
+        }
     }
+
     if ($bActive) {
         $smarty = JTLSmarty::getInstance(false, \JTL\Smarty\ContextType::BACKEND);
+
         foreach ($widgets as $widget) {
-            $widget->kWidget    = (int)$widget->kWidget;
-            $widget->kPlugin    = (int)$widget->kPlugin;
-            $widget->nPos       = (int)$widget->nPos;
-            $widget->bExpanded  = (int)$widget->bExpanded;
-            $widget->bActive    = (int)$widget->bActive;
-            $widget->bExtension = (int)$widget->bExtension;
-            $widget->cContent   = '';
-            $className          = '\JTL\Widgets\\' . $widget->cClass;
-            $classPath          = null;
-            $widget->cNiceTitle = str_replace(['--', ' '], '-', $widget->cTitle);
-            $widget->cNiceTitle = mb_convert_case(
-                preg_replace('/[äüöß\(\)\/\\\]/iu', '', $widget->cNiceTitle),
-                MB_CASE_LOWER
-            );
-            $plugin             = null;
+            $widget->cContent = '';
+            $className        = '\JTL\Widgets\\' . $widget->cClass;
+            $classPath        = null;
+
             if ($widget->kPlugin > 0) {
-                $loader = Helper::getLoader($widget->bExtension === 1, $db, $cache);
-                $plugin = $loader->init($widget->kPlugin);
-                $hit    = $plugin->getWidgets()->getWidgetByID($widget->kWidget);
-                if ($hit === null) {
-                    continue;
+                $hit = $widget->plugin->getWidgets()->getWidgetByID($widget->kWidget);
+
+                if ($hit !== null) {
+                    $className = $hit->className;
+                    $classPath = $hit->classFile;
+
+                    if (file_exists($classPath)) {
+                        require_once $classPath;
+                    }
                 }
-                $className = $hit->className;
-                $classPath = $hit->classFile;
             }
-            if ($classPath !== null && file_exists($classPath)) {
-                require_once $classPath;
-            }
+
             if (class_exists($className)) {
                 /** @var \JTL\Widgets\AbstractWidget $instance */
-                $instance         = new $className($smarty, $db, $plugin);
+                $instance         = new $className($smarty, $db, $widget->plugin);
                 $widget->cContent = $instance->getContent();
                 $widget->hasBody  = $instance->hasBody;
             }

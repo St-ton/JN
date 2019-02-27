9c86706ca343ba415c11962fd5ea7417bec94f99
Marco Stickel <marco.stickel@jtl-software.com>
2019-02-27 15:25:28 +0100

    cs. SHOP-3059

--- includes/src/IO/IOMethods.php
+++ includes/src/IO/IOMethods.php
@@ -496,12 +496,12 @@ class IOMethods
                 if ($box->getType() === Type::CONTAINER) {
                     foreach ($box->getChildren() as $childBox) {
                         if (\get_class($childBox) === Wishlist::class) {
-                            $renderer = new DefaultRenderer($smarty, $childBox);
+                            $renderer                                    = new DefaultRenderer($smarty, $childBox);
                             $response->cBoxContainer[$childBox->getID()] = $renderer->render();
                         }
                     }
                 } elseif (\get_class($box) === Wishlist::class) {
-                    $renderer = new DefaultRenderer($smarty, $box);
+                    $renderer                               = new DefaultRenderer($smarty, $box);
                     $response->cBoxContainer[$box->getID()] = $renderer->render();
                 }
             }
@@ -544,14 +544,12 @@ class IOMethods
                 if ($box->getType() === Type::CONTAINER) {
                     foreach ($box->getChildren() as $childBox) {
                         if ($childBox->getType() === Wishlist::class) {
-                            $renderer = new DefaultRenderer($smarty, $childBox);
-
+                            $renderer                                    = new DefaultRenderer($smarty, $childBox);
                             $response->cBoxContainer[$childBox->getID()] = $renderer->render();
                         }
                     }
                 } elseif (\get_class($box) === Wishlist::class) {
-                    $renderer = new DefaultRenderer($smarty, $box);
-
+                    $renderer                               = new DefaultRenderer($smarty, $box);
                     $response->cBoxContainer[$box->getID()] = $renderer->render();
                 }
             }

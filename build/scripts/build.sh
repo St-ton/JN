c4d71c199166552a539d9f6eb5e60687b4543122
mirkoschmidt <mschmidi92@gmx.de>
2019-03-01 10:57:57 +0100

    Remove "Authoritative class maps" SHOP-3072 #request-merge

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -86,7 +86,7 @@ build_create()
 
 build_composer_execute()
 {
-    composer install --no-dev -a -o -q -d ${REPOSITORY_DIR}/includes;
+    composer install --no-dev -o -q -d ${REPOSITORY_DIR}/includes;
 }
 
 build_create_deleted_files_csv()

297e9d9287ce8bcb30c168d8174dd5b95ee50329
Felix Moche <felix.moche@jtl-software.com>
2019-03-22 12:12:42 +0100

    re-added image api

--- includes/src/dbeS/Starter.php
+++ includes/src/dbeS/Starter.php
@@ -10,6 +10,7 @@ use Generator;
 use JTL\Cache\JTLCacheInterface;
 use JTL\DB\DbInterface;
 use JTL\dbeS\Push\AbstractPush;
+use JTL\dbeS\Push\ImageAPI;
 use JTL\dbeS\Push\Invoice;
 use JTL\dbeS\Push\MediaFiles;
 use JTL\dbeS\Push\Payments;
@@ -32,6 +33,7 @@ use JTL\dbeS\Sync\Customer;
 use JTL\dbeS\Sync\DeliverySlips;
 use JTL\dbeS\Sync\QuickSync;
 use JTL\Helpers\Text;
+use JTL\Shop;
 use JTL\XML;
 use Psr\Log\LoggerInterface;
 use JTL\dbeS\Push\Orders as PushOrders;
@@ -49,6 +51,10 @@ class Starter
     public const ERROR_UNZIP = 2;
 
     public const OK = 0;
+
+    private const DIRECTION_PUSH = 'push';
+
+    private const DIRECTION_PULL = 'pull';
 
     /**
      * @var array
@@ -83,7 +89,8 @@ class Starter
         'GetKunden_xml'        => Customers::class,
         'GetMediendateien_xml' => MediaFiles::class,
         'GetZahlungen_xml'     => Payments::class,
-        'Invoice_xml'          => Invoice::class
+        'Invoice_xml'          => Invoice::class,
+        'bild'                 => ImageAPI::class
     ];
 
     /**
@@ -261,13 +268,13 @@ class Starter
     }
 
     /**
+     * handling of files that do not fit the general push/pull scheme
+     *
      * @param string $handledFile
      * @param array  $post
-     * @param array  $files
-     * @return int
      * @throws \Exception
      */
-    public function start(string $handledFile, array $post, array $files): int
+    private function handleSpecialCases(string $handledFile, array $post): void
     {
         if ($handledFile === 'lastjobs') {
             $this->init($post, [], false);
@@ -282,13 +289,38 @@ class Starter
             echo $test->execute();
             exit();
         }
+        if ($handledFile === 'bild') {
+            $conf = Shop::getSettings([\CONF_BILDER]);
+            if ($conf['bilder']['bilder_externe_bildschnittstelle'] === 'N') {
+                exit(); // api disabled
+            }
+            if ($conf['bilder']['bilder_externe_bildschnittstelle'] === 'W' && !$this->checkAuth($post)) {
+                exit(); // api is wawi only
+            }
+            $this->init($post, [], false);
+            $api = new ImageAPI($this->db, $this->cache, $this->logger);
+            $api->getData();
+            exit();
+        }
+    }
+
+    /**
+     * @param string $handledFile
+     * @param array  $post
+     * @param array  $files
+     * @return int
+     * @throws \Exception
+     */
+    public function start(string $handledFile, array $post, array $files): int
+    {
+        $this->handleSpecialCases($handledFile, $post);
         $this->executeNetSync($handledFile);
-        $direction = 'pull';
+        $direction = self::DIRECTION_PULL;
         $handler   = self::$pullMapping[$handledFile] ?? null;
         if ($handler === null) {
             $handler = self::$pushMapping[$handledFile] ?? null;
             if ($handler !== null) {
-                $direction = 'push';
+                $direction = self::DIRECTION_PUSH;
             }
         }
         if ($handler === null) {
@@ -297,7 +329,7 @@ class Starter
         $this->setPostData($post);
         $this->setData($files['data']['tmp_name'] ?? null);
 
-        if ($direction === 'pull') {
+        if ($direction === self::DIRECTION_PULL) {
             $res        = '';
             $unzip      = $handler !== Brocken::class;
             $fromHandle = $handler === Customer::class;
@@ -325,22 +357,6 @@ class Starter
             }
 
             echo self::OK;
-        }
-        exit();
-    }
-
-    /**
-     * @param string $handledFile
-     */
-    private function bypass(string $handledFile): void
-    {
-        $file = \PFAD_ROOT . \PFAD_DBES . \basename($handledFile) . '.php';
-        $real = \realpath($file);
-        if ($real === false || \strpos($real, \PFAD_ROOT . \PFAD_DBES) !== 0) {
-            exit();
-        }
-        if (\file_exists($real)) {
-            include $real;
         }
         exit();
     }

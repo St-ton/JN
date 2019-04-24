ccd2a910ad424d7b34a05b09a294b61ccb2ce7d3
Marco Stickel <marco.stickel@jtl-software.com>
2019-04-24 15:12:39 +0200

    get base URL. SHOP-3113

--- templates/NOVA/js/jtl.io.js
+++ templates/NOVA/js/jtl.io.js
@@ -91,24 +91,12 @@
         },
 
         getBaseURL: function () {
-            var url = location.href;  // entire url including querystring - also: window.location.href;
-            var baseURL = url.substring(0, url.indexOf('/', 14));
+            var pathArray = location.href.split( '/' );
+            var protocol = pathArray[0];
+            var host = pathArray[2];
+            var url = protocol + '//' + host + '/';
 
-            if (baseURL.indexOf('http://localhost') != -1) {
-                // Base Url for localhost
-                var url = location.href;  // window.location.href;
-                var pathname = location.pathname;  // window.location.pathname;
-                var index1 = url.indexOf(pathname);
-                var index2 = url.indexOf("/", index1 + 1);
-                var baseLocalUrl = url.substr(0, index2);
-
-                return baseLocalUrl + "/";
-            }
-            else {
-                // Root Url for domain name
-                return baseURL + "/";
-            }
-
+            return url;
         }
     };
 

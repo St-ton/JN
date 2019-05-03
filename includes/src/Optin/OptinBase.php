7c574ee4138d7582f4a325affe022c4d3a6b5183
Clemens Rudolph <clemens.rudolph@jtl-software.com>
2019-05-03 16:04:13 +0200

    inbetween ci. small fixes.

--- includes/src/Optin/OptinBase.php
+++ includes/src/Optin/OptinBase.php
@@ -176,6 +176,11 @@ abstract class OptinBase extends OptinFactory
         $newRow->dCreated     = $this->foundOptinTupel->dCreated;
         $newRow->dActivated   = $this->foundOptinTupel->dActivated;
         $newRow->dDeActivated = $this->nowDataTime->format('Y-m-d H:i:s');
+        foreach (array_keys(get_object_vars($newRow)) as $element) {
+            if (empty($newRow->$element)) {
+                unset($newRow->$element);
+            }
+        }
         $this->dbHandler->insert('toptinhistory', $newRow);
         $this->dbHandler->delete('toptin', 'kOptinCode', $this->foundOptinTupel->kOptinCode);
     }

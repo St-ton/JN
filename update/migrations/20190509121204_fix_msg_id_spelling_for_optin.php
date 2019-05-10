4e9624efc291f220b13707b7e96aacdd9724aeca
Clemens Rudolph <clemens.rudolph@jtl-software.com>
2019-05-10 10:10:28 +0200

    migration uses added; comments optimized

--- update/migrations/20190509121204_fix_msg_id_spelling_for_optin.php
+++ update/migrations/20190509121204_fix_msg_id_spelling_for_optin.php
@@ -6,19 +6,11 @@
  * @created Thu, 09 May 2019 12:12:04 +0200
  */
 
+use JTL\Update\IMigration;
+use JTL\Update\Migration;
+
 /**
  * Migration
- *
- * Available methods:
- * execute            - returns affected rows
- * fetchOne           - single fetched object
- * fetchAll           - array of fetched objects
- * fetchArray         - array of fetched assoc arrays
- * dropColumn         - drops a column if exists
- * setLocalization    - add localization
- * removeLocalization - remove localization
- * setConfig          - add / update config property
- * removeConfig       - remove config property
  */
 class Migration_20190509121204 extends Migration implements IMigration
 {

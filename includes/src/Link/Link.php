1b825a6fc948c2f250ccac7a82ef0827741bb687
Clemens Rudolph <clemens.rudolph@jtl-software.com>
2019-05-14 12:40:03 +0200

    fixed language fall back to defaults. SHOP-3257

--- includes/src/Link/Link.php
+++ includes/src/Link/Link.php
@@ -207,8 +207,9 @@ final class Link extends AbstractLink
      */
     public function __construct(DbInterface $db)
     {
-        $this->db         = $db;
-        $this->childLinks = new Collection();
+        $this->db                = $db;
+        $this->childLinks        = new Collection();
+        $this->currentLanguageID = Shop::getLanguageID();
     }
 
     /**
@@ -216,11 +217,10 @@ final class Link extends AbstractLink
      */
     public function load(int $id): LinkInterface
     {
-        $this->currentLanguageID = Shop::getLanguageID();
-        $this->id                = $id;
-        $link                    = $this->db->queryPrepared(
+        $this->id = $id;
+        $link     = $this->db->queryPrepared(
             "SELECT tlink.*, tlinksprache.cISOSprache, tlink.cName AS displayName,
-                tlinksprache.cName AS localizedName,  tlinksprache.cTitle AS localizedTitle, 
+                tlinksprache.cName AS localizedName,  tlinksprache.cTitle AS localizedTitle,
                 tlinksprache.cContent AS content, tlinksprache.cMetaDescription AS metaDescription,
                 tlinksprache.cMetaKeywords AS metaKeywords, tlinksprache.cMetaTitle AS metaTitle,
                 tseo.kSprache AS languageID, tseo.cSeo AS localizedUrl,

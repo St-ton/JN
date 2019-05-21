2194f6e87e4c5de1ba114d36cab870ee8f8f963a
Felix Moche <felix.moche@jtl-software.com>
2019-05-21 15:34:24 +0200

    check customer id before sending upload file

--- jtl.php
+++ jtl.php
@@ -75,11 +75,13 @@ if (isset($_POST['kUpload'])
     && Form::validateToken()
 ) {
     $file = new UploadDatei((int)$_POST['kUpload']);
-    UploadDatei::send_file_to_browser(
-        PFAD_UPLOADS . $file->cPfad,
-        'application/octet-stream',
-        $file->cName
-    );
+    if ($file->validateOwner((int)$_SESSION['Kunde']->kKunde)) {
+        UploadDatei::send_file_to_browser(
+            PFAD_UPLOADS . $file->cPfad,
+            'application/octet-stream',
+            $file->cName
+        );
+    }
 }
 
 unset($_SESSION['JTL_REDIRECT']);

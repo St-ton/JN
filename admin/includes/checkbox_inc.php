67ac576c40a47a101a07ca87ed4e6ed156c24be7
Felix Moche <felix.moche@jtl-software.com>
2019-06-06 17:19:38 +0200

    use model

--- admin/includes/checkbox_inc.php
+++ admin/includes/checkbox_inc.php
@@ -6,80 +6,76 @@
 
 use JTL\CheckBox;
 use JTL\Helpers\Text;
+use JTL\Language\LanguageModel;
 use JTL\Shop;
 
 /**
  * @param array $post
- * @param array $languages
+ * @param LanguageModel[] $languages
  * @return array
  */
-function plausiCheckBox($post, $languages)
+function plausiCheckBox(array $post, array $languages): array
 {
-    $plausi = [];
+    $checks = [];
     if (!is_array($languages) || count($languages) === 0) {
-        $plausi['oSprache_arr'] = 1;
+        $checks['oSprache_arr'] = 1;
 
-        return $plausi;
+        return $checks;
     }
-    if (is_array($post) && count($post) > 0) {
-        if (!isset($post['cName']) || mb_strlen($post['cName']) === 0) {
-            $plausi['cName'] = 1;
-        }
-        $bText = false;
-        foreach ($languages as $oSprache) {
-            if (mb_strlen($post['cText_' . $oSprache->cISO]) > 0) {
-                $bText = true;
-                break;
-            }
-        }
-        if (!$bText) {
-            $plausi['cText'] = 1;
-        }
-        $bLink = true;
-        if ((int)$post['nLink'] === 1) {
-            $bLink = false;
-            if (isset($post['kLink']) && (int)$post['kLink'] > 0) {
-                $bLink = true;
-            }
-        }
-        if (!$bLink) {
-            $plausi['kLink'] = 1;
-        }
-        if (!is_array($post['cAnzeigeOrt']) || count($post['cAnzeigeOrt']) === 0) {
-            $plausi['cAnzeigeOrt'] = 1;
-        } else {
-            foreach ($post['cAnzeigeOrt'] as $cAnzeigeOrt) {
-                if ((int)$cAnzeigeOrt === 3 && $post['kCheckBoxFunktion'] == 1) {
-                    $plausi['cAnzeigeOrt'] = 2;
-                }
-            }
-        }
-        if (!isset($post['nPflicht']) || mb_strlen($post['nPflicht']) === 0) {
-            $plausi['nPflicht'] = 1;
-        }
-        if (!isset($post['nAktiv']) || mb_strlen($post['nAktiv']) === 0) {
-            $plausi['nAktiv'] = 1;
-        }
-        if (!isset($post['nLogging']) || mb_strlen($post['nLogging']) === 0) {
-            $plausi['nLogging'] = 1;
-        }
-        if (!isset($post['nSort']) || (int)$post['nSort'] === 0) {
-            $plausi['nSort'] = 1;
-        }
-        if (!is_array($post['kKundengruppe']) || count($post['kKundengruppe']) === 0) {
-            $plausi['kKundengruppe'] = 1;
+    if (!isset($post['cName']) || mb_strlen($post['cName']) === 0) {
+        $checks['cName'] = 1;
+    }
+    $text = false;
+    $link = true;
+    foreach ($languages as $language) {
+        if (mb_strlen($post['cText_' . $language->getIso()]) > 0) {
+            $text = true;
+            break;
         }
     }
+    if (!$text) {
+        $checks['cText'] = 1;
+    }
+    if ((int)$post['nLink'] === 1) {
+        $link = isset($post['kLink']) && (int)$post['kLink'] > 0;
+    }
+    if (!$link) {
+        $checks['kLink'] = 1;
+    }
+    if (!is_array($post['cAnzeigeOrt']) || count($post['cAnzeigeOrt']) === 0) {
+        $checks['cAnzeigeOrt'] = 1;
+    } else {
+        foreach ($post['cAnzeigeOrt'] as $cAnzeigeOrt) {
+            if ((int)$cAnzeigeOrt === 3 && (int)$post['kCheckBoxFunktion'] === 1) {
+                $checks['cAnzeigeOrt'] = 2;
+            }
+        }
+    }
+    if (!isset($post['nPflicht']) || mb_strlen($post['nPflicht']) === 0) {
+        $checks['nPflicht'] = 1;
+    }
+    if (!isset($post['nAktiv']) || mb_strlen($post['nAktiv']) === 0) {
+        $checks['nAktiv'] = 1;
+    }
+    if (!isset($post['nLogging']) || mb_strlen($post['nLogging']) === 0) {
+        $checks['nLogging'] = 1;
+    }
+    if (!isset($post['nSort']) || (int)$post['nSort'] === 0) {
+        $checks['nSort'] = 1;
+    }
+    if (!is_array($post['kKundengruppe']) || count($post['kKundengruppe']) === 0) {
+        $checks['kKundengruppe'] = 1;
+    }
 
-    return $plausi;
+    return $checks;
 }
 
 /**
  * @param array $post
- * @param array $languages
+ * @param LanguageModel[] $languages
  * @return CheckBox
  */
-function speicherCheckBox($post, $languages)
+function speicherCheckBox(array $post, array $languages): CheckBox
 {
     if (isset($post['kCheckBox']) && (int)$post['kCheckBox'] > 0) {
         $checkBox = new CheckBox((int)$post['kCheckBox']);
@@ -112,8 +108,9 @@ function speicherCheckBox($post, $languages)
     $texts               = [];
     $descr               = [];
     foreach ($languages as $language) {
-        $texts[$language->cISO] = str_replace('"', '&quot;', $post['cText_' . $language->cISO]);
-        $descr[$language->cISO] = str_replace('"', '&quot;', $post['cBeschreibung_' . $language->cISO]);
+        $code         = $language->getIso();
+        $texts[$code] = str_replace('"', '&quot;', $post['cText_' . $code]);
+        $descr[$code] = str_replace('"', '&quot;', $post['cBeschreibung_' . $code]);
     }
 
     $checkBox->insertDB($texts, $descr);

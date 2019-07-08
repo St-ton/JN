67ce0087757e0da64c6bd4e9cf8ddeca2bb0334e
Michael Hillmann <michael.hillmann@jtl-software.com>
2019-07-08 09:08:02 +0200

    updated lang vars

--- update/migrations/20190704154900_add_password_check_lang.php
+++ update/migrations/20190704154900_add_password_check_lang.php
@@ -19,18 +19,18 @@ class Migration_20190704154900 extends Migration implements IMigration
 
     public function up()
     {
-        $this->setLocalization('ger', 'login', 'passwordTooShort', 'Das Passwort muss aus mindestens %s zeichen bestehen.');
-        $this->setLocalization('eng', 'login', 'passwordTooShort', 'The password should have at least %s characters.');
-        $this->setLocalization('ger', 'login', 'passwordIsWeak', 'Schwach; Versuchen Sie Buchstaben und Zahlen zu kombinieren.');
-        $this->setLocalization('eng', 'login', 'passwordIsWeak', 'Weak; try combining letters & numbers.');
-        $this->setLocalization('ger', 'login', 'passwordIsMedium', 'Medium; Versuchen Sie Spezialzeichen.');
+        $this->setLocalization('ger', 'login', 'passwordTooShort', 'Das Passwort muss aus mindestens %s Zeichen bestehen.');
+        $this->setLocalization('eng', 'login', 'passwordTooShort', 'The password must have at least %s characters.');
+        $this->setLocalization('ger', 'login', 'passwordIsWeak', 'Schwach; versuchen Sie, Buchstaben und Zahlen zu kombinieren.');
+        $this->setLocalization('eng', 'login', 'passwordIsWeak', 'Weak; try combining letters and numbers.');
+        $this->setLocalization('ger', 'login', 'passwordIsMedium', 'Medium; versuchen Sie, Spezialzeichen zu verwenden.');
         $this->setLocalization('eng', 'login', 'passwordIsMedium', 'Medium; try using special characters.');
         $this->setLocalization('ger', 'login', 'passwordIsStrong', 'Starkes Passwort.');
         $this->setLocalization('eng', 'login', 'passwordIsStrong', 'Strong password.');
-        $this->setLocalization('ger', 'login', 'passwordhasUsername', 'Das Passwort enthält den Nutzernamen.');
-        $this->setLocalization('eng', 'login', 'passwordhasUsername', 'The password contains your username.');
+        $this->setLocalization('ger', 'login', 'passwordhasUsername', 'Das Passwort darf den Nutzernamen nicht enthalten.');
+        $this->setLocalization('eng', 'login', 'passwordhasUsername', 'The password must not contain your username.');
         $this->setLocalization('ger', 'login', 'typeYourPassword', 'Geben Sie ein Passwort ein.');
-        $this->setLocalization('eng', 'login', 'typeYourPassword', 'Type your password.');
+        $this->setLocalization('eng', 'login', 'typeYourPassword', 'Enter a password.');
 
         $this->execute(
             "UPDATE teinstellungen

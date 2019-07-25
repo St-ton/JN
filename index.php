e322fd490e109f40dd742927de4c1cbce8bb7262
Felix Moche <felix.moche@jtl-software.com>
2019-07-25 15:18:54 +0200

    dbg

--- index.php
+++ index.php
@@ -14,27 +14,6 @@ $NaviFilter = Shop::run();
 executeHook(HOOK_INDEX_NAVI_HEAD_POSTGET);
 Cart::checkAdditions();
 $file = Shop::getEntryPoint();
-
-//class foo
-//{
-//    public function test()
-//    {
-//        return rand(0,1)===2;
-//    }
-//}
-//
-//$res = [];
-//$res[] = new foo();
-//$res[] = new foo();
-//$res[] = new foo();
-//$res[] = new foo();
-//$res[] = new foo();
-//$res[] = new foo();
-//Shop::dbg(count($res),false, 'c1');
-//$res2 = some($res, function ($e) {
-//    return $e->test() === true;
-//});
-//Shop::dbg($res2);
 if ($file !== null && !Shop::$is404) {
     require PFAD_ROOT . basename($file);
 }

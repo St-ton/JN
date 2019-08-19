69063aef124d6f6382423013eb37f439cb9e2965
Felix Moche <felix.moche@jtl-software.com>
2019-08-19 18:03:12 +0200

    cs

--- includes/src/Plugin/MigrationHelper.php
+++ includes/src/Plugin/MigrationHelper.php
@@ -220,7 +220,7 @@ final class MigrationHelper
      * @param string $description
      * @param string $author
      * @return string
-     * @throws \SmartyException
+     * @throws \SmartyExceptionincludes/src/Plugin/MigrationHelper.php
      * @throws \Exception
      */
     public static function create(string $pluginDir, string $description, string $author): string

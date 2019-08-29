823834b9d440de63c7fb0a8ec66b3de41b9f1571
Falk Prüfer <falk.pruefer@jtl-software.com>
2019-08-29 17:05:54 +0200

    Aliasing old classes for backward compatibility.

--- includes/modules/fallback/FallBackPayment.php
+++ includes/modules/fallback/FallBackPayment.php
@@ -2,54 +2,7 @@
 /**
  * @copyright (c) JTL-Software-GmbH
  * @license http://jtl-url.de/jtlshoplicense
+ * @deprecated since 5.0.0 - use JTL\Plugin\Payment\FallbackMethod instead
  */
 
-include_once PFAD_ROOT . PFAD_INCLUDES_MODULES . 'PaymentMethod.class.php';
-
-/**
- * Class FallBackPayment
- *
- * FallBack-PaymentMethod (Modul-ID: za_null_jtl)
- * for a order that goes to 0.0 during the cashing of a shop-credit
- */
-class FallBackPayment extends PaymentMethod
-{
-    /**
-     * @param int $nAgainCheckout
-     * @return $this
-     */
-    public function init($nAgainCheckout = 0)
-    {
-        parent::init();
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isSelectable()
-    {
-        // this payment-method is always selectable
-        return true;
-    }
-
-    /**
-     * @param array $vArgs
-     * @return bool
-     */
-    public function isValidIntern($vArgs = [])
-    {
-        // this payment-method is always valid
-        return true;
-    }
-
-    /**
-     * @return bool
-     */
-    public function canPayAgain()
-    {
-        // the "payNow"-link (there's no reason for that)
-        return false;
-    }
-}
+class_alias(\JTL\Plugin\Payment\FallbackMethod::class, 'FallBackPayment', true);

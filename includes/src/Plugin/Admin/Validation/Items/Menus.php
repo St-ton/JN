35f8c09584da5fb2ea5f0b04e5e9dab9838eed60
Felix <felix.moche@jtl-software.com>
2019-09-19 12:09:00 +0000

    Apply suggestion to includes/src/Plugin/Admin/Validation/Items/Menus.php

--- includes/src/Plugin/Admin/Validation/Items/Menus.php
+++ includes/src/Plugin/Admin/Validation/Items/Menus.php
@@ -50,7 +50,7 @@ final class Menus extends AbstractItem
             \preg_match('/[0-9]+/', $i, $hits2);
             if (\mb_strlen($hits2[0]) === \mb_strlen($i)) {
                 \preg_match(
-                    '/[\w0-9\_\- ]+/u',
+                    '/[\w\- ]+/u',
                     $customLink['Name'],
                     $hits
                 );

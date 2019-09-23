6fc1c694f89b2f54f2c207a645947b20106fb0ba
Felix Moche <felix.moche@jtl-software.com>
2019-09-23 17:05:14 +0200

    support all image types

--- includes/src/dbeS/Push/ImageAPI.php
+++ includes/src/dbeS/Push/ImageAPI.php
@@ -6,12 +6,13 @@
 
 namespace JTL\dbeS\Push;
 
-use JTL\DB\ReturnType;
+use InvalidArgumentException;
 use JTL\Helpers\Request;
 use JTL\Media\Image;
-use JTL\Media\Image\Product;
+use JTL\Media\IMedia;
+use JTL\Media\Media;
+use JTL\Media\MediaImageRequest;
 use JTL\Shop;
-use stdClass;
 
 /**
  * Class ImageAPI
@@ -20,39 +21,97 @@ use stdClass;
 final class ImageAPI extends AbstractPush
 {
     /**
+     * @var string
+     */
+    private $imageType;
+
+    /**
+     * @var int
+     */
+    private $imageID = 0;
+
+    /**
      * @inheritdoc
      */
     public function getData()
     {
-        $productID = Request::verifyGPCDataInt('a');
-        $imageNo   = Request::verifyGPCDataInt('n');
-        $url       = Request::verifyGPCDataInt('url');
-        $size      = Request::verifyGPCDataInt('s');
-        if ($productID <= 0 || $imageNo <= 0 || $size <= 0) {
+        try {
+            $this->getImageType();
+        } catch (InvalidArgumentException $e) {
             return;
         }
-        $cgroup   = $this->db->select('tkundengruppe', 'cStandard', 'Y');
-        $cgroupID = (int)($cgroup->kKundengruppe ?? 0);
-        if ($cgroupID === 0) {
+        $instance = Media::getClass($this->imageType);
+        /** @var IMedia $instance */
+        $imageNo = Request::getInt('n', 1);
+        $path    = $instance::getPathByID($this->imageID, $imageNo);
+        if ($path === null) {
             return;
         }
-        foreach ($this->getProductImages($productID, $imageNo, $cgroupID) as $productImage) {
-            $image = Product::getThumb(
-                Image::TYPE_PRODUCT,
-                (int)$productImage->kArtikel,
-                $productImage,
-                $this->getSizePath($size),
-                (int)$productImage->nNr
-            );
-            if (!\file_exists($image)) {
-                Product::cacheImage(Product::toRequest($image));
-            }
-            if ($url === 1) {
-                echo Shop::getURL() . '/' . $image . "<br/>\n";
-            } else {
-                $this->displayImage(\PFAD_ROOT . $image);
-            }
+        $req   = MediaImageRequest::create([
+            'type'       => $this->imageType,
+            'id'         => $this->imageID,
+            'size'       => $this->getSizeByID(Request::verifyGPCDataInt('s')),
+            'number'     => $imageNo,
+            'ext'        => \pathinfo($path)['extension'],
+            'sourcePath' => $path
+        ]);
+        $names = $instance::getImageNames($req);
+        if (\count($names) === 0) {
+            return;
         }
+        $req->setName($names[0]);
+        $thumb = $req->getThumb();
+        if (!\file_exists(\PFAD_ROOT . $thumb)) {
+            $instance::cacheImage($req);
+        }
+        if (Request::verifyGPCDataInt('url') === 1) {
+            echo Shop::getURL() . '/' . $thumb;
+        } else {
+            $this->displayImage(\PFAD_ROOT . $thumb);
+        }
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    private function getImageType(): void
+    {
+        if (($id = Request::verifyGPCDataInt('a')) > 0) {
+            $this->imageType = Image::TYPE_PRODUCT;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('k')) > 0) {
+            $this->imageType = Image::TYPE_CATEGORY;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('h')) > 0) {
+            $this->imageType = Image::TYPE_CATEGORY;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('n')) > 0) {
+            $this->imageType = Image::TYPE_NEWS;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('nk')) > 0) {
+            $this->imageType = Image::TYPE_NEWSCATEGORY;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('m')) > 0) {
+            $this->imageType = Image::TYPE_CHARACTERISTIC_VALUE;
+            $this->imageID   = $id;
+            return;
+        }
+        if (($id = Request::verifyGPCDataInt('c')) > 0) {
+            $this->imageType = Image::TYPE_CHARACTERISTIC;
+            $this->imageID   = $id;
+            return;
+        }
+        throw new InvalidArgumentException('Invalid image type');
     }
 
     /**
@@ -60,56 +119,28 @@ final class ImageAPI extends AbstractPush
      */
     private function displayImage(string $imagePath): void
     {
-        $mime = $this->getMimeType($imagePath);
-        if ($mime !== null) {
+        if (($mime = $this->getMimeType($imagePath)) !== null) {
             \header('Content-type: ' . $mime);
             \readfile($imagePath);
         }
     }
 
     /**
-     * @param int $productID
-     * @param int $imageNo
-     * @param int $cgroupID
-     * @return stdClass[]
-     */
-    private function getProductImages(int $productID, int $imageNo, int $cgroupID): array
-    {
-        $noQry = $productID === $imageNo
-            ? ''
-            : ' AND tartikelpict.nNr = ' . $imageNo;
-
-        return $this->db->queryPrepared(
-            'SELECT tartikelpict.cPfad, tartikelpict.kArtikel, tartikel.cSeo, tartikelpict.nNr
-                FROM tartikelpict
-                JOIN tartikel
-                    ON tartikel.kArtikel = tartikelpict.kArtikel
-                LEFT JOIN tartikelsichtbarkeit
-                    ON tartikel.kArtikel = tartikelsichtbarkeit.kArtikel
-                    AND tartikelsichtbarkeit.kKundengruppe = :cgid
-                WHERE tartikelsichtbarkeit.kArtikel IS NULL
-                    AND tartikel.kArtikel = :pid' . $noQry,
-            ['cgid' => $cgroupID, 'pid' => $productID],
-            ReturnType::ARRAY_OF_OBJECTS
-        );
-    }
-
-    /**
      * @param string $imagePath
      * @return string|null
      */
-    private function getMimeType($imagePath): ?string
+    private function getMimeType(string $imagePath): ?string
     {
-        $data = \getimagesize($imagePath);
-
-        return $data['mime'] ?? null;
+        return \file_exists($imagePath)
+            ? \getimagesize($imagePath)['mime'] ?? null
+            : null;
     }
 
     /**
      * @param int $size
-     * @return string|null
+     * @return string
      */
-    private function getSizePath(int $size): ?string
+    private function getSizeByID(int $size): string
     {
         switch ($size) {
             case 1:

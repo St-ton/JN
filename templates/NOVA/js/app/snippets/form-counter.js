391baa48a679be15d2b38cbfaad36009acfef65c
Michael Hillmann <michael.hillmann@jtl-software.com>
2019-11-15 09:39:38 +0100

    colons

--- templates/NOVA/js/app/snippets/form-counter.js
+++ templates/NOVA/js/app/snippets/form-counter.js
@@ -1,24 +1,24 @@
 
 const Classes = {
 	counter	: 'form-counter'
-}
+};
 
 const Data = {
 	up		: 'data-count-up',
 	down	: 'data-count-down'
-}
+};
 
 
 const proxy = function(fn, ...args) {
 	return function() {
 		fn.call(this, ...args)
 	}
-}
+};
 
 const updateCount = function(increase = false) {
-	let $input = $(this).parents(`.${Classes.counter}`).find('.form-control')
-	let input = $input.get(0)
-	let stepDirection = increase ? 'stepUp' : 'stepDown'
+	let $input = $(this).parents(`.${Classes.counter}`).find('.form-control');
+	let input = $input.get(0);
+	let stepDirection = increase ? 'stepUp' : 'stepDown';
 
 	try {
 		input[stepDirection]()
@@ -27,23 +27,23 @@ const updateCount = function(increase = false) {
 
 		let newValue = increase ? parseFloat(input.value) + step : parseFloat(input.value) - step;
 
-		if(newValue > parseFloat(input.max)) return
-		if(newValue < parseFloat(input.min)) return
+		if(newValue > parseFloat(input.max)) return;
+		if(newValue < parseFloat(input.min)) return;
 
 		input.value = (newValue).toFixed($input.data('decimals'));
 	}
-}
+};
 
 window.initNumberInput = function () {
-	$(`[${Data.up}]`).on('click', proxy(updateCount, true))
-	$(`[${Data.down}]`).on('click', proxy(updateCount))
+	$(`[${Data.up}]`).on('click', proxy(updateCount, true));
+	$(`[${Data.down}]`).on('click', proxy(updateCount));
 	$(`.${Classes.counter} .form-control`).on('keyup blur', function() {
-		let min = parseInt(this.min)
-		let max = parseInt(this.max)
-		let value = parseInt(this.value)
+		let min = parseInt(this.min);
+		let max = parseInt(this.max);
+		let value = parseInt(this.value);
 
-		if(value < min || isNaN(value)) this.value = min
-		if(value > max) this.value = max
+		if(value < min || isNaN(value)) this.value = min;
+		if(value > max) this.value = max;
 	})
 };
 

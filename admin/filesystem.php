fec303dbf297e548f5840632e456a26fc904271d
Michael Hillmann <michael.hillmann@jtl-software.com>
2019-12-02 12:08:57 +0100

    check permission SHOP-837

--- admin/filesystem.php
+++ admin/filesystem.php
@@ -7,7 +7,6 @@
 /** @global \JTL\Smarty\JTLSmarty $smarty */
 
 require_once __DIR__ . '/includes/admininclude.php';
-$oAccount->redirectOnFailure();
 $oAccount->permission('FILESYSTEM_VIEW', true, true);
 
 use JTL\Alert\Alert;

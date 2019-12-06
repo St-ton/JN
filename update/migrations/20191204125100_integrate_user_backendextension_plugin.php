4cf07cc62a4d8f82986350aee07e156eb2eaff2e
Michael Hillmann <michael.hillmann@jtl-software.com>
2019-12-06 12:40:05 +0100

    deactivate old plugin SHOP-2621

--- update/migrations/20191204125100_integrate_user_backendextension_plugin.php
+++ update/migrations/20191204125100_integrate_user_backendextension_plugin.php
@@ -7,8 +7,13 @@
  * @created Wed, 04 Dec 2019 12:51:00 +0200
  */
 
+use JTL\DB\ReturnType;
+use JTL\Plugin\Admin\StateChanger;
+use JTL\Plugin\Admin\Validation\LegacyPluginValidator;
+use JTL\Plugin\Admin\Validation\PluginValidator;
 use JTL\Update\IMigration;
 use JTL\Update\Migration;
+use JTL\XMLParser;
 
 /**
  * Class Migration_20191204125100
@@ -20,6 +25,23 @@ class Migration_20191204125100 extends Migration implements IMigration
 
     public function up()
     {
+        $db              = $this->getDB();
+        $cache           = Shop::Container()->getCache();
+        $parser          = new XMLParser();
+        $legacyValidator = new LegacyPluginValidator($db, $parser);
+        $pluginValidator = new PluginValidator($db, $parser);
+        $stateChanger    = new StateChanger($db, $cache, $legacyValidator, $pluginValidator);
+
+        $res = $db->query(
+            "SELECT kPlugin
+                  FROM tplugin
+                  WHERE cPluginID = 'jtl_backenduser_extension'",
+            ReturnType::SINGLE_OBJECT
+        );
+        if (!empty($res)) {
+            $stateChanger->deactivate($res->kPlugin);
+        }
+
         $this->execute("
           UPDATE `tadminloginattribut`
             SET cAttribValue = 'N'

b4b4405139256f9d0e5f0528eb6e76991007e5e6
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2020-01-15 15:19:00 +0100

    Add migration that breaks redirect entry loops. SHOP-3391

---
+++ update/migrations/20200115104100_resolve_redirect_loops.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Resolve redirect loops
+ */
+
+/**
+ * Class Migration_20200115104100
+ */
+class Migration_20200115104100 extends Migration implements IMigration
+{
+    protected $author      = 'Danny Raufeisen';
+    protected $description = 'Resolve redirect loops';
+
+    public function up()
+    {
+        $shopPath = rtrim(parse_url(Shop::getURL())['path'] ?? '', '/') . '/';
+
+        $entries = $this->fetchAll(
+            "SELECT r2.kRedirect
+            FROM tredirect AS r1
+            JOIN tredirect AS r2
+            ON r2.cFromUrl = r1.cToUrl
+            OR r2.cFromUrl = CONCAT('{$shopPath}', r1.cToUrl)"
+        );
+
+        foreach ($entries as $entry) {
+            $kRedirect = $entry->kRedirect;
+            $chain     = [];
+
+            while ($kRedirect) {
+                $chain[] = $kRedirect;
+
+                $kRedirect = $this->fetchOne(
+                    "SELECT r2.kRedirect
+                        FROM tredirect AS r1
+                        JOIN tredirect AS r2
+                        ON r1.kRedirect = {$kRedirect}
+                        AND (r2.cFromUrl = r1.cToUrl
+                            OR r2.cFromUrl = CONCAT('{$shopPath}', r1.cToUrl)
+                        )"
+                )->kRedirect;
+
+                if (in_array($kRedirect, $chain)) {
+                    $this->execute("DELETE FROM tredirect WHERE kRedirect = {$kRedirect}");
+                    break;
+                }
+            }
+        }
+    }
+
+    public function down()
+    {
+    }
+}

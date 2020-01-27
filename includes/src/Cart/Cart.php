65f80fcf42b3ae87c0c369ae3a14d6688871f4cf
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-01-27 16:40:10 +0100

    check if shipping method possible SHOP-3168

--- includes/src/Cart/Cart.php
+++ includes/src/Cart/Cart.php
@@ -27,6 +27,7 @@ use JTL\Shop;
 use stdClass;
 use function Functional\select;
 use function Functional\some;
+use function Functional\map;
 
 /**
  * Class Warenkorb
@@ -1853,6 +1854,15 @@ class Cart
         $itemCount       = 0;
         $totalWeight     = 0;
         $shippingClasses = ShippingMethod::getShippingClasses(Frontend::getCart());
+        $poCode          = ($_SESSION['Lieferadresse']->cPLZ ?? null) ?? Frontend::getCustomer()->cPLZ;
+        $shippingMethods = map(ShippingMethod::getPossibleShippingMethods(
+            $_SESSION['Lieferadresse']->cLand ?? $_SESSION['Kunde']->cLand ?? $_SESSION['cLieferlandISO'],
+            $poCode,
+            $shippingClasses,
+            $customerGroupID
+        ), static function ($e) {
+            return $e->kVersandart;
+        });
 
         foreach ($this->PositionenArr as $item) {
             $totalWeight += $item->fGesamtgewicht;
@@ -1882,6 +1892,7 @@ class Cart
                     OR ( va.kVersandberechnung = 2 AND vas.fBis > 0 AND :totalWeight <= vas.fBis )
                     OR ( va.kVersandberechnung = 3 AND vas.fBis > 0 AND :maxPrices <= vas.fBis )
                     )
+                AND va.kVersandart IN (' . implode(', ', $shippingMethods) . ')
                 ORDER BY minPrice, nSort ASC LIMIT 1',
             [
                 'iso'         => '%' . $countryCode . '%',

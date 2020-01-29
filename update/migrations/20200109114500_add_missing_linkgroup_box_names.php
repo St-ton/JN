a131df1a2c612063f47b962014f0b1e5e3c19051
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-01-29 12:15:02 +0100

    use suggested sql SHOP-3276

--- update/migrations/20200109114500_add_missing_linkgroup_box_names.php
+++ update/migrations/20200109114500_add_missing_linkgroup_box_names.php
@@ -6,11 +6,8 @@
  * @created Thu, 9 Jan 2020 11:45:00 +0200
  */
 
-use JTL\DB\ReturnType;
 use JTL\Update\IMigration;
 use JTL\Update\Migration;
-use function Functional\group;
-use function Functional\reindex;
 
 /**
  * Class Migration_20200109114500
@@ -25,38 +22,18 @@ class Migration_20200109114500 extends Migration implements IMigration
      */
     public function up()
     {
-        $this->execute('ALTER TABLE tboxsprache ADD UNIQUE KEY `kBox_cISO` (kBox, cISO)');
-        $langs = ['ger', 'eng'];
-        $boxes = $this->getDB()->query(
-            'SELECT kBox, kCustomID FROM tboxen WHERE kCustomID != 0',
-            ReturnType::ARRAY_OF_OBJECTS
+        $this->execute(
+            "INSERT INTO tboxsprache (kBox, cISO, cTitel, cInhalt)
+                SELECT tboxen.kBox, tlinkgruppesprache.cISOSprache, tlinkgruppesprache.cName, ''
+                    FROM tboxen
+                    INNER JOIN tlinkgruppesprache
+                        ON tlinkgruppesprache.kLinkgruppe = tboxen.kCustomID
+                    LEFT JOIN tboxsprache
+                        ON tboxsprache.kBox = tboxen.kBox
+                        AND tboxsprache.cISO = tlinkgruppesprache.cISOSprache
+                    WHERE kCustomID > 0
+                        AND tboxsprache.kBoxSprache IS NULL;"
         );
-        $linkGroupLang = group(
-            $this->getDB()->query('SELECT * FROM tlinkgruppesprache', ReturnType::ARRAY_OF_OBJECTS),
-            function ($e) {
-                return $e->kLinkgruppe;
-            }
-        );
-        foreach ($linkGroupLang as &$lang) {
-            $lang = reindex($lang, function ($e) {
-                return $e->cISOSprache;
-            });
-        }
-
-        foreach ($boxes as $box) {
-            foreach ($langs as $lang) {
-                $this->execute(
-                    "INSERT IGNORE INTO tboxsprache (
-                          kBox, cISO, cTitel, cInhalt
-                      ) VALUES (
-                        " . $box->kBox . ",
-                        '" . $lang . "',
-                        '" . $linkGroupLang[(string)$box->kCustomID][(string)$lang]->cName ."',
-                        ''
-                    )"
-                );
-            }
-        }
     }
 
     /**
@@ -64,6 +41,5 @@ class Migration_20200109114500 extends Migration implements IMigration
      */
     public function down()
     {
-        $this->execute('ALTER TABLE tboxsprache DROP INDEX `kBox_cISO`');
     }
 }

de981b4a00e1c0fb3400a7785f4439494ac7900a
Marco Stickel <marco.stickel@jtl-software.com>
2020-01-30 12:33:22 +0100

    sanitizes error messages in 4.06. SHOP-3833

--- templates/Evo/js/global.js
+++ templates/Evo/js/global.js
@@ -170,6 +170,15 @@ function navigation()
     }
 }
 
+function sanitizeOutput(val) {
+    return val.replace(/\&/g, '&amp;')
+        .replace(/\</g, '&lt;')
+        .replace(/\>/g, '&gt;')
+        .replace(/\"/g, '&quot;')
+        .replace(/\'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
+}
+
 function addValidationListener() {
     var forms  = $('form.evo-validate'),
         inputs = $('form.evo-validate input,form.evo-validate select,form.evo-validate textarea'),
@@ -180,7 +189,7 @@ function addValidationListener() {
         forms[i].addEventListener('invalid', function (event) {
             event.preventDefault();
             $(event.target).closest('.form-group').find('div.form-error-msg').remove();
-            $(event.target).closest('.form-group').addClass('has-error').append('<div class="form-error-msg text-danger"><i class="fa fa-warning"></i> ' + event.target.validationMessage + '</div>');
+            $(event.target).closest('.form-group').addClass('has-error').append('<div class="form-error-msg text-danger"><i class="fa fa-warning"></i> ' + sanitizeOutput(event.target.validationMessage) + '</div>');
 
             if (!$body.data('doScrolling')) {
                 var $firstError = $(event.target).closest('.form-group.has-error');
@@ -230,7 +239,7 @@ function checkInputError(event)
         if ($equalsTo.length === 1) {
             var theOther = $equalsTo[0];
             if (theOther.value !== '' && theOther.value !== event.target.value && event.target.value !== '') {
-                event.target.setCustomValidity($target.data('custom-message') !== undefined ? $target.data('custom-message') : event.target.validationMessage);
+                event.target.setCustomValidity($target.data('custom-message') !== undefined ? $target.data('custom-message') : sanitizeOutput(event.target.validationMessage));
             } else {
                 event.target.setCustomValidity('');
             }
@@ -240,7 +249,7 @@ function checkInputError(event)
     if (event.target.validity.valid) {
         $target.closest('.form-group').removeClass('has-error');
     } else {
-        $target.closest('.form-group').addClass('has-error').append('<div class="form-error-msg text-danger"><i class="fa fa-exclamation-triangle"></i> ' + event.target.validationMessage + '</div>');
+        $target.closest('.form-group').addClass('has-error').append('<div class="form-error-msg text-danger"><i class="fa fa-exclamation-triangle"></i> ' + sanitizeOutput(event.target.validationMessage) + '</div>');
     }
 }
 

1b8deb52055d1db447e77d6bec5c874f33ab43a1
Felix Moche <felix.moche@jtl-software.com>
2020-02-24 16:35:57 +0100

    moved cache constants

--- includes/src/Cache/JTLCache.php
+++ includes/src/Cache/JTLCache.php
@@ -13,24 +13,6 @@ use JTL\Session\Frontend;
 use JTL\Shop;
 use JTL\Shopsetting;
 
-\define('CACHING_ROOT_DIR', __DIR__ . '/');
-\define('CACHING_METHODS_DIR', \CACHING_ROOT_DIR . 'CachingMethods/');
-\define('CACHING_GROUP_ARTICLE', 'art');
-\define('CACHING_GROUP_PRODUCT', 'art');
-\define('CACHING_GROUP_CATEGORY', 'cat');
-\define('CACHING_GROUP_LANGUAGE', 'lang');
-\define('CACHING_GROUP_TEMPLATE', 'tpl');
-\define('CACHING_GROUP_OPTION', 'opt');
-\define('CACHING_GROUP_PLUGIN', 'plgn');
-\define('CACHING_GROUP_CORE', 'core');
-\define('CACHING_GROUP_OBJECT', 'obj');
-\define('CACHING_GROUP_BOX', 'bx');
-\define('CACHING_GROUP_NEWS', 'nws');
-\define('CACHING_GROUP_ATTRIBUTE', 'attr');
-\define('CACHING_GROUP_MANUFACTURER', 'mnf');
-\define('CACHING_GROUP_FILTER', 'fltr');
-\define('CACHING_GROUP_FILTER_CHARACTERISTIC', 'fltrchr');
-
 /**
  * Class JTLCache
  * @package JTL\Cache

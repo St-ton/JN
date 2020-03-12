a7a6cea4d4187ab9a7480b707b9e3af48912db31
Felix Moche <felix.moche@jtl-software.com>
2020-03-12 11:17:59 +0100

    hide links

--- templates/NOVA/js/consent.youtube.js
+++ templates/NOVA/js/consent.youtube.js
@@ -6,10 +6,14 @@ document.addEventListener('consent.updated', function(e) {
 });
 function embedYoutube(detail) {
 	if (detail !== null && typeof detail.youtube !== 'undefined' && detail.youtube === true) {
-		var embeds = document.querySelectorAll('iframe.needs-consent.youtube');
-		for (var i = 0; i < embeds.length; ++i) {
+		let embeds = document.querySelectorAll('iframe.needs-consent.youtube');
+		for (let i = 0; i < embeds.length; ++i) {
 			embeds[i].src = embeds[i].dataset.src;
 			embeds[i].className = 'youtube';
+		}
+		let notices = document.querySelectorAll('a[data-consent="youtube"]');
+		for (let j = 0; j < notices.length; ++j) {
+			notices[j].classList.add('invisible');
 		}
 	}
 }

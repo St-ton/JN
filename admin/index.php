d4fa6b53f6cc0ba40c01283f3faf864daad4b493
Jan Ebert <jan.ebert@jtl-software.com>
2020-04-23 13:07:40 +0200

    added error if cookie not set SHOP-3841

--- admin/index.php
+++ admin/index.php
@@ -207,6 +207,9 @@ if ($oAccount->getIsAuthenticated()) {
         $alertHelper->addAlert(Alert::TYPE_ERROR, __('errorSessionExpired'), 'errorSessionExpired');
     }
     Shop::Container()->getGetText()->loadAdminLocale('pages/login');
+    if (!isset($_COOKIE['eSIdAdm']) || empty($_COOKIE['eSIdAdm'])) {
+        $alertHelper->addAlert(Alert::TYPE_ERROR, __('errorWrongCookieSettings'), 'errorWrongCookieSettings');
+    }
     $smarty->assign('uri', isset($_REQUEST['uri']) && mb_strlen(trim($_REQUEST['uri'])) > 0
         ? trim($_REQUEST['uri'])
         : '')

7a67ab8fa44f2390ae7cda15f488815ec4311f92
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-05-08 14:51:48 +0200

    fix update subject SHOP-4066

--- includes/src/Mail/Admin/Controller.php
+++ includes/src/Mail/Admin/Controller.php
@@ -14,6 +14,7 @@ use JTL\Mail\Mailer;
 use JTL\Mail\Template\Model;
 use JTL\Mail\Template\TemplateFactory;
 use JTL\Mail\Template\TemplateInterface;
+use JTL\Shop;
 use PHPMailer\PHPMailer\Exception;
 use stdClass;
 
@@ -384,14 +385,13 @@ final class Controller
             $convertText        = \mb_detect_encoding($upd->cContentText, ['UTF-8'], true) !== 'UTF-8';
             $upd->cContentHtml  = $convertHTML === true ? Text::convertUTF8($upd->cContentHtml) : $upd->cContentHtml;
             $upd->cContentText  = $convertText === true ? Text::convertUTF8($upd->cContentText) : $upd->cContentText;
-            $this->db->delete(
+            $updCount           = $this->db->update(
                 'temailvorlagesprache',
                 ['kEmailVorlage', 'kSprache'],
-                [$templateID, $lang->getId()]
+                [$templateID, $lang->getId()],
+                $upd
             );
-            if ($this->db->insert('temailvorlagesprache', $upd)) {
-                ++$affected;
-            }
+            $affected          += $updCount > 0 ? $updCount : 0;
         }
 
         return $affected;

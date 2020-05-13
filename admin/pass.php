5201226f2538cbdeb4bf0237e9cf9cc5f40249f7
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-05-13 11:48:51 +0200

    remove applicable, xss SHOP-4114

--- admin/pass.php
+++ admin/pass.php
@@ -37,12 +37,12 @@ if (isset($_POST['mail']) && Form::validateToken()) {
     } else {
         $alertHelper->addAlert(Alert::TYPE_ERROR, __('errorPasswordMismatch'), 'errorPasswordMismatch');
     }
-    $smarty->assign('fpwh', $_POST['fpwh'])
-           ->assign('fpm', $_POST['fpm']);
+    $smarty->assign('fpwh', Text::filterXSS($_POST['fpwh']))
+           ->assign('fpm', Text::filterXSS($_POST['fpm']));
     $step = 'confirm';
 } elseif (isset($_GET['fpwh'], $_GET['mail'])) {
-    $smarty->assign('fpwh', $_GET['fpwh'])
-           ->assign('fpm', $_GET['mail']);
+    $smarty->assign('fpwh', Text::filterXSS($_GET['fpwh']))
+           ->assign('fpm', Text::filterXSS($_GET['mail']));
     $step = 'confirm';
 }
 

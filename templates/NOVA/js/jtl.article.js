3f2c27def1d881a38c30c774ae81e1b1f2f7762e
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-06-19 14:38:38 +0200

    remove spin.js SHOP-4201

--- templates/NOVA/js/jtl.article.js
+++ templates/NOVA/js/jtl.article.js
@@ -549,8 +549,6 @@
                 id         = wrapper.substring(1),
                 $modalBody = $('.modal-body', this.modalView);
 
-            $wrapper.addClass('loading');
-
             $.ajax(url, {data: {'isAjax':1, 'quickView':1}})
                 .done(function(data) {
                     var $html      = $('<div />').html(data);
@@ -583,8 +581,7 @@
                     if ($config.length > 0) {
                         // Configurator in child article!? Currently not supported!
                         $config.remove();
-                        $modalBody.addClass('loading');
-                        var spinner = $.evo.extended().spinner($modalBody.get(0));
+                        $.evo.extended().startSpinner($modalBody);
                         location.href = url;
                     }
                     if (title.length > 0 && title.text().length > 0) {
@@ -615,7 +612,7 @@
                     }
                 })
                 .always(function() {
-                    $wrapper.removeClass('loading');
+                    $.evo.extended().stopSpinner();
                 });
         },
 
@@ -928,7 +925,7 @@
                     return;
                 }
 
-                let $spinner = $.evo.extended().spinner(container.get(0));
+                $.evo.extended().startSpinner(container);
 
                 $('#buy_form').find('*[data-selected="true"]')
                     .attr('checked', true)
@@ -937,7 +934,6 @@
 
                 form = $.evo.io().getFormValues('buy_form');
 
-                container.addClass('loading');
                 $.evo.io().call('buildConfiguration', [form], that, function (error, data) {
                     var result,
                         i,
@@ -987,8 +983,7 @@
                         $('#cfg-tab-summary-finish').children().addClass('disabled');
                         $('#cfg-tab-summary-finish').addClass('disabled');
                     }
-                    $spinner.stop();
-                    container.removeClass('loading');
+                    $.evo.extended().stopSpinner();
                     if (error) {
                         $.evo.error(data);
                         return;
@@ -1171,18 +1166,18 @@
 
         setArticleContent: function(id, variation, url, variations, wrapper) {
             var $wrapper  = this.getWrapper(wrapper),
-                listStyle = $('#ed_list.active').length > 0 ? 'list' : 'gallery',
-                $spinner  = $.evo.extended().spinner($wrapper.get(0));
+                listStyle = $('#ed_list.active').length > 0 ? 'list' : 'gallery';
+                $.evo.extended().startSpinner($wrapper);
 
             if (this.modalShown) {
                 this.loadModalArticle(url, wrapper,
                     function() {
                         var article = new ArticleClass();
                         article.register(wrapper);
-                        $spinner.stop();
+                        $.evo.extended().stopSpinner();
                     },
                     function() {
-                        $spinner.stop();
+                        $.evo.extended().stopSpinner();
                         $.evo.error('Error loading ' + url);
                     }
                 );
@@ -1198,13 +1193,12 @@
                     if (document.location.href !== url) {
                         history.pushState({a: id, a2: variation, url: url, variations: variations}, "", url);
                     }
-
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
 
                     window.initNumberInput();
                 }, function () {
                     $.evo.error('Error loading ' + url);
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
                 }, false, wrapper);
             } else {
                 $.evo.extended().loadContent(url + (url.indexOf('?') >= 0 ? '&' : '?') + 'isListStyle=' + listStyle, function (content) {
@@ -1228,7 +1222,7 @@
                     if (!$wrapper.hasClass('productbox-hover')) {
                         $.evo.extended().autoheight();
                     }
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
 
                     window.initNumberInput();
 
@@ -1240,7 +1234,7 @@
                     });
                 }, function () {
                     $.evo.error('Error loading ' + url);
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
                 }, false, wrapper);
             }
         },
@@ -1388,12 +1382,10 @@
                     value    = $current.data('value'),
                     io       = $.evo.io(),
                     args     = io.getFormValues(formID),
-                    $spinner = null,
                     $wrapper = this.getWrapper(wrapper);
 
                 if (animation) {
-                    $wrapper.addClass('loading');
-                    $spinner = $.evo.extended().spinner();
+                    $.evo.extended().startSpinner();
                 } else {
                     $('.updatingStockInfo', $wrapper).show();
                 }
@@ -1405,9 +1397,8 @@
                     .variationDispose(wrapper);
 
                 io.call('checkVarkombiDependencies', [args, key, value], $item, function (error, data) {
-                    $wrapper.removeClass('loading');
                     if (animation) {
-                        $spinner.stop();
+                        $.evo.extended().stopSpinner();
                     }
                     $('.updatingStockInfo', $wrapper).hide();
                     if (error) {
@@ -1421,12 +1412,10 @@
             var formID   = $item.closest('form').attr('id'),
                 $wrapper = this.getWrapper(wrapper),
                 io       = $.evo.io(),
-                args     = io.getFormValues(formID),
-                $spinner = null;
+                args     = io.getFormValues(formID);
 
             if (animation) {
-                $wrapper.addClass('loading');
-                $spinner = $.evo.extended().spinner();
+                $.evo.extended().startSpinner();
             }
 
             args.wrapper = wrapper;
@@ -1441,9 +1430,8 @@
                     $action.removeClass('on-list');
                 }
 
-                $wrapper.removeClass('loading');
                 if (animation) {
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
                 }
                 if (error) {
                     $.evo.error('checkDependencies');
--- templates/NOVA/js/jtl.evo.js
+++ templates/NOVA/js/jtl.evo.js
@@ -651,38 +651,15 @@
             });
         },
 
-        spinner: function(target) {
-            var opts = {
-              lines: 12             // The number of lines to draw
-            , length: 7             // The length of each line
-            , width: 5              // The line thickness
-            , radius: 10            // The radius of the inner circle
-            , scale: 2.0            // Scales overall size of the spinner
-            , corners: 1            // Roundness (0..1)
-            , color: '#000'         // #rgb or #rrggbb
-            , opacity: 1/4          // Opacity of the lines
-            , rotate: 0             // Rotation offset
-            , direction: 1          // 1: clockwise, -1: counterclockwise
-            , speed: 1              // Rounds per second
-            , trail: 100            // Afterglow percentage
-            , fps: 20               // Frames per second when using setTimeout()
-            , zIndex: 2e9           // Use a high z-index by default
-            , className: 'spinner'  // CSS class to assign to the element
-            , top: '50%'            // center vertically
-            , left: '50%'           // center horizontally
-            , shadow: false         // Whether to render a shadow
-            , hwaccel: false        // Whether to use hardware acceleration (might be buggy)
-            , position: 'absolute'  // Element positioning
-            };
-
-            if (typeof target === 'undefined') {
-                target = document.getElementsByClassName('product-offer')[0];
+        startSpinner: function (target) {
+            target = target || $('body');
+            if ($('.jtl-spinner').length === 0) {
+                target.append('<div class="jtl-spinner"><i class="fa fa-spinner fa-pulse"></i></div>');
             }
-            if ((typeof target !== 'undefined' && target.id === 'result-wrapper') || $(target).hasClass('product-offer')) {
-                opts.position = 'fixed';
-            }
+        },
 
-            return new Spinner(opts).spin(target);
+        stopSpinner: function () {
+            $('body').find('.jtl-spinner').remove();
         },
 
         trigger: function(event, args) {
@@ -738,10 +715,8 @@
         },
 
         updateWishlistItem: function($wrapper) {
-            let formID   = 'wl-items-form',
-                $spinner = $.evo.extended().spinner($wrapper.get(0));
-
-            $wrapper.addClass('loading');
+            let formID   = 'wl-items-form';
+            $.evo.extended().startSpinner($wrapper);
             $.evo.io().call(
                 'updateWishlistItem',
                 [
@@ -749,7 +724,7 @@
                     $.evo.io().getFormValues(formID)
                 ],
                 $(this) , function(error, data) {
-                    $spinner.stop();
+                    $.evo.extended().stopSpinner();
                     $wrapper.removeClass('loading');
                 });
         },
@@ -858,19 +833,17 @@
 
         initFilters: function (href) {
             let $wrapper = $('.js-collapse-filter'),
-                $spinner = $.evo.extended().spinner($wrapper.get(0))
                 self=this;
+            $.evo.extended().startSpinner($wrapper);
 
-            $wrapper.addClass('loading');
             $.ajax(href, {data: {'useMobileFilters':1}})
-            .done(function(data) {
-                $wrapper.html(data);
-                self.initPriceSlider($wrapper, false);
-            })
-            .always(function() {
-                $spinner.stop();
-                $wrapper.removeClass('loading');
-            });
+                .done(function(data) {
+                    $wrapper.html(data);
+                    self.initPriceSlider($wrapper, false);
+                })
+                .always(function() {
+                    $.evo.extended().stopSpinner();
+                });
         },
 
         initFilterEvents: function() {

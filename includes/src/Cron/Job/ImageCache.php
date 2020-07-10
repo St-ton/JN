28f6f4408a3e3b66190b1a638261aede0ea3148f
Felix Moche <felix.moche@jtl-software.com>
2020-07-10 19:02:14 +0200

    avoid timeouts, fixed finishing single jobs

--- includes/src/Cron/Job/ImageCache.php
+++ includes/src/Cron/Job/ImageCache.php
@@ -5,6 +5,7 @@ namespace JTL\Cron\Job;
 use JTL\Cron\Job;
 use JTL\Cron\JobInterface;
 use JTL\Cron\QueueEntry;
+use JTL\DB\ReturnType;
 use JTL\Media\IMedia;
 use JTL\Media\Media;
 
@@ -41,27 +42,33 @@ final class ImageCache extends Job
     private function generateImageCache(int $index, IMedia $instance): bool
     {
         $rendered = 0;
+        $limit    = $this->getLimit();
         $uncached = $instance->getUncachedImageCount();
-        $images   = $instance->getImages(true, $index, $this->getLimit());
+        $images   = $instance->getImages(true, $index, $limit);
         $totalAll = $instance->getTotalImageCount();
         $this->logger->debug(\sprintf('Uncached %s images: %d/%d', $instance::getType(), $uncached, $totalAll));
         if ($index >= $totalAll) {
             $index  = 0;
-            $images = $instance->getImages(true, $index, $this->getLimit());
+            $images = $instance->getImages(true, $index, $limit);
         }
         while (\count($images) === 0 && $index < $totalAll) {
-            $index += $this->getLimit();
-            $images = $instance->getImages(true, $index, $this->getLimit());
+            $index += $limit;
+            $images = $instance->getImages(true, $index, $limit);
         }
+        $thisRun = \count($images);
         foreach ($images as $image) {
             $instance->cacheImage($image);
             ++$index;
             ++$rendered;
+            $this->logger->debug(\sprintf('generated image %d/%d', $rendered, $thisRun));
+            if ($index % 10 === 0) {
+                $this->db->query('SELECT 1 AS avoidTimeout', ReturnType::DEFAULT);
+            }
         }
         $this->logger->debug(\sprintf('Generated cache for %d %s images', $rendered, $instance::getType()));
-        $this->nextIndex = $uncached === 0 ? 0 : $index;
+        $this->nextIndex = $uncached === 0 || ($uncached - $rendered === 0) ? 0 : $index;
 
-        return $uncached === 0;
+        return $this->nextIndex === 0;
     }
 
     /**
@@ -71,10 +78,9 @@ final class ImageCache extends Job
     {
         parent::start($queueEntry);
         $this->logger->debug(\sprintf('Generating image cache - max. %d', $this->getLimit()));
-        $media = Media::getInstance();
-        $res   = true;
-        foreach ($media->getRegisteredClasses() as $type) {
-            $res = $this->generateImageCache($queueEntry->tasksExecuted, $type) && $res;
+        $res = true;
+        foreach (Media::getInstance()->getRegisteredClasses() as $type) {
+            $res = $this->generateImageCache($queueEntry->tasksExecuted, new $type($this->db)) && $res;
         }
         $queueEntry->tasksExecuted = $this->nextIndex;
         $this->setFinished($res);

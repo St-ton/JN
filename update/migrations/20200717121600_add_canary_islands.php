9e5f57940561f464d6f5544baef6b324c31df620
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-07-17 14:18:20 +0200

    check if set SHOP-1864

--- update/migrations/20200717121600_add_canary_islands.php
+++ update/migrations/20200717121600_add_canary_islands.php
@@ -6,6 +6,7 @@
  * @created Fr, 17 July 2020 12:16:00 +0100
  */
 
+use JTL\DB\ReturnType;
 use JTL\Update\IMigration;
 use JTL\Update\Migration;
 
@@ -23,7 +24,10 @@ class Migration_20200717121600 extends Migration implements IMigration
      */
     public function up()
     {
-       $this->execute("INSERT INTO tland VALUES ('IC', 'Kanarische Inseln', 'Canary Islands', 1, 'Europa')");
+       $isSet = $this->getDB()->query("SELECT `cISO` FROM `tland` WHERE cISO = 'IC'", ReturnType::ARRAY_OF_OBJECTS);
+       if (empty($isSet)) {
+           $this->execute("INSERT INTO `tland` VALUES ('IC', 'Kanarische Inseln', 'Canary Islands', 1, 'Europa')");
+       }
     }
 
     /**
@@ -32,6 +36,6 @@ class Migration_20200717121600 extends Migration implements IMigration
      */
     public function down()
     {
-        $this->execute("DELETE FROM tland WHERE cISO = 'IC'");
+        $this->execute("DELETE FROM `tland` WHERE cISO = 'IC'");
     }
 }

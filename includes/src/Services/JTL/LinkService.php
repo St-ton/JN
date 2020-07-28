c2a5dcca8df2809e047e1c8ae3545e4dc054b718
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-07-28 11:11:56 +0200

    terms lang corrections SHOP-4262

--- includes/src/Services/JTL/LinkService.php
+++ includes/src/Services/JTL/LinkService.php
@@ -499,8 +499,10 @@ final class LinkService implements LinkServiceInterface
      */
     public function getAGBWRB(int $langID, int $customerGroupID)
     {
-        $linkAGB = null;
-        $linkWRB = null;
+        $linkAGB     = null;
+        $linkWRB     = null;
+        $linkWRBForm = null;
+        $conf        = Shop::getSettings([CONF_KAUFABWICKLUNG])['kaufabwicklung'];
         // kLink für AGB und WRB suchen
         foreach ($this->getSpecialPages() as $sp) {
             /** @var LinkInterface $sp */
@@ -508,6 +510,8 @@ final class LinkService implements LinkServiceInterface
                 $linkAGB = $sp;
             } elseif ($sp->getLinkType() === \LINKTYP_WRB) {
                 $linkWRB = $sp;
+            } elseif ($sp->getLinkType() === \LINKTYP_WRB_FORMULAR) {
+                $linkWRBForm = $sp;
             }
         }
         $data = $this->db->select(
@@ -523,10 +527,28 @@ final class LinkService implements LinkServiceInterface
         if (empty($data->kText)) {
             return false;
         }
-        $data->cURLAGB  = $linkAGB !== null ? $linkAGB->getURL() : '';
-        $data->cURLWRB  = $linkWRB !== null ? $linkWRB->getURL() : '';
-        $data->kLinkAGB = $linkAGB !== null ? $linkAGB->getID() : 0;
-        $data->kLinkWRB = $linkWRB !== null ? $linkWRB->getID() : 0;
+        $data->cURLAGB      = $linkAGB !== null ? $linkAGB->getURL() : '';
+        $data->cURLWRB      = $linkWRB !== null ? $linkWRB->getURL() : '';
+        $data->cURLWRBForm  = $linkWRB !== null ? $linkWRBForm->getURL() : '';
+        $data->kLinkAGB     = $linkAGB !== null ? $linkAGB->getID() : 0;
+        $data->kLinkWRB     = $linkWRB !== null ? $linkWRB->getID() : 0;
+        $data->kLinkWRBForm = $linkWRB !== null ? $linkWRBForm->getID() : 0;
+
+        $data->agbWrbNotice = $conf['bestellvorgang_wrb_anzeigen'] === '1'
+        ? sprintf(
+            Shop::Lang()->get('termsCancelationNotice', 'checkout'),
+            $data->cURLAGB,
+            $data->kLinkAGB > 0 ? 'class="popup"' : 'data-toggle="modal" data-target="#agb-modal" class="modal-popup"',
+            $data->cURLWRB,
+            $data->kLinkWRB > 0 ? 'class="popup"' : 'data-toggle="modal" data-target="#wrb-modal" class="modal-popup"',
+            $data->cURLWRBForm,
+            $data->kLinkWRBForm > 0 ? 'class="popup"' : 'data-toggle="modal" data-target="#wrb-form-modal" class="modal-popup"'
+        )
+        : sprintf(
+            Shop::Lang()->get('termsNotice', 'checkout'),
+            $data->cURLAGB,
+            $data->kLinkAGB > 0 ? 'class="popup"' : 'data-toggle="modal" data-target="#agb-modal" class="modal-popup"'
+        );
 
         return $data;
     }
--- update/migrations/20200727150100_lang_variables_misc_law_fixes.php
+++ update/migrations/20200727150100_lang_variables_misc_law_fixes.php
@@ -31,6 +31,12 @@ class Migration_20200727150100 extends Migration implements IMigration
         $this->setLocalization('ger', 'productDetails', 'suggestedPrice', 'Unverbindliche Preisempfehlung des Herstellers');
         $this->setLocalization('eng', 'productDetails', 'suggestedPrice', 'Manufacturers recommended retail price');
         $this->removeLocalization('suggestedPriceExpl', 'productDetails');
+
+        //SHOP-4262
+        $this->setLocalization('ger', 'checkout', 'termsNotice', 'Es gelten die <a href="%s" %s>Allgemeinen Geschäftsbedingungen</a>.');
+        $this->setLocalization('eng', 'checkout', 'termsNotice', 'The <a href="%s" %s>General Terms and Conditions</a> apply.');
+        $this->setLocalization('ger', 'checkout', 'termsCancelationNotice', 'Es gelten die <a href="%s" %s>Allgemeinen Geschäftsbedingungen</a> und die <a href="%s" %s>Widerrufsbelehrung</a>. Das <a href="%s" %s>Muster-Widerrufsformular</a> habe ich zur Kenntnis genommen.');
+        $this->setLocalization('eng', 'checkout', 'termsCancelationNotice', 'The <a href="%s" %s>General Terms and Conditions</a> apply. I have noted the <a href="%s" %s>withdrawal policy</a> as well as the <a href="%s" %s>model withdrawal form</a>.');
     }
 
     /**
@@ -48,5 +54,9 @@ class Migration_20200727150100 extends Migration implements IMigration
         $this->setLocalization('ger', 'productDetails', 'suggestedPriceExpl', '** Unverbindliche Preisempfehlung');
         $this->setLocalization('eng', 'productDetails', 'suggestedPrice', 'Manufacturers RRP');
         $this->setLocalization('eng', 'productDetails', 'suggestedPriceExpl', '** Recommended retail price');
+
+        //SHOP-4262
+        $this->removeLocalization('termsNotice', 'checkout');
+        $this->removeLocalization('termsCancelationNotice', 'checkout');
     }
 }

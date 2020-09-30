80ba494d0a5052bc20765daefc1653898772f694
Michael Hillmann <michael.hillmann@jtl-software.com>
2020-09-30 11:50:10 +0200

    refresh on success to show new plugin in menu SHOP-4411

--- admin/premiumplugin.php
+++ admin/premiumplugin.php
@@ -9,24 +9,31 @@ require_once __DIR__ . '/includes/admininclude.php';
 
 $oAccount->permission('PLUGIN_ADMIN_VIEW', true, true);
 /** @global \JTL\Smarty\JTLSmarty $smarty */
-$recommendations = new Manager(Shop::Container()->getAlertService(), Request::verifyGPDataString('scope'));
-$recommendation  = $recommendations->getRecommendationById(Request::verifyGPDataString('id'));
-$alertHelper     = Shop::Container()->getAlertService();
+$recommendationID = Request::verifyGPDataString('id');
+$recommendations  = new Manager(Shop::Container()->getAlertService(), Request::verifyGPDataString('scope'));
+$recommendation   = $recommendations->getRecommendationById($recommendationID);
+$alertHelper      = Shop::Container()->getAlertService();
 
 if (Request::verifyGPDataString('action') === 'install') {
     Shop::Container()->getGetText()->loadAdminLocale('pages/pluginverwaltung');
 
     $installer = new ExtensionInstaller(Shop::Container()->getDB());
     $installer->setRecommendations($recommendations->getRecommendations());
-    $errorMsg = $installer->onSaveStep([Request::verifyGPDataString('id')]);
+    $errorMsg = $installer->onSaveStep([$recommendationID]);
     if ($errorMsg === '') {
         $successMsg = Manager::SCOPE_BACKEND_PAYMENT_PROVIDER
             ? __('successInstallPaymentPlugin')
             : __('successInstallLegalPlugin');
-        $alertHelper->addAlert(Alert::TYPE_SUCCESS, $successMsg, 'successInstall');
-    } else {
-        $alertHelper->addAlert(Alert::TYPE_WARNING, $errorMsg, 'errorInstall');
+        $alertHelper->addAlert(
+            Alert::TYPE_SUCCESS,
+            $successMsg,
+            'successInstall',
+            ['fadeOut' => Alert::FADE_NEVER, 'saveInSession' => true]
+        );
+        header('Refresh:0');
+        exit;
     }
+    $alertHelper->addAlert(Alert::TYPE_WARNING, $errorMsg, 'errorInstall');
 }
 
 $smarty->assign('recommendation', $recommendation)

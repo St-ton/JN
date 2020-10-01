f035b5f21d3f6b441e0ec00a78146be436371ecd
Jan Ebert <jan.ebert@jtl-software.com>
2020-10-01 14:44:31 +0200

    Update build.sh

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -114,7 +114,7 @@ build_create_deleted_files_csv()
     fi
 
     cd ${REPOSITORY_DIR};
-    git pull;
+    git pull 2>&1 >/dev/null;
     git diff --name-status --diff-filter D tags/v4.03.0 ${REMOTE_STR}${APPLICATION_VERSION} -- ${REPOSITORY_DIR} ':!admin/classes' ':!classes' ':!includes/ext' ':!includes/plugins' > ${DELETE_FILES_CSV_FILENAME_TMP};
     cd ${CUR_PWD};
 

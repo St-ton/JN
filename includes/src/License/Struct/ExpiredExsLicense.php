c5a1eefbf9a96aaad809fc30e951d94cb3d9b346
Felix Moche <felix.moche@jtl-software.com>
2020-10-05 17:05:07 +0200

    tpl

--- includes/src/License/Struct/ExpiredExsLicense.php
+++ includes/src/License/Struct/ExpiredExsLicense.php
@@ -3,6 +3,8 @@
 namespace JTL\License\Struct;
 
 use DateTime;
+use JTL\Shop;
+use JTL\Template\Model;
 use JTLShop\SemVer\Version;
 use stdClass;
 
@@ -18,6 +20,7 @@ class ExpiredExsLicense extends ExsLicense
      */
     public function initFromPluginData(stdClass $data): void
     {
+        $this->setType(self::TYPE_PLUGIN);
         $this->setName($data->cName);
         $this->setExsID($data->exsID);
         $this->setQueryDate(new DateTime());
@@ -27,7 +30,6 @@ class ExpiredExsLicense extends ExsLicense
         $license->setExpired(true);
         $license->setCreated(new DateTime());
         $license->setType(License::TYPE_NONE);
-        $this->setType(self::TYPE_PLUGIN);
         $this->setLicense($license);
         $this->setID($data->cPluginID);
         $this->setState(self::STATE_ACTIVE);
@@ -44,6 +46,40 @@ class ExpiredExsLicense extends ExsLicense
         $ref->setInstalled(true);
         $ref->setInstalledVersion(Version::parse($data->nVersion));
         $ref->setDateInstalled($data->dInstalliert);
+        $this->setReferencedItem($ref);
+    }
+
+    /**
+     * @param Model $data
+     * @throws \Exception
+     */
+    public function initFromTemplateData(Model $data): void
+    {
+        $this->setType(self::TYPE_TEMPLATE);
+        $this->setName($data->cTemplate);
+        $this->setExsID($data->getExsID());
+        $this->setQueryDate(new DateTime());
+        $license = new License();
+        $license->setIsBound(true);
+        $license->setKey($data->cTemplate);
+        $license->setExpired(true);
+        $license->setCreated(new DateTime());
+        $license->setType(License::TYPE_NONE);
+        $this->setLicense($license);
+        $this->setID($data->cTemplate);
+        $this->setState(self::STATE_ACTIVE);
+        $subscription = new Subscription();
+        $subscription->setExpired(true);
+        $license->setSubscription($subscription);
+        $vendor = new Vendor();
+        $vendor->setName($data->getUrl());
+        $vendor->setHref($data->getUrl());
+        $this->setVendor($vendor);
+        $this->setLinks([]);
+        $ref = new ReferencedTemplate();
+        $ref->setInternalID($data->getTemplateID());
+        $ref->setInstalled(true);
+        $ref->setInstalledVersion(Version::parse($data->getVersion()));
         $this->setReferencedItem($ref);
     }
 }

c3e9e3e6c07ea56480f68b706b37611e01db561b
Daniel Raufison <danny.raufeisen@jtl-software.com>
2020-11-06 17:46:55 +0100

    Merge old redirects import format into new csv importer. SHOP-4632

--- admin/includes/csv_importer_inc.php
+++ admin/includes/csv_importer_inc.php
@@ -3,6 +3,8 @@
 use JTL\DB\ReturnType;
 use JTL\Helpers\Form;
 use JTL\Helpers\Request;
+use JTL\Helpers\URL;
+use JTL\Language\LanguageHelper;
 use JTL\Shop;
 
 /**
@@ -24,74 +26,141 @@ use JTL\Shop;
  */
 function handleCsvImportAction($importerId, $target, $fields = [], $delim = null, $importType = 2)
 {
-    if (Form::validateToken() && Request::verifyGPDataString('importcsv') === $importerId) {
-        if (isset($_FILES['csvfile']['type'])
-            && (
-                $_FILES['csvfile']['type'] === 'application/vnd.ms-excel'
-                || $_FILES['csvfile']['type'] === 'text/csv'
-                || $_FILES['csvfile']['type'] === 'application/csv'
-                || $_FILES['csvfile']['type'] === 'application/vnd.msexcel'
-            )
-        ) {
-            $csvFilename = $_FILES['csvfile']['tmp_name'];
-            $fs          = fopen($_FILES['csvfile']['tmp_name'], 'rb');
-            $nErrors     = 0;
+    if (Form::validateToken() === false || Request::verifyGPDataString('importcsv') !== $importerId) {
+        return -1;
+    }
 
-            if ($delim === null) {
-                $delim = getCsvDelimiter($csvFilename);
-            }
+    $csvMime = $_FILES['csvfile']['type'] ?? null;
 
-            if (count($fields) === 0) {
-                $fields = fgetcsv($fs, 0, $delim);
-            }
-
-            if (isset($_REQUEST['importType'])) {
-                $importType = Request::verifyGPCDataInt('importType');
-            }
-
-            if ($importType === 0 && is_string($target)) {
-                Shop::Container()->getDB()->query('TRUNCATE ' . $target, ReturnType::AFFECTED_ROWS);
-            }
-            $importDeleteDone = false;
-            while (($row = fgetcsv($fs, 0, $delim)) !== false) {
-                $obj = new stdClass();
-
-                foreach ($fields as $i => $field) {
-                    $row[$i]     = Shop::Container()->getDB()->escape($row[$i]);
-                    $obj->$field = $row[$i];
-                }
-
-                if (is_callable($target)) {
-                    $res = $target($obj, $importDeleteDone, $importType);
-
-                    if ($res === false) {
-                        ++$nErrors;
-                    }
-                } elseif (is_string($target)) {
-                    $table = $target;
-
-                    if ($importType === 0 || $importerId === 2) {
-                        $res = Shop::Container()->getDB()->insert($table, $obj);
-
-                        if ($res === 0) {
-                            ++$nErrors;
-                        }
-                    } elseif ($importType === 1) {
-                        Shop::Container()->getDB()->delete($target, $fields, $row);
-                        $res = Shop::Container()->getDB()->insert($table, $obj);
-
-                        if ($res === 0) {
-                            ++$nErrors;
-                        }
-                    }
-                }
-            }
-
-            return $nErrors;
-        }
-
+    if ($csvMime !== 'application/vnd.ms-excel'
+        && $csvMime !== 'text/csv'
+        && $csvMime !== 'application/csv'
+        && $csvMime !== 'application/vnd.msexcel'
+    ) {
         return 1;
     }
 
-    return -1;
+    $csvFilename       = $_FILES['csvfile']['tmp_name'];
+    $fs                = fopen($_FILES['csvfile']['tmp_name'], 'rb');
+    $nErrors           = 0;
+    $importDeleteDone  = false;
+    $oldRedirectFormat = false;
+    $mapArticleNumber  = false;
+    $defLanguage       = LanguageHelper::getDefaultLanguage();
+
+    if ($delim === null) {
+        $delim = getCsvDelimiter($csvFilename);
+    }
+
+    if (count($fields) === 0) {
+        $fields = fgetcsv($fs, 0, $delim);
+    }
+
+    foreach ($fields as &$field) {
+        if ($field === 'sourceurl') {
+            $field             = 'cFromUrl';
+            $oldRedirectFormat = true;
+        } elseif ($field === 'destinationurl') {
+            $field             = 'cToUrl';
+            $oldRedirectFormat = true;
+        } elseif ($field === 'articlenumber') {
+            $field             = 'cArtNr';
+            $oldRedirectFormat = true;
+            $mapArticleNumber  = true;
+        } elseif ($field === 'languageiso') {
+            $field             = 'cIso';
+            $oldRedirectFormat = true;
+            $mapArticleNumber  = true;
+        }
+    }
+
+    unset($field);
+
+    if (isset($_REQUEST['importType'])) {
+        $importType = Request::verifyGPCDataInt('importType');
+    }
+
+    if ($importType === 0 && is_string($target)) {
+        Shop::Container()->getDB()->query('TRUNCATE ' . $target, ReturnType::AFFECTED_ROWS);
+    }
+
+    while (($row = fgetcsv($fs, 0, $delim)) !== false) {
+        $obj = new stdClass();
+
+        foreach ($fields as $i => $field) {
+            $obj->$field = Shop::Container()->getDB()->escape($row[$i]);
+        }
+
+        if ($oldRedirectFormat) {
+            $parsed = \parse_url($obj->cFromUrl);
+            $from   = $parsed['path'];
+
+            if (isset($parsed['query'])) {
+                $from .= '?' . $parsed['query'];
+            }
+
+            $obj->cFromUrl = $from;
+        }
+
+        if ($mapArticleNumber) {
+            $obj->cToUrl = getArtNrUrl($obj->cArtNr, $obj->cIso ?? $defLanguage->cISO);
+            unset($obj->cArtNr, $obj->cIso);
+        }
+
+        if (is_callable($target)) {
+            $res = $target($obj, $importDeleteDone, $importType);
+
+            if ($res === false) {
+                ++$nErrors;
+            }
+        } elseif (is_string($target)) {
+            $table = $target;
+
+            if ($importType === 0 || $importerId === 2) {
+                $res = Shop::Container()->getDB()->insert($table, $obj);
+
+                if ($res === 0) {
+                    ++$nErrors;
+                }
+            } elseif ($importType === 1) {
+                Shop::Container()->getDB()->delete($target, $fields, $row);
+                $res = Shop::Container()->getDB()->insert($table, $obj);
+
+                if ($res === 0) {
+                    ++$nErrors;
+                }
+            }
+        }
+    }
+
+    return $nErrors;
+}
+
+/**
+ * @param string $artNo
+ * @param string $iso
+ * @return string|null
+ */
+function getArtNrUrl(string $artNo, string $iso): ?string
+{
+    if (\mb_strlen($artNo) === 0) {
+        return null;
+    }
+
+    $item = Shop::Container()->getDB()->executeQueryPrepared(
+        "SELECT tartikel.kArtikel, tseo.cSeo
+            FROM tartikel
+            LEFT JOIN tsprache
+                ON tsprache.cISO = :iso
+            LEFT JOIN tseo
+                ON tseo.kKey = tartikel.kArtikel
+                AND tseo.cKey = 'kArtikel'
+                AND tseo.kSprache = tsprache.kSprache
+            WHERE tartikel.cArtNr = :artno
+            LIMIT 1",
+        ['iso' => \mb_convert_case($iso, \MB_CASE_LOWER), 'artno' => $artNo],
+        ReturnType::SINGLE_OBJECT
+    );
+
+    return URL::buildURL($item, \URLART_ARTIKEL);
 }

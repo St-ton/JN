d9296a97aab2c7568a551e34af4af15cba26779c
Felix Moche <felix.moche@jtl-software.com>
2020-11-10 14:00:14 +0100

    map sort availability to default

--- includes/src/Mapper/SortingType.php
+++ includes/src/Mapper/SortingType.php
@@ -32,6 +32,7 @@ class SortingType
             case \SEARCH_SORT_NONE:
                 return None::class;
             case \SEARCH_SORT_STANDARD:
+            case \SEARCH_SORT_AVAILABILITY: // option remove in 5.0.0
                 return SortDefault::class;
             case \SEARCH_SORT_NAME_ASC:
                 return NameASC::class;

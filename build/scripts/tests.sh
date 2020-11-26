1b7e08a999780a3cd9d95433f7392c3f0e38e2ee
oskar <clemens.rudolph@jtl-software.com>
2020-11-26 10:30:15 +0000

    Update tests.sh

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -5,6 +5,8 @@ XGID=$2
 
 echo "] execute 'composer install'.."
 composer install -o -q -d includes/
+echo "] prepare all sources for re-use.."
+chown -R ${XUID}.${XGID} .
 
 echo "] check composer packages vulnerabilities..";
 ./includes/vendor/bin/security-checker security:check "includes/composer.lock"
@@ -17,6 +19,9 @@ for COMPONENT in build/components/*/ ; do
     echo "] execute 'composer install' for ${COMPONENT}.."
     composer install --no-plugins -a -o -q -d ${COMPONENT}
 
+    echo "] prepare all sources for re-use.."
+    chown -R ${XUID}.${XGID} .
+
     echo "] check composer packages vulnerabilities for ${COMPONENT}.."
     ./includes/vendor/bin/security-checker security:check "${COMPONENT}composer.lock"
     if [ $? -ne 0 ]; then
@@ -28,5 +33,4 @@ echo "] execute tests..";
 ./includes/vendor/bin/phpunit tests;
 echo "] unit tests finished."
 
-echo "] prepare all sources for re-use.."
-chown -R ${XUID}.${XGID} .
+

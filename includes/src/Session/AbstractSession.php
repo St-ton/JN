145b26cf451bc0641065d0b76800eff0ce4c48d2
Felix Moche <felix.moche@jtl-software.com>
2020-12-18 12:16:53 +0100

    removed handling for php < 7.3

--- includes/src/Session/AbstractSession.php
+++ includes/src/Session/AbstractSession.php
@@ -93,32 +93,17 @@ abstract class AbstractSession
      */
     private function setCookie(CookieConfig $cookieConfig): bool
     {
-        if (\PHP_VERSION_ID > 70300) {
-            $config = [
+        return \setcookie(
+            \session_name(),
+            \session_id(),
+            [
                 'expires'  => ($cookieConfig->getLifetime() === 0) ? 0 : \time() + $cookieConfig->getLifetime(),
                 'path'     => $cookieConfig->getPath(),
                 'domain'   => $cookieConfig->getDomain(),
                 'secure'   => $cookieConfig->isSecure(),
                 'httponly' => $cookieConfig->isHttpOnly(),
-            ];
-            if (\strlen($cookieConfig->getSameSite()) > 2) {
-                $config['samesite'] = $cookieConfig->getSameSite();
-            }
-
-            return \setcookie(
-                \session_name(),
-                \session_id(),
-                $config
-            );
-        }
-        return \setcookie(
-            \session_name(),
-            \session_id(),
-            ($cookieConfig->getLifetime() === 0) ? 0 : \time() + $cookieConfig->getLifetime(),
-            $cookieConfig->getPath(),
-            $cookieConfig->getDomain(),
-            $cookieConfig->isSecure(),
-            $cookieConfig->isHttpOnly()
+                'samesite' => $cookieConfig->getSameSite()
+            ]
         );
     }
 

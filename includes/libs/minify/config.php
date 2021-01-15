6b39617cc8c356fe54a3aa7e488d3d0a5e486c01
Felix Moche <felix.moche@jtl-software.com>
2021-01-15 12:30:31 +0100

    redis example

--- includes/libs/minify/config.php
+++ includes/libs/minify/config.php
@@ -65,8 +65,11 @@ $min_envArgs   = null;
  * To use APC/Memcache/ZendPlatform for cache storage, require the class and
  * set $min_cachePath to an instance. Example below:
  */
-//$min_cachePath = new Minify\Cache\APC();
-
+//$redis = new Redis();
+//$redis->connect('localhost', 6379, REDIS_CONNECT_TIMEOUT);
+//$redis->select(1);
+//$min_cachePath = new \Minify\Cache\Redis($redis);
+//$min_cachePath = new \Minify\Cache\APC();
 /**
  * Leave an empty string to use PHP's $_SERVER['DOCUMENT_ROOT'].
  *

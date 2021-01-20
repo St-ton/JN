75214fd1f5780419499617302a5043959a1c36c0
Falk@JTL <falk.pruefer@jtl-software.com>
2021-01-20 15:12:33 +0100

    Activate maintenance mode if safe mode is active. SHOP-4773

--- admin/pluginverwaltung.php
+++ admin/pluginverwaltung.php
@@ -438,6 +438,9 @@ if ($reload === true) {
 }
 
 $alert = Shop::Container()->getAlertService();
+if (SAFE_MODE) {
+    $alert->addAlert(Alert::TYPE_WARNING, __('Safe mode restrictions.'), 'warnSafeMode');
+}
 $alert->addAlert(Alert::TYPE_ERROR, $errorMsg, 'errorPlugin');
 $alert->addAlert(Alert::TYPE_NOTE, $notice, 'noticePlugin');
 

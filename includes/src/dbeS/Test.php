fdf36ee0a3917d60c0c9fdda003c94ac96b282b0
Falk@JTL <falk.pruefer@jtl-software.com>
2021-03-11 15:53:34 +0100

    Use explizit type conversion in conditions. SHOP-5060

--- includes/src/dbeS/Test.php
+++ includes/src/dbeS/Test.php
@@ -47,52 +47,24 @@ class Test
             );
         }
         $versionStr = null;
-        if (Request::postInt('kKunde') > 0) {
-            $state = $this->db->query(
-                "SHOW TABLE STATUS LIKE 'tkunde'",
-                ReturnType::SINGLE_OBJECT
-            );
-            if ($state->Auto_increment < (int)$_POST['kKunde']) {
-                $this->db->query(
-                    'ALTER TABLE tkunde AUTO_INCREMENT = ' . (int)$_POST['kKunde'],
-                    ReturnType::DEFAULT
+        foreach ([
+            'kKunde'           => 'tkunde',
+            'kBestellung'      => 'tbestellung',
+            'kLieferadresse'   => 'tlieferadresse',
+            'kZahlungseingang' => 'tzahlungseingang'
+        ] as $idField => $table) {
+            if (($id = Request::postInt($idField)) > 0) {
+                $state = $this->db->query(
+                    "SHOW TABLE STATUS LIKE '" . $table . "'",
+                    ReturnType::SINGLE_OBJECT
                 );
-            }
-        }
-        if (Request::postInt('kBestellung') > 0) {
-            $state = $this->db->query(
-                "SHOW TABLE STATUS LIKE 'tbestellung'",
-                ReturnType::SINGLE_OBJECT
-            );
-            if ($state->Auto_increment < (int)$_POST['kBestellung']) {
-                $this->db->query(
-                    'ALTER TABLE tbestellung AUTO_INCREMENT = ' . (int)$_POST['kBestellung'],
-                    ReturnType::DEFAULT
-                );
-            }
-        }
-        if (Request::postInt('kLieferadresse') > 0) {
-            $state = $this->db->query(
-                "SHOW TABLE STATUS LIKE 'tlieferadresse'",
-                ReturnType::SINGLE_OBJECT
-            );
-            if ($state->Auto_increment < (int)$_POST['kLieferadresse']) {
-                $this->db->query(
-                    'ALTER TABLE tlieferadresse AUTO_INCREMENT = ' . (int)$_POST['kLieferadresse'],
-                    ReturnType::DEFAULT
-                );
-            }
-        }
-        if (Request::postInt('kZahlungseingang') > 0) {
-            $state = $this->db->query(
-                "SHOW TABLE STATUS LIKE 'tzahlungseingang'",
-                ReturnType::SINGLE_OBJECT
-            );
-            if ($state->Auto_increment < (int)$_POST['kZahlungseingang']) {
-                $this->db->query(
-                    'ALTER TABLE tzahlungseingang AUTO_INCREMENT  = ' . (int)$_POST['kZahlungseingang'],
-                    ReturnType::DEFAULT
-                );
+                if (\is_object($state) && (int)$state->Auto_increment < $id) {
+                    $this->db->queryPrepared(
+                        'ALTER TABLE ' . $table . ' AUTO_INCREMENT = :newId',
+                        ['newId' => $id],
+                        ReturnType::DEFAULT
+                    );
+                }
             }
         }
         $version    = Shop::getShopDatabaseVersion();

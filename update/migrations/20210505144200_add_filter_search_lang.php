072e670a52ff0cea50fcd20eb4976e6d3f4b9cd5
Michael Hillmann <michael.hillmann@jtl-software.com>
2021-05-20 11:51:50 +0200

    add template setting SHOP-4828

--- update/migrations/20210505144200_add_filter_search_lang.php
+++ update/migrations/20210505144200_add_filter_search_lang.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+use JTL\Shop;
+use JTL\Template\Config;
 use JTL\Update\IMigration;
 use JTL\Update\Migration;
 
@@ -9,7 +11,7 @@ use JTL\Update\Migration;
 class Migration_20210505144200 extends Migration implements IMigration
 {
     protected $author      = 'mh';
-    protected $description = 'Add filter search lang';
+    protected $description = 'Add filter search lang and setting';
 
     /**
      * @inheritDoc
@@ -18,6 +20,15 @@ class Migration_20210505144200 extends Migration implements IMigration
     {
         $this->setLocalization('ger', 'productOverview', 'filterSearchPlaceholder', 'Suchen in %s');
         $this->setLocalization('eng', 'productOverview', 'filterSearchPlaceholder', 'Search in %s');
+
+        $template = Shop::Container()->getTemplateService()->getActiveTemplate(false);
+        $config   = new Config($template->getDir(), $this->getDB());
+        $settings = Shop::getSettings([\CONF_TEMPLATE])['template'];
+        if (!isset($settings['productlist']['filter_search_count'])
+            && ($template->getName() === 'NOVA' || $template->getParent() === 'NOVA')
+        ) {
+            $config->updateConfigInDB('productlist', 'filter_search_count', '20');
+        }
     }
 
     /**
@@ -26,5 +37,12 @@ class Migration_20210505144200 extends Migration implements IMigration
     public function down(): void
     {
         $this->removeLocalization('filterSearchPlaceholder', 'productOverview');
+
+        $this->execute(
+            "DELETE FROM ttemplateeinstellungen
+                WHERE cTemplate = 'NOVA'
+                  AND cName='filter_search_count'
+                  AND cSektion = 'productlist'"
+        );
     }
 }

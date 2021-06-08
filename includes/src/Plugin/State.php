072ec12adc62739d8ad2b8c646e0b1325bef76d8
Felix Moche <felix.moche@jtl-software.com>
2021-06-08 16:16:31 +0200

    deprecated

--- includes/src/Plugin/State.php
+++ includes/src/Plugin/State.php
@@ -26,8 +26,14 @@ class State extends Enum
 
     public const EXS_LICENSE_EXPIRED = 7;
 
+    /**
+     * @deprecated
+     */
     public const ESX_LICENSE_EXPIRED = 7;
 
+    /**
+     * @deprecated
+     */
     public const ESX_SUBSCRIPTION_EXPIRED = 8;
 
     public const EXS_SUBSCRIPTION_EXPIRED = 8;

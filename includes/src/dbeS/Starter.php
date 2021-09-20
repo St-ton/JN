86994e48aa2252a42d5f57035dc93e2f5181c0f1
Falk@JTL <falk.pruefer@jtl-software.com>
2021-09-20 11:52:18 +0200

    Undo further changes.

--- includes/src/dbeS/Starter.php
+++ includes/src/dbeS/Starter.php
@@ -416,17 +416,8 @@ class Starter
                 continue;
             }
             $data = \file_get_contents($xmlFile);
-            $gen  = static function ($string, $data) {
-                $result = $string ? \simplexml_load_string($data) : XML::unserialize($data);
 
-                return \is_array($result)
-                    ? \array_map(static function ($item) {
-                        return !\is_array($item) ? [] : $item;
-                    }, $result)
-                    : $result;
-            };
-
-            yield [$xmlFile => $gen($string, $data)];
+            yield [$xmlFile => $string ? \simplexml_load_string($data) : XML::unserialize($data)];
         }
     }
 

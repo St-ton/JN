716b51776838a29f70738b4219190c8f20835226
JulianL <julian.langer@jtl-software.com>
2021-09-27 16:32:27 +0200

    fixed codestyle and added class-doc

--- includes/src/Mail/Template/ProductTemplate.php
+++ includes/src/Mail/Template/ProductTemplate.php
@@ -2,10 +2,14 @@
 
 namespace JTL\Mail\Template;
 
+/**
+ * Class ProductTemplate
+ * @package JTL\Mail\Template
+ */
 class ProductTemplate extends AbstractTemplate
 {
     /**
-     * replace product-Name with original name from db.
+     * replace product name with original name from db.
      *
      * @param object $data
      *
@@ -13,12 +17,12 @@ class ProductTemplate extends AbstractTemplate
      */
     protected function useOriginalName(object $data): object
     {
-        if (\property_exists($data, 'tartikel') &&
-            \property_exists($data->tartikel, 'cName') &&
-            \property_exists($data->tartikel, 'originalName')) {
+        if (\property_exists($data, 'tartikel')
+            && \property_exists($data->tartikel, 'cName')
+            && \property_exists($data->tartikel, 'originalName')
+        ) {
             $data->tartikel->cName = $data->tartikel->originalName;
         }
-        
         return $data;
     }
 }

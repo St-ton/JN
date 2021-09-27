aa72ab3a5973cad87247997e54ccf0ebd8f62477
JulianL <julian.langer@jtl-software.com>
2021-09-27 15:51:50 +0200

    codestyle

--- includes/src/Mail/Template/ProductTemplate.php
+++ includes/src/Mail/Template/ProductTemplate.php
@@ -6,7 +6,7 @@ class ProductTemplate extends AbstractTemplate
 {
     protected function useOriginalName(object $data): object
     {
-        if (\property_exists($data, 'tartikel') && 
+        if (\property_exists($data, 'tartikel') &&
             \property_exists($data->tartikel, 'cName') &&
             \property_exists($data->tartikel, 'originalName')) {
             $data->tartikel->cName = $data->tartikel->originalName;

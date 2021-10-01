1ec3b23907d67d8d192eb8920e833d444fb9cf68
Felix Moche <felix.moche@jtl-software.com>
2021-10-01 14:52:47 +0200

    spaces

--- admin/includes/rss_inc.php
+++ admin/includes/rss_inc.php
@@ -30,19 +30,19 @@ function generiereRSSXML()
     // ISO-8859-1
     $xml = '<?xml version="1.0" encoding="' . JTL_CHARSET . '"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-	<channel>
-		<title>' . $conf['rss']['rss_titel'] . '</title>
-		<link>' . $shopURL . '</link>
-		<description>' . $conf['rss']['rss_description'] . '</description>
-		<language>' . Text::convertISO2ISO639($language->cISO) . '</language>
-		<copyright>' . $conf['rss']['rss_copyright'] . '</copyright>
-		<pubDate>' . date('r') . '</pubDate>
-		<atom:link href="' . $shopURL . '/rss.xml" rel="self" type="application/rss+xml" />
-		<image>
-			<url>' . $conf['rss']['rss_logoURL'] . '</url>
-			<title>' . $conf['rss']['rss_titel'] . '</title>
-			<link>' . $shopURL . '</link>
-		</image>';
+    <channel>
+        <title>' . $conf['rss']['rss_titel'] . '</title>
+        <link>' . $shopURL . '</link>
+        <description>' . $conf['rss']['rss_description'] . '</description>
+        <language>' . Text::convertISO2ISO639($language->cISO) . '</language>
+        <copyright>' . $conf['rss']['rss_copyright'] . '</copyright>
+        <pubDate>' . date('r') . '</pubDate>
+        <atom:link href="' . $shopURL . '/rss.xml" rel="self" type="application/rss+xml" />
+        <image>
+            <url>' . $conf['rss']['rss_logoURL'] . '</url>
+            <title>' . $conf['rss']['rss_titel'] . '</title>
+            <link>' . $shopURL . '</link>
+        </image>';
     //Artikel STD Sprache
     $lagerfilter = Shop::getProductFilter()->getFilterSQL()->getStockFilterSQL();
     $days        = (int)$conf['rss']['rss_alterTage'];
@@ -129,9 +129,9 @@ function generiereRSSXML()
     }
 
     $xml .= '
-	</channel>
+    </channel>
 </rss>
-		';
+        ';
 
     $file = fopen(PFAD_ROOT . FILE_RSS_FEED, 'w+');
     fwrite($file, $xml);
@@ -155,7 +155,7 @@ function bauerfc2822datum($dateString)
  * @param string $text
  * @return string
  */
-function wandelXMLEntitiesUm($text)
+function wandelXMLEntitiesUm($text): string
 {
     return mb_strlen($text) > 0
         ? '<![CDATA[ ' . Text::htmlentitydecode($text) . ' ]]>'

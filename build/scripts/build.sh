10f05a1b5f1a12f805529579376654ce602b9b27
oskar <clemens.rudolph@jtl-software.com>
2021-11-17 10:26:55 +0000

    Update build.sh, fix CA-check during submodule update

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -75,6 +75,7 @@ build_composer_execute()
 
 build_git_submodule_init()
 {
+    git config http.sslverify false;
     git submodule init -- ${REPOSITORY_DIR};
     git submodule update -- ${REPOSITORY_DIR};
 }

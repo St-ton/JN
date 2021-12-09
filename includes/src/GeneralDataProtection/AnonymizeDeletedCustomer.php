3e8c13f845eb14bfb2b75821adf29973719ba6e9
oskar.jtl <clemens.rudolph@jtl-software.com>
2021-12-09 16:09:15 +0100

    anonymize orphaned payments SHOP-5928

--- includes/src/GeneralDataProtection/AnonymizeDeletedCustomer.php
+++ includes/src/GeneralDataProtection/AnonymizeDeletedCustomer.php
@@ -68,15 +68,21 @@ class AnonymizeDeletedCustomer extends Method implements MethodInterface
                 AND z.cAbgeholt != 'N'
                 AND NOT EXISTS (
                     SELECT k.kKunde
-                    FROM tkunde k INNER JOIN tbestellung b ON k.kKunde = b.kKunde
-                    WHERE b.kBestellung = z.kBestellung
+                    FROM tkunde k 
+                        INNER JOIN tbestellung b ON k.kKunde = b.kKunde
+                    WHERE 
+                        b.kBestellung = z.kBestellung
+                        AND k.cKundenNr != :anonString
+                        AND k.cVorname != :anonString
+                        AND k.cNachname != :anonString
                 )
                 AND z.dZeit <= :dateLimit
             ORDER BY z.dZeit ASC
             LIMIT :workLimit",
             [
                 'dateLimit' => $this->dateLimit,
-                'workLimit' => $this->workLimit
+                'workLimit' => $this->workLimit,
+                'anonString' => Customer::CUSTOMER_ANONYM
             ]
         );
     }

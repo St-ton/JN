43873f859940ee5a68bc33bcc799c6eb373837dd
Felix Moche <felix.moche@jtl-software.com>
2022-02-16 14:01:42 +0100

    split price migration into two

--- update/migrations/20190415164325_remove_tpreise.php
+++ update/migrations/20190415164325_remove_tpreise.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Remove tpreise
  *
@@ -22,11 +22,6 @@ class Migration_20190415164325 extends Migration implements IMigration
      */
     public function up()
     {
-        $this->execute('DROP TABLE IF EXISTS tpreise');
-        $this->execute('ANALYZE TABLE tpreis');
-        $this->execute('ANALYZE TABLE tpreisverlauf');
-        $this->execute('UPDATE tpreis SET kKunde = 0 WHERE kKunde IS NULL');
-
         $this->execute(
             'DELETE FROM tpreis
                  WHERE kPreis IN (SELECT * FROM (
@@ -42,7 +37,6 @@ class Migration_20190415164325 extends Migration implements IMigration
             $this->execute('DROP INDEX kArtikel ON tpreis');
         }
         $this->execute('CREATE UNIQUE INDEX kArtikel on tpreis(kArtikel, kKundengruppe, kKunde)');
-
         $this->execute(
             'DELETE FROM tpreisverlauf
                  WHERE kPreisverlauf IN (SELECT * FROM (
@@ -58,7 +52,6 @@ class Migration_20190415164325 extends Migration implements IMigration
             $this->execute('DROP INDEX kArtikel ON tpreisverlauf');
         }
         $this->execute('CREATE UNIQUE INDEX kArtikel on tpreisverlauf(kArtikel, kKundengruppe, dDate)');
-
         $this->execute(
             'DELETE FROM tpreisdetail
                  WHERE kPreisDetail IN (SELECT * FROM (
@@ -92,24 +85,5 @@ class Migration_20190415164325 extends Migration implements IMigration
             $this->execute('DROP INDEX kArtikel ON tpreis');
         }
         $this->execute('CREATE INDEX kArtikel on tpreis(kArtikel, kKundengruppe, kKunde)');
-        $this->execute(
-            'CREATE TABLE tpreise (
-                kKundengruppe   INT UNSIGNED DEFAULT 0  NOT NULL,
-                kArtikel        INT UNSIGNED DEFAULT 0  NOT NULL,
-                fVKNetto        DOUBLE                      NULL,
-                nAnzahl1        INT UNSIGNED                NULL,
-                nAnzahl2        INT UNSIGNED                NULL,
-                nAnzahl3        INT UNSIGNED                NULL,
-                nAnzahl4        INT UNSIGNED                NULL,
-                nAnzahl5        INT UNSIGNED                NULL,
-                fPreis1         DOUBLE                      NULL,
-                fPreis2         DOUBLE                      NULL,
-                fPreis3         DOUBLE                      NULL,
-                fPreis4         DOUBLE                      NULL,
-                fPreis5         DOUBLE                      NULL,
-                PRIMARY KEY (kArtikel, kKundengruppe),
-                KEY fVKNetto (fVKNetto)
-            ) ENGINE = InnoDB COLLATE = utf8_unicode_ci'
-        );
     }
 }

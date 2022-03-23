f6be13ce0c3a45141db0838a5b39b05ef4964f92
Felix <felix.moche@jtl-software.com>
2022-03-23 15:51:01 +0100

    types, prepared

--- includes/src/Cron/Queue.php
+++ includes/src/Cron/Queue.php
@@ -16,22 +16,22 @@ class Queue
     /**
      * @var QueueEntry[]
      */
-    private $queueEntries = [];
+    private array $queueEntries = [];
 
     /**
      * @var DbInterface
      */
-    private $db;
+    private DbInterface $db;
 
     /**
      * @var JobFactory
      */
-    private $factory;
+    private JobFactory $factory;
 
     /**
      * @var LoggerInterface
      */
-    private $logger;
+    private LoggerInterface $logger;
 
     /**
      * Queue constructor.
@@ -75,7 +75,8 @@ class Queue
                 WHERE isRunning = 1
                     AND startTime <= NOW()
                     AND lastStart IS NOT NULL
-                    AND DATE_SUB(CURTIME(), INTERVAL ' . \QUEUE_MAX_STUCK_HOURS . ' Hour) > lastStart'
+                    AND DATE_SUB(CURTIME(), INTERVAL :ntrvl Hour) > lastStart',
+            ['ntrvl' => \QUEUE_MAX_STUCK_HOURS]
         );
     }
 

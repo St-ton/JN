eedc1748d7c0a4868b75068df7db2c843d354b03
Clemens <clemens.rudolph@googlemail.com>
2022-05-05 14:06:25 +0200

    logger removed

--- includes/src/Cron/Job/GeneralDataProtect.php
+++ includes/src/Cron/Job/GeneralDataProtect.php
@@ -39,12 +39,6 @@ final class GeneralDataProtect extends Job
      */
     public function start(QueueEntry $queueEntry): JobInterface
     {
-        // --DEBUG-- -------------------------------------------------------------
-        require_once('/www/shop5_02/includes/vendor/apache/log4php/src/main/php/Logger.php');
-        \Logger::configure('/www/shop5_02/_logging_conf.xml');
-        $oLogger = \Logger::getLogger('default');
-        // --DEBUG-- --------------------------------------------------------------
-
         parent::start($queueEntry);
         $this->foreignKey = (int)$queueEntry->foreignKey;
 
@@ -57,11 +51,6 @@ final class GeneralDataProtect extends Job
         if ($tableCleaner->getIsFinished()) {
             $this->setForeignKey((string)$this->foreignKey++);
         }
-        // if ($queueEntry->foreignKey < 0 || $queueEntry->foreignKey >= $tableCleaner->getMethodCount()) {
-        //     $this->setFinished(true);
-        // } else {
-        //     $this->setFinished(false);
-        // }
         $this->setFinished($this->foreignKey >= $tableCleaner->getMethodCount());
 
         return $this;

60e786706ffd7f670aa83e9ef1681c78d9249824
Felix <felix.moche@jtl-software.com>
2022-05-17 11:57:02 +0200

    lang routes

--- includes/src/Router/Router.php
+++ includes/src/Router/Router.php
@@ -6,6 +6,7 @@ use FastRoute\Dispatcher;
 use JTL\DB\DbInterface;
 use JTL\Events\Dispatcher as CoreDispatcher;
 use JTL\Events\Event;
+use JTL\Language\LanguageHelper;
 use JTL\Router\Controller\CategoryController;
 use JTL\Router\Controller\ConsentController;
 use JTL\Router\Controller\DefaultController;
@@ -31,6 +32,8 @@ use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Laminas\HttpHandlerRunner\Exception\EmitterException;
+use League\Route\Route;
+use League\Route\RouteGroup;
 use League\Route\Router as BaseRouter;
 use League\Route\Strategy\JsonStrategy;
 
@@ -75,12 +78,9 @@ class Router
         $consentController      = new ConsentController();
         $ioController           = new IOController($this->db, $cache, $state, $cgid, $conf, $alert);
 
-        $router = new BaseRouter();
-
+        $router          = new BaseRouter();
         $responseFactory = new ResponseFactory();
-
-        $strategy = new SmartyStrategy($responseFactory, Shop::Smarty(), $state);
-
+        $strategy        = new SmartyStrategy($responseFactory, Shop::Smarty(), $state);
         $router->setStrategy($strategy);
 
         $router->middleware(new MaintenanceModeMiddleware());
@@ -89,31 +89,69 @@ class Router
         $phpFileCheckMiddleware = new PhpFileCheckMiddleware();
         $visibilityMiddleware   = new VisibilityMiddleware();
 
-        $router->get('/products/{id:\d+}', [$productController, 'getResponse'])->middleware($visibilityMiddleware);
-        $router->get('/products/{name}', [$productController, 'getResponse'])->middleware($visibilityMiddleware);
-        $router->post('/products/{id:\d+}', [$productController, 'getResponse'])->middleware($visibilityMiddleware);
-        $router->post('/products/{name}', [$productController, 'getResponse'])->middleware($visibilityMiddleware);
+        $this->router  = $router;
+        $groups        = [''];
+        $languageCodes = [];
+        foreach (LanguageHelper::getAllLanguages() as $language) {
+            if (!\defined('URL_SHOP_' . mb_convert_case($language->getIso(), \MB_CASE_UPPER))) {
+                $languageCodes[] = $language->getIso639();
+            }
+        }
+        if (\count($languageCodes) > 1) {
+            $groups[] = '/{lang:(?:' . \implode('|', $languageCodes) . ')}';
+        }
+        foreach ($groups as $localized) {
+            $router->group($localized, function (RouteGroup $route) use (
+                $productController,
+                $visibilityMiddleware,
+                $categoryController,
+                $manufacturerController,
+                $newsController,
+                $pageController,
+                $localized
+            ) {
+                $route->get('/products/{id:\d+}', [$productController, 'getResponse'])
+                    ->setName('ROUTE_PRODUCT_BY_ID' . ($localized !== '' ? '_LOCALIZED' : ''))
+                    ->middleware($visibilityMiddleware);
+                $route->get('/products/{name}', [$productController, 'getResponse'])
+                    ->setName('ROUTE_PRODUCT_BY_NAME' . ($localized !== '' ? '_LOCALIZED' : ''))
+                    ->middleware($visibilityMiddleware);
+                $route->post('/products/{id:\d+}', [$productController, 'getResponse'])
+                    ->middleware($visibilityMiddleware);
+                $route->post('/products/{name}', [$productController, 'getResponse'])
+                    ->middleware($visibilityMiddleware);
 
-        $router->post('/_updateconsent', [$consentController, 'getResponse'])
+                $route->get('/categories/{id:\d+}', [$categoryController, 'getResponse'])
+                    ->setName('ROUTE_CATEGORY_BY_ID' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->get('/categories/{name}', [$categoryController, 'getResponse'])
+                    ->setName('ROUTE_CATEGORY_BY_NAME' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->post('/categories/{id:\d+}', [$categoryController, 'getResponse']);
+                $route->post('/categories/{name}', [$categoryController, 'getResponse']);
+
+                $route->get('/manufacturers/{id:\d+}', [$manufacturerController, 'getResponse'])
+                    ->setName('ROUTE_MANUFACTURER_BY_ID' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->get('/manufacturers/{name}', [$manufacturerController, 'getResponse'])
+                    ->setName('ROUTE_MANUFACTURER_BY_NAME' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->post('/manufacturers/{id:\d+}', [$manufacturerController, 'getResponse']);
+                $route->post('/manufacturers/{name}', [$manufacturerController, 'getResponse']);
+
+                $route->get('/news/{id:\d+}', [$newsController, 'getResponse'])
+                    ->setName('ROUTE_NEWS_BY_ID' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->get('/news/{name}', [$newsController, 'getResponse'])
+                    ->setName('ROUTE_NEWS_BY_NAME' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->post('/news/{id:\d+}', [$newsController, 'getResponse']);
+                $route->post('/news/{name}', [$newsController, 'getResponse']);
+
+                $route->get('/page/{id:\d+}', [$pageController, 'getResponse'])
+                    ->setName('ROUTE_PAGE_BY_ID' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->get('/page/{name}', [$pageController, 'getResponse'])
+                    ->setName('ROUTE_PAGE_BY_NAME' . ($localized !== '' ? '_LOCALIZED' : ''));
+                $route->post('/page/{id:\d+}', [$pageController, 'getResponse']);
+                $route->post('/page/{name}', [$pageController, 'getResponse']);
+            });
+        }
+        $router->post('/updateconsent', [$consentController, 'getResponse'])
             ->setStrategy(new JsonStrategy($responseFactory));
-
-        $router->get('/categories/{id:\d+}', [$categoryController, 'getResponse']);
-        $router->post('/categories/{id:\d+}', [$categoryController, 'getResponse']);
-        $router->get('/categories/{name}', [$categoryController, 'getResponse']);
-        $router->post('/categories/{name}', [$categoryController, 'getResponse']);
-
-        $router->get('/manufacturers/{id:\d+}', [$manufacturerController, 'getResponse']);
-        $router->post('/manufacturers/{id:\d+}', [$manufacturerController, 'getResponse']);
-        $router->get('/manufacturers/{name}', [$manufacturerController, 'getResponse']);
-        $router->post('/manufacturers/{name}', [$manufacturerController, 'getResponse']);
-
-        $router->get('/news/{id:\d+}', [$newsController, 'getResponse']);
-        $router->post('/news/{id:\d+}', [$newsController, 'getResponse']);
-
-        $router->get('/page/{id:\d+}', [$pageController, 'getResponse']);
-        $router->post('/page/{id:\d+}', [$pageController, 'getResponse']);
-        $router->get('/page/{name}', [$pageController, 'getResponse']);
-        $router->post('/page/{name}', [$pageController, 'getResponse']);
 
         $router->get('/io', [$ioController, 'getResponse']);
         $router->post('/io', [$ioController, 'getResponse']);
@@ -124,10 +162,8 @@ class Router
         $router->get('/', [$rootController, 'getResponse']);
         $router->post('/', [$rootController, 'getResponse']);
 
-        $router->get('/{any:.*}', [$defaultController, 'getResponse']);
-        $router->post('/{any:.*}', [$defaultController, 'getResponse']);
-
-        $this->router = $router;
+//        $router->get('/{any:.*}', [$defaultController, 'getResponse']);
+//        $router->post('/{any:.*}', [$defaultController, 'getResponse']);
     }
 
     /**
@@ -149,6 +185,76 @@ class Router
         }
 
         return $uri;
+    }
+
+    /**
+     * @param string     $name
+     * @param array|null $replacements
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function getNamedPath(string $name, ?array $replacements = null): string
+    {
+        $path = $this->router->getNamedRoute($name)->getPath();
+
+        return $replacements === null ? $path : $this->getPath($path, $replacements);
+    }
+
+    /**
+     * fixed version of League\Route::getPath() with replacements
+     *
+     * @param string $path
+     * @param array  $replacements
+     * @return string
+     */
+    protected function getPath(string $path, array $replacements): string
+    {
+        $hasReplacementRegex = '/{(' . \implode('|', \array_keys($replacements)) . ')(:.*)?}/';
+
+        \preg_match_all('/\[(.*?)?{(?<keys>.*?)}/', $path, $matches);
+
+        $isOptionalRegex = '/(.*)?{('
+            . \implode('|', $matches['keys'])
+            . ')(:.*)?}(.*)?/';
+
+        $isPartiallyOptionalRegex = '/^([^\[\]{}]+)?\[((?:.*)?{(?:'
+            . \implode('|', $matches['keys'])
+            . ')(?::.*)?}(?:.*)?)\]?([^\[\]{}]+)?(?:[\[\]]+)?$/';
+
+        $toReplace = [];
+
+        foreach ($replacements as $wildcard => $actual) {
+            $toReplace['/{' . \preg_quote($wildcard, '/') . '(:[^\}]*)?}/'] = $actual;
+        }
+        $segments = [];
+
+        foreach (\array_filter(\explode('/', $path)) as $segment) {
+            // segment is partially optional with a wildcard, strip it if no match, tidy up if match
+            if (\preg_match($isPartiallyOptionalRegex, $segment)) {
+                $segment = \preg_match($hasReplacementRegex, $segment)
+                    ? \preg_replace($isPartiallyOptionalRegex, '$1$2$3', $segment)
+                    : \preg_replace($isPartiallyOptionalRegex, '$1', $segment);
+            }
+
+            // segment either isn't a wildcard or there is a replacement
+            if (!\preg_match('/{(.*?)}/', $segment) || \preg_match($hasReplacementRegex, $segment)) {
+                $segments[] = \preg_replace(['/\[$/', '/\]+$/'], '', $segment);
+                continue;
+            }
+
+            // segment is a required wildcard, no replacement, still gets added
+            if (!\preg_match($isOptionalRegex, $segment)) {
+                $segments[] = \preg_replace(['/\[$/', '/\]+$/'], '', $segment);
+                continue;
+            }
+
+            // segment is completely optional with no replacement, strip it and break
+            if (\preg_match($isOptionalRegex, $segment) && !\preg_match($hasReplacementRegex, $segment)) {
+                break;
+            }
+        }
+
+        return \preg_replace(\array_keys($toReplace), \array_values($toReplace), '/' . \implode('/', $segments));
     }
 
     /**

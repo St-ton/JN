212628ba1ee32977290c64f489a373a82266bc77
Felix Moche <felix.moche@jtl-software.com>
2022-06-08 11:50:06 +0200

    cleanup, deprecations

--- includes/globalinclude.php
+++ includes/globalinclude.php
@@ -9,7 +9,7 @@ use JTL\Router\Router;
 use JTL\Router\State;
 use JTL\Session\Frontend;
 use JTL\Shop;
-use JTLShop\SemVer\Version;
+use JTL\Shopsetting;
 
 $nStartzeit = microtime(true);
 
@@ -37,33 +37,25 @@ if (defined('PFAD_ROOT')) {
 
 require_once PFAD_ROOT . PFAD_INCLUDES . 'autoload.php';
 
-defined('DB_HOST') || handleFatal('Kein MySql-Datenbank Host angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
+defined('DB_HOST') || handleFatal('Kein MySql-Datenbankhost angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
 defined('DB_NAME') || handleFatal('Kein MySql Datenbanknamen angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
-defined('DB_USER') || handleFatal('Kein MySql-Datenbank Benutzer angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
-defined('DB_PASS') || handleFatal('Kein MySql-Datenbank Passwort angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
-
-define(
-    'JTL_VERSION',
-    (int)sprintf(
-        '%d%02d',
-        Version::parse(APPLICATION_VERSION)->getMajor(),
-        Version::parse(APPLICATION_VERSION)->getMinor()
-    )
-); // DEPRECATED since 5.0.0
-define('JTL_MINOR_VERSION', (int)Version::parse(APPLICATION_VERSION)->getPatch()); // DEPRECATED since 5.0.0
+defined('DB_USER') || handleFatal('Kein MySql-Datenbankbenutzer angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
+defined('DB_PASS') || handleFatal('Kein MySql-Datenbankpasswort angegeben. Bitte config.JTL-Shop.ini.php bearbeiten!');
 
 Profiler::start();
 
-$db     = null;
-$config = null;
-$shop   = Shop::getInstance();
+$db    = null;
+$cache = null;
+$shop  = Shop::getInstance();
 
 if (!function_exists('Shop')) {
     /**
      * @return Shop
+     * @deprecated since 5.2.0
      */
     function Shop(): Shop
     {
+        trigger_error(__METHOD__ . ' is deprecated and should not be used anymore.', E_USER_DEPRECATED);
         return Shop::getInstance();
     }
 }
@@ -80,11 +72,11 @@ try {
 if (!defined('CLI_BATCHRUN')) {
     $cache = Shop::Container()->getCache();
     $cache->setJtlCacheConfig($db->selectAll('teinstellungen', 'kEinstellungenSektion', CONF_CACHING));
-    $config = Shop::getSettingSection(CONF_GLOBAL);
     $lang   = LanguageHelper::getInstance($db, $cache);
 }
+$config = Shopsetting::getInstance()->getAll();
 if (PHP_SAPI !== 'cli'
-    && $config['kaufabwicklung_ssl_nutzen'] === 'P'
+    && $config['global']['kaufabwicklung_ssl_nutzen'] === 'P'
     && (!isset($_SERVER['HTTPS'])
         || (mb_convert_case($_SERVER['HTTPS'], MB_CASE_LOWER) !== 'on' && (int)$_SERVER['HTTPS'] !== 1))
 ) {
@@ -106,7 +98,7 @@ if (PHP_SAPI !== 'cli'
 if (!JTL_INCLUDE_ONLY_DB && !defined('CLI_BATCHRUN')) {
     $debugbar = Shop::Container()->getDebugBar();
     require_once PFAD_ROOT . PFAD_INCLUDES . 'sprachfunktionen.php';
-    Shop::setRouter(new Router($db, $cache ?? Shop::Container()->getCache(), new State()));
+    Shop::setRouter(new Router($db, $cache, new State(), Shop::Container()->getAlertService(), $config));
     $globalMetaData = Metadata::getGlobalMetaData();
     $session        = (defined('JTLCRON') && JTLCRON === true)
         ? Frontend::getInstance(true, true, 'JTLCRON')

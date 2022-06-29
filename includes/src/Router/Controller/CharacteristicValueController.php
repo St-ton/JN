a377301925321a25dea60da401367b9a8cf8b24a
Felix Moche <felix.moche@jtl-software.com>
2022-06-29 16:38:06 +0200

    cv controller

---
+++ includes/src/Router/Controller/CharacteristicValueController.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+
+namespace JTL\Router\Controller;
+
+use JTL\Catalog\Category\Kategorie;
+use JTL\Catalog\Category\KategorieListe;
+use JTL\Catalog\Product\ArtikelListe;
+use JTL\Catalog\Product\Bestseller;
+use JTL\Extensions\SelectionWizard\Wizard;
+use JTL\Filter\Metadata;
+use JTL\Filter\Pagination\ItemFactory;
+use JTL\Filter\Pagination\Pagination;
+use JTL\Helpers\Category;
+use JTL\Helpers\Product;
+use JTL\Helpers\Request;
+use JTL\Router\DefaultParser;
+use JTL\Router\State;
+use JTL\Session\Frontend;
+use JTL\Shop;
+use JTL\Smarty\JTLSmarty;
+use Laminas\Diactoros\Response\RedirectResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use stdClass;
+
+/**
+ * Class CharacteristicValueController
+ * @package JTL\Router\Controller
+ */
+class CharacteristicValueController extends ProductListController
+{
+    /**
+     * @inheritdoc
+     */
+    public function getStateFromSlug(array $args): State
+    {
+        $id   = (int)($args['id'] ?? 0);
+        $name = $args['name'] ?? null;
+        if ($id < 1 && $name === null) {
+            return $this->state;
+        }
+        if ($name !== null) {
+            $parser = new DefaultParser($this->db, $this->state);
+            $name   = $parser->parse($name);
+        }
+        $seo = $id > 0
+            ? $this->db->getSingleObject(
+                'SELECT *
+                    FROM tseo
+                    WHERE cKey = :key
+                      AND kKey = :kid
+                      AND kSprache = :lid',
+                ['key' => 'kMerkmalWert', 'kid' => $id, 'lid' => $this->state->languageID]
+            )
+            : $this->db->getSingleObject(
+                'SELECT *
+                    FROM tseo
+                    WHERE cKey = :key AND cSeo = :seo',
+                ['key' => 'kMerkmalWert', 'seo' => $name]
+            );
+        if ($seo === null) {
+            return $this->handleSeoError($id, $this->state->languageID);
+        }
+        $slug          = $seo->cSeo;
+        $seo->kKey     = (int)$seo->kKey;
+        $seo->kSprache = (int)$seo->kSprache;
+
+        return $this->updateState($seo, $slug);
+    }
+
+    /**
+     * @param int $id
+     * @param int $languageID
+     * @return State
+     */
+    private function handleSeoError(int $id, int $languageID): State
+    {
+        if ($id > 0) {
+            $exists = $this->db->getSingleObject(
+                'SELECT kMerkmalWert
+                    FROM tmerkmalwert
+                    WHERE kMerkmalWert = :pid',
+                ['pid' => $id]
+            );
+            if ($exists !== null) {
+                $seo = (object)[
+                    'cSeo'     => '',
+                    'cKey'     => 'kMerkmalWert',
+                    'kKey'     => $id,
+                    'kSprache' => $languageID
+                ];
+
+                return $this->updateState($seo, $seo->cSeo);
+            }
+        }
+        $this->state->is404 = true;
+
+        return $this->updateProductFilter();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getResponse(ServerRequestInterface $request, array $args, JTLSmarty $smarty): ResponseInterface
+    {
+        $this->getStateFromSlug($args);
+        if (!$this->init()) {
+            return $this->notFoundResponse($request, $args, $smarty);
+        }
+
+        return parent::getResponse($request, $args, $smarty);
+    }
+}

f6b8f65bfb13b329a5067e62432ec9ab86f996af
Felix <felix.moche@jtl-software.com>
2022-07-11 14:19:57 +0200

    types, cs

--- includes/src/Boxes/Items/UpcomingProducts.php
+++ includes/src/Boxes/Items/UpcomingProducts.php
@@ -22,45 +22,46 @@ final class UpcomingProducts extends AbstractBox
         parent::__construct($config);
         $this->setShow(false);
         $customerGroupID = Frontend::getCustomerGroup()->getID();
-        if ($customerGroupID > 0 && Frontend::getCustomerGroup()->mayViewCategories()) {
-            $cached         = true;
-            $cacheTags      = [\CACHING_GROUP_BOX, \CACHING_GROUP_ARTICLE];
-            $stockFilterSQL = Shop::getProductFilter()->getFilterSQL()->getStockFilterSQL();
-            $parentSQL      = ' AND tartikel.kVaterArtikel = 0';
-            $limit          = (int)$config['boxen']['box_erscheinende_anzahl_basis'];
-            $cacheID        = 'box_ucp_' . $customerGroupID . '_' . $limit . \md5($stockFilterSQL . $parentSQL);
-            if (($productIDs = Shop::Container()->getCache()->get($cacheID)) === false) {
-                $cached     = false;
-                $productIDs = Shop::Container()->getDB()->getInts(
-                    'SELECT tartikel.kArtikel
-                        FROM tartikel
-                        LEFT JOIN tartikelsichtbarkeit 
-                            ON tartikel.kArtikel = tartikelsichtbarkeit.kArtikel
-                            AND tartikelsichtbarkeit.kKundengruppe = :cid
-                        WHERE tartikelsichtbarkeit.kArtikel IS NULL ' .
-                            $stockFilterSQL . ' ' .
-                            $parentSQL . '
-                            AND NOW() < tartikel.dErscheinungsdatum
-                        LIMIT :lmt',
-                    'kArtikel',
-                    ['cid' => $customerGroupID, 'lmt' => $limit]
-                );
-                Shop::Container()->getCache()->set($cacheID, $productIDs, $cacheTags);
-            }
-            \shuffle($productIDs);
-            $res = \array_slice($productIDs, 0, $config['boxen']['box_erscheinende_anzahl_anzeige']);
-            if (\count($res) > 0) {
-                $this->setShow(true);
-                $products = new ArtikelListe();
-                $products->getArtikelByKeys($res, 0, \count($res));
-                $this->setProducts($products);
-                $this->setURL(SearchSpecial::buildURL(\SEARCHSPECIALS_UPCOMINGPRODUCTS));
-                \executeHook(\HOOK_BOXEN_INC_ERSCHEINENDEPRODUKTE, [
-                    'box'        => &$this,
-                    'cache_tags' => &$cacheTags,
-                    'cached'     => $cached
-                ]);
-            }
+        if ($customerGroupID <= 0 || !Frontend::getCustomerGroup()->mayViewCategories()) {
+            return;
+        }
+        $cached         = true;
+        $cacheTags      = [\CACHING_GROUP_BOX, \CACHING_GROUP_ARTICLE];
+        $stockFilterSQL = Shop::getProductFilter()->getFilterSQL()->getStockFilterSQL();
+        $parentSQL      = ' AND tartikel.kVaterArtikel = 0';
+        $limit          = (int)$config['boxen']['box_erscheinende_anzahl_basis'];
+        $cacheID        = 'box_ucp_' . $customerGroupID . '_' . $limit . \md5($stockFilterSQL . $parentSQL);
+        if (($productIDs = Shop::Container()->getCache()->get($cacheID)) === false) {
+            $cached     = false;
+            $productIDs = Shop::Container()->getDB()->getInts(
+                'SELECT tartikel.kArtikel
+                    FROM tartikel
+                    LEFT JOIN tartikelsichtbarkeit 
+                        ON tartikel.kArtikel = tartikelsichtbarkeit.kArtikel
+                        AND tartikelsichtbarkeit.kKundengruppe = :cid
+                    WHERE tartikelsichtbarkeit.kArtikel IS NULL ' .
+                        $stockFilterSQL . ' ' .
+                        $parentSQL . '
+                        AND NOW() < tartikel.dErscheinungsdatum
+                    LIMIT :lmt',
+                'kArtikel',
+                ['cid' => $customerGroupID, 'lmt' => $limit]
+            );
+            Shop::Container()->getCache()->set($cacheID, $productIDs, $cacheTags);
+        }
+        \shuffle($productIDs);
+        $res = \array_slice($productIDs, 0, $config['boxen']['box_erscheinende_anzahl_anzeige']);
+        if (\count($res) > 0) {
+            $this->setShow(true);
+            $products = new ArtikelListe();
+            $products->getArtikelByKeys($res, 0, \count($res));
+            $this->setProducts($products);
+            $this->setURL(SearchSpecial::buildURL(\SEARCHSPECIALS_UPCOMINGPRODUCTS));
+            \executeHook(\HOOK_BOXEN_INC_ERSCHEINENDEPRODUKTE, [
+                'box'        => &$this,
+                'cache_tags' => &$cacheTags,
+                'cached'     => $cached
+            ]);
         }
     }
 }

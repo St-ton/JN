f4520f4bd4dee0f83ba7ba3b6bebf6984287dd66
Felix <felix.moche@jtl-software.com>
2022-07-11 14:30:33 +0200

    throw

--- includes/src/Recommendation/Manager.php
+++ includes/src/Recommendation/Manager.php
@@ -113,7 +113,7 @@ class Manager
             Shop::Container()->getLogService()->error($e->getMessage());
         }
 
-        return empty($res) ? [] : \json_decode((string)$res->getBody(), false)->extensions;
+        return empty($res) ? [] : \json_decode((string)$res->getBody(), false, 512, \JSON_THROW_ON_ERROR)->extensions;
     }
 
     /**

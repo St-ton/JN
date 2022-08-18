5a27db68669aa572b3f7ac63aea8991dc3921d1e
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2022-08-18 12:36:50 +0200

    Apply kundenimport.php. SHOP-6701

--- admin/kundenimport.php
+++ admin/kundenimport.php
@@ -12,21 +12,35 @@ require_once __DIR__ . '/includes/admininclude.php';
 
 $oAccount->permission('IMPORT_CUSTOMER_VIEW', true, true);
 
-if (isset($_FILES['csv']['tmp_name'])
-    && Request::postInt('kundenimport') === 1
-    && Form::validateToken()
-    && mb_strlen($_FILES['csv']['tmp_name']) > 0
-) {
-    $importer = new Import(Shop::Container()->getDB());
-    $importer->setCustomerGroupID(Request::postInt('kKundengruppe'));
-    $importer->setLanguageID(Request::postInt('kSprache'));
-    $result = $importer->processFile($_FILES['csv']['tmp_name']);
-    $notice = '';
-    foreach ($result as $item) {
-        $notice .= $item . '<br>';
+if (Form::validateToken()) {
+    if (isset($_FILES['csv']['tmp_name'])
+        && Request::postInt('kundenimport') === 1
+        && mb_strlen($_FILES['csv']['tmp_name']) > 0
+    ) {
+        $importer = new Import(Shop::Container()->getDB());
+        $importer->setCustomerGroupID(Request::postInt('kKundengruppe'));
+        $importer->setLanguageID(Request::postInt('kSprache'));
+
+        if ($importer->processFile($_FILES['csv']['tmp_name']) === false) {
+            $this->alertService->addError(\implode('<br>', $importer->getErrors()), 'importError');
+        }
+
+        if ($importer->getImportedRowsCount() > 0) {
+            $this->alertService->addSuccess(
+                \sprintf(\__('successImportCustomerCsv'), $importer->getImportedRowsCount()),
+                'importSuccess',
+                ['dismissable' => true, 'fadeOut' => 0]
+            );
+
+            $smarty->assign('noPasswordCustomerIds', $importer->getNoPasswordCustomerIds());
+        }
+    } elseif (Request::postVar('action') === 'notify-customers') {
+        $noPasswordCustomerIds = \json_decode(Request::postVar('noPasswordCustomerIds', '[]'));
+        $importer              = new Import($this->db);
+        $importer->notifyCustomers($noPasswordCustomerIds);
     }
-    Shop::Container()->getAlertService()->addAlert(Alert::TYPE_NOTE, $notice, 'importNotice');
 }
+
 $smarty->assign('kundengruppen', Shop::Container()->getDB()->getObjects(
     'SELECT * FROM tkundengruppe ORDER BY cName'
 ))

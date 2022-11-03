efa7385aadd3f33c8164d2bfff17c9a8889788ff
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2022-11-03 15:17:37 +0100

    Add currency to URL. SHOP-6964

--- includes/src/Router/Router.php
+++ includes/src/Router/Router.php
@@ -380,19 +380,19 @@ class Router
             $pfx .= $this->path;
         }
         if ($forceDynamic === false && $byName === true) {
-            $scheme = $this->config['global']['routing_default_language'] ?? 'F';
-            if ($isDefaultLocale && $scheme === 'F') {
-                return $pfx . '/' . ($replacements['name'] ?? $replacements['id']);
+            if ($isDefaultLocale) {
+                $scheme = $this->config['global']['routing_default_language'] ?? 'F';
+            } else {
+                $scheme = $this->config['global']['routing_scheme'] ?? 'F';
             }
-            if ($isDefaultLocale && $scheme === 'L') {
-                return $pfx . '/' . $replacements['lang'] . '/' . ($replacements['name'] ?? $replacements['id'] ?? '');
+            if ($scheme === 'F') {
+                return $pfx . '/' . ($replacements['name'] ?? $replacements['id'])
+                    . (isset($replacements['currency']) ? '?curr=' . $replacements['currency'] : '');
             }
-            $scheme = $this->config['global']['routing_scheme'] ?? 'F';
-            if (!$isDefaultLocale && $scheme === 'F') {
-                return $pfx . '/' . ($replacements['name'] ?? $replacements['id']);
-            }
-            if (!$isDefaultLocale && $scheme === 'L') {
-                return $pfx . '/' . $replacements['lang'] . '/' . ($replacements['name'] ?? $replacements['id'] ?? '');
+            if ($scheme === 'L') {
+                return $pfx . '/' . $replacements['lang'] . '/'
+                    . ($replacements['name'] ?? $replacements['id'] ?? '')
+                    . (isset($replacements['currency']) ? '?curr=' . $replacements['currency'] : '');
             }
         }
 
--- templates/NOVA/snippets/currency_dropdown.tpl
+++ templates/NOVA/snippets/currency_dropdown.tpl
@@ -7,8 +7,9 @@
             right=true
             text=$currentCurrency->getName()}
             {foreach $allCurrencies as $currency}
-                {dropdownitem href=$currency->getURLFull() rel="nofollow" active=($currentCurrency->getName() === $currency->getName())}
-                {$currency->getName()}
+                {dropdownitem href=$currency->getURLFull() rel="nofollow"
+                    active=($currentCurrency->getName() === $currency->getName())}
+                    {$currency->getName()}
                 {/dropdownitem}
             {/foreach}
         {/navitemdropdown}

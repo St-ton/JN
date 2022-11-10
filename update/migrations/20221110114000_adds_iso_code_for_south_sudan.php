0b68c9a5921f270e51597f681978df54fc676755
MStickel <marco.stickel@jtl-software.com>
2022-11-10 13:40:15 +0100

    adds iso code for south sudan. SHOP-5569

---
+++ update/migrations/20221110114000_adds_iso_code_for_south_sudan.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+use JTL\Services\JTL\CountryService;
+use JTL\Shop;
+use JTL\Template\Config;
+use JTL\Update\IMigration;
+use JTL\Update\Migration;
+
+/**
+ * Class Migration_20221110114000
+ */
+class Migration_20221110114000 extends Migration implements IMigration
+{
+    protected $author = 'ms';
+    protected $description = 'adds iso code for south sudan';
+
+    /**
+     * @inheritDoc
+     */
+    public function up(): void
+    {
+        $this->db->executeQuery("INSERT IGNORE INTO  
+            tland 
+                (cISO, cDeutsch, cEnglisch, nEU, cKontinent, bPermitRegistration, bRequireStateDefinition) 
+            VALUES ,
+                ('SS', 'Südsudan', 'South Sudan',0,'Afrika',0,0)");
+        Shop::Container()->getCache()->flush(CountryService::CACHE_ID);
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(): void
+    {
+        $this->db->delete('tland', 'cISO', 'SS');
+        Shop::Container()->getCache()->flush(CountryService::CACHE_ID);
+    }
+}

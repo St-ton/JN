f9292d2ab7ffaebf891430af10d084210d791c77
Stefan.JTL <stefan.langkau@jtl-software.com>
2022-12-06 12:54:23 +0100

    Dismiss currency from standard-voucher phrases SHOP-5642

---
+++ update/migrations/20221206124527_change_voucher_placeholder_shop_5642.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+/**
+ * Change Voucher placeholder SHOP-5642
+ *
+ * @author sl
+ * @created Tue, 06 Dec 2022 12:45:27 +0100
+ */
+
+use JTL\Update\IMigration;
+use JTL\Update\Migration;
+
+/**
+ * Class Migration_20221206124527
+ */
+class Migration_20221206124527 extends Migration implements IMigration
+{
+    protected $author = 'sl';
+    protected $description = 'Change Voucher placeholder SHOP-5642';
+
+    /**
+     * @inheritDoc
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down()
+    {
+    }
+}

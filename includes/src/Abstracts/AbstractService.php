008bde8394887980d625a63d86676432d932af87
Stefan Langkau <stefan.langkau@jtl-software.com>
2023-01-18 10:14:20 +0100

    Introducing DataTableObjectInterface SHOP-6988

--- includes/src/Abstracts/AbstractService.php
+++ includes/src/Abstracts/AbstractService.php
@@ -2,6 +2,8 @@
 
 namespace JTL\Abstracts;
 
+use JTL\DataObjects\DataObjectInterface;
+use JTL\DataObjects\DataTableObjectInterface;
 use JTL\Interfaces\RepositoryInterface;
 use JTL\Interfaces\ServiceInterface;
 
@@ -19,7 +21,25 @@ abstract class AbstractService implements ServiceInterface
     }
 
     /**
-     * @inheritDoc
+     * @inheritdoc
      */
     abstract public function getRepository(): RepositoryInterface;
+
+    /**
+     * @param array $filters
+     * @return array
+     */
+    abstract public function getList(array $filters): array;
+
+    /**
+     * @param DataTableObjectInterface|DataObjectInterface $object
+     * @return int
+     */
+    abstract public function insert(DataTableObjectInterface|DataObjectInterface $object): int;
+
+    /**
+     * @param DataTableObjectInterface|DataObjectInterface $object
+     * @return bool
+     */
+    abstract public function update(DataTableObjectInterface|DataObjectInterface $object): bool;
 }

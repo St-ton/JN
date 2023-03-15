109097928df843620dd14756a69bb9ba232cfcf4
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2023-03-15 13:13:44 +0100

    Count sells for marked bestsellers differently. SHOP-7150

--- includes/src/Cron/Job/TopSeller.php
+++ includes/src/Cron/Job/TopSeller.php
@@ -33,24 +33,32 @@ final class TopSeller extends Job
         $this->db->query('TRUNCATE tbestseller');
         $this->db->queryPrepared(
             'INSERT INTO tbestseller (kArtikel, fAnzahl, isBestseller)
-                SELECT m.kArtikel, SUM(m.nAnzahl) AS anz,
-                       SUM(m.nAnzahl) >= :minCount AND m.dErstellt > SUBDATE(CURDATE(), :maxDays)
+                SELECT
+                    p.kArtikel,
+                    IF(countInTime >= :minCount, countInTime, countTotal) as fAnzahl,
+                    countInTime >= :minCount as isBestseller
+                FROM (
+                    SELECT
+                        m.kArtikel,
+                        SUM(IF(m.dErstellt > SUBDATE(CURDATE(), :maxDays), m.nAnzahl, 0)) AS countInTime,
+                        SUM(m.nAnzahl) AS countTotal
                     FROM (
                         SELECT twarenkorbpos.kArtikel, twarenkorbpos.nAnzahl, tbestellung.dErstellt
                         FROM tbestellung
-                        INNER JOIN twarenkorbpos ON twarenkorbpos.kWarenkorb = tbestellung.kWarenkorb
+                            INNER JOIN twarenkorbpos ON twarenkorbpos.kWarenkorb = tbestellung.kWarenkorb
                         WHERE tbestellung.cStatus > 1
-                          AND twarenkorbpos.kArtikel > 0
+                            AND twarenkorbpos.kArtikel > 0
                         UNION ALL
                         SELECT tartikel.kVaterArtikel, twarenkorbpos.nAnzahl, tbestellung.dErstellt
                         FROM tbestellung
-                        INNER JOIN twarenkorbpos ON twarenkorbpos.kWarenkorb = tbestellung.kWarenkorb
-                        INNER JOIN tartikel ON twarenkorbpos.kArtikel = tartikel.kArtikel
+                            INNER JOIN twarenkorbpos ON twarenkorbpos.kWarenkorb = tbestellung.kWarenkorb
+                            INNER JOIN tartikel ON twarenkorbpos.kArtikel = tartikel.kArtikel
                         WHERE tbestellung.cStatus > 1
-                          AND twarenkorbpos.kArtikel > 0
-                          AND tartikel.kVaterArtikel > 0
+                            AND twarenkorbpos.kArtikel > 0
+                            AND tartikel.kVaterArtikel > 0
                     ) AS m
-                    GROUP BY m.kArtikel',
+                    GROUP BY m.kArtikel
+                ) AS p',
             $params
         );
         $this->setFinished(true);

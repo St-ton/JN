7bc2640211e17174be8b3f4321ca46ca11e25581
Stefan Langkau <stefan.langkau@jtl-software.com>
2023-03-22 17:58:04 +0100

    Fix pipeline

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ use JTL\TestEnvironment\ProvideTestData as ProvideTestData;
 
 if (isset($_SERVER['HTTP_TESTDB']) === true) {
     define('TESTDB', true);
-
 }
 require __DIR__ . '/includes/globalinclude.php';
 

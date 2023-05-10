c2522cd3eb5641f0427a3de699daeddf0302d822
Felix Moche <felix.moche@jtl-software.com>
2023-05-10 17:58:41 +0200

    controller

---
+++ includes/src/Router/Controller/Backend/UpgradeController.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace JTL\Router\Controller\Backend;
+
+use Exception;
+use JTL\Backend\DirManager;
+use JTL\Backend\Permissions;
+use JTL\Backend\Settings\Manager;
+use JTL\Backend\Settings\SectionFactory;
+use JTL\Backend\Settings\Sections\SectionInterface;
+use JTL\Backend\Upgrade\Channels;
+use JTL\Backend\Upgrade\ReleaseDownloader;
+use JTL\Helpers\Form;
+use JTL\Helpers\GeneralObject;
+use JTL\Helpers\Request;
+use JTL\Helpers\Text;
+use JTL\Minify\MinifyService;
+use JTL\Shop;
+use JTL\Smarty\JTLSmarty;
+use JTLShop\SemVer\Version;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use stdClass;
+
+/**
+ * Class CacheController
+ * @package JTL\Router\Controller\Backend
+ */
+class UpgradeController extends AbstractBackendController
+{
+    /**
+     * @var string
+     */
+    private string $tab = 'uebersicht';
+
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getResponse(ServerRequestInterface $request, array $args, JTLSmarty $smarty): ResponseInterface
+    {
+        $this->smarty = $smarty;
+        $this->checkPermissions(Permissions::OBJECTCACHE_VIEW);
+        $this->getText->loadAdminLocale('pages/upgrade');
+
+        $releaseDownloader = new ReleaseDownloader($smarty);
+        $releaseDownloader->getReleases();
+        if ($request->getMethod() === 'POST' && Form::validateToken()) {
+            if (Request::postInt('upgrade') === 1) {
+                $requestedID = Request::postInt('newerversions');
+                $release = $releaseDownloader->getReleaseByID($requestedID);
+                dd($release);
+            }
+        }
+
+        return $this->smarty->getResponse('upgrade.tpl');
+    }
+}

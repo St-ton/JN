de9c56f71487b64388d4f55435dd08bc11102e97
Felix Moche <felix.moche@jtl-software.com>
2023-05-16 11:50:55 +0200

    cleanup

--- includes/src/Router/Controller/Backend/UpgradeController.php
+++ includes/src/Router/Controller/Backend/UpgradeController.php
@@ -2,70 +2,73 @@
 
 namespace JTL\Router\Controller\Backend;
 
-use Exception;
-use JTL\Backend\DirManager;
 use JTL\Backend\Permissions;
-use JTL\Backend\Settings\Manager;
-use JTL\Backend\Settings\SectionFactory;
-use JTL\Backend\Settings\Sections\SectionInterface;
 use JTL\Backend\Upgrade\Channels;
 use JTL\Backend\Upgrade\ReleaseDownloader;
 use JTL\Backend\Upgrade\Upgrader;
 use JTL\Filesystem\Filesystem;
 use JTL\Helpers\Form;
-use JTL\Helpers\GeneralObject;
 use JTL\Helpers\Request;
-use JTL\Helpers\Text;
-use JTL\Minify\MinifyService;
 use JTL\Shop;
 use JTL\Smarty\JTLSmarty;
-use JTL\Update\MigrationManager;
 use JTLShop\SemVer\Version;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use stdClass;
 
 /**
- * Class CacheController
+ * Class UpgradeController
  * @package JTL\Router\Controller\Backend
+ * @since 5.3.0
  */
 class UpgradeController extends AbstractBackendController
 {
-    /**
-     * @var string
-     */
-    private string $tab = 'uebersicht';
-
-
-
     /**
      * @inheritdoc
      */
     public function getResponse(ServerRequestInterface $request, array $args, JTLSmarty $smarty): ResponseInterface
     {
         $this->smarty = $smarty;
-
         $this->checkPermissions(Permissions::OBJECTCACHE_VIEW);
         $this->getText->loadAdminLocale('pages/upgrade');
+        $this->smarty->assign('logs', [])
+            ->assign('errors', []);
 
-        $releaseDownloader = new ReleaseDownloader($smarty);
-        $releaseDownloader->getReleases();
         if ($request->getMethod() === 'POST' && Form::validateToken()) {
             if (Request::postInt('upgrade') === 1) {
-                $requestedID = Request::postInt('newerversions');
-                $release = $releaseDownloader->getReleaseByID($requestedID);
-                if ($release !== null) {
-                    $upgrader = new Upgrader(
-                        $this->db,
-                        $this->cache,
-                        Shop::Container()->get(Filesystem::class),
-                        $smarty
-                    );
-                    $upgrader->upgradeByRelease($release);
-                }
+                $this->upgrade();
             }
         }
+        $this->assignReleaseData();
 
         return $this->smarty->getResponse('upgrade.tpl');
+    }
+
+    private function assignReleaseData(): void
+    {
+        $activeChannel     = Channels::getActiveChannel();
+        $releaseDownloader = new ReleaseDownloader($this->db);
+        $this->smarty->assign('channels', Channels::getChannels())
+            ->assign('activeChannel', $activeChannel)
+            ->assign('availableVersions', $releaseDownloader->getReleases($activeChannel))
+            ->assign('currentVersion', Version::parse(\APPLICATION_VERSION));
+    }
+
+    private function upgrade(): void
+    {
+        $requestedID       = Request::postInt('newerversions');
+        $releaseDownloader = new ReleaseDownloader($this->db);
+        $release           = $releaseDownloader->getReleaseByID($requestedID);
+        if ($release === null) {
+            return;
+        }
+        $upgrader = new Upgrader(
+            $this->db,
+            $this->cache,
+            Shop::Container()->get(Filesystem::class),
+            $this->smarty
+        );
+        $upgrader->upgradeByRelease($release);
+        $this->smarty->assign('logs', $upgrader->getLogs())
+            ->assign('errors', $upgrader->getErrors());
     }
 }

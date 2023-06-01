192881ca736faaf529f131d1e41b2fa330d00918
Felix Moche <felix.moche@jtl-software.com>
2023-06-01 11:27:57 +0200

    moved php version check to release

--- includes/src/License/Struct/ReferencedTemplate.php
+++ includes/src/License/Struct/ReferencedTemplate.php
@@ -29,7 +29,6 @@ class ReferencedTemplate extends ReferencedItem
             $installedVersion = Version::parse($data->version);
             $availableVersion = $available === null ? Version::parse('0.0.0') : $available->getVersion();
             $latestVersion    = $latest === null ? $availableVersion : $latest->getVersion();
-            $php              = $this->getCurrentPhpVersion();
             $this->setMaxInstallableVersion($installedVersion);
             $this->setHasUpdate(false);
             $this->setCanBeUpdated(false);
@@ -37,12 +36,7 @@ class ReferencedTemplate extends ReferencedItem
                 $this->setMaxInstallableVersion($availableVersion);
                 $this->setHasUpdate(true);
                 $this->setCanBeUpdated(true);
-                if ($available->getPhpMaxVersion() !== null && $php->greaterThan($available->getPhpMaxVersion())) {
-                    $this->setPhpVersionOK(self::PHP_VERSION_HIGH);
-                    $this->setCanBeUpdated(false);
-                }
-                if ($available->getPhpMinVersion() !== null && $php->smallerThan($available->getPhpMinVersion())) {
-                    $this->setPhpVersionOK(self::PHP_VERSION_LOW);
+                if ($available->getPhpVersionOK() !== $available::PHP_VERSION_OK) {
                     $this->setCanBeUpdated(false);
                 }
             } elseif ($latestVersion->greaterThan($availableVersion)
@@ -51,12 +45,6 @@ class ReferencedTemplate extends ReferencedItem
                 $this->setMaxInstallableVersion($latestVersion);
                 $this->setHasUpdate(true);
                 $this->setCanBeUpdated(false);
-                if ($latest->getPhpMaxVersion() !== null && $php->greaterThan($latest->getPhpMaxVersion())) {
-                    $this->setPhpVersionOK(self::PHP_VERSION_HIGH);
-                }
-                if ($latest->getPhpMinVersion() !== null && $php->smallerThan($latest->getPhpMinVersion())) {
-                    $this->setPhpVersionOK(self::PHP_VERSION_LOW);
-                }
             }
             $this->setID($data->cTemplate);
             $this->setInstalled(true);
